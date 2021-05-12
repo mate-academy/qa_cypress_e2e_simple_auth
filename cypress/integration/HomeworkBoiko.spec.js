0f2f183d09b2288bfdd09a323e7b772fc38bce21
tnboyko <obolyab@gmail.com>
2021-05-12 14:15:58 +0300

    Added login tests

--- cypress/integration/HomeworkBoiko.spec.js
+++ cypress/integration/HomeworkBoiko.spec.js
@@ -1,9 +1,27 @@
 /// <reference types="cypress" />
 
-before(function () {
+
+// it('User can login with valid creds (tomsmith/SuperSecretPassword!)', () => {
+//     cy.visit('https://the-internet.herokuapp.com/login');
+//     cy.get('#username').type('tomsmith').should('have.value', 'tomsmith');
+//     cy.get('#password').type('SuperSecretPassword!').should('have.value', 'SuperSecretPassword!');
+//     cy.get('.fa').click();
+// });
+
+// it('User can\'t login with invalid creds, error message must appear', () => {
+//     cy.visit('https://the-internet.herokuapp.com/login');
+//     cy.get('#username').type('tanya').should('have.value', 'tanya');
+//     cy.get('#password').type('NotASecretPassword!').should('have.value', 'NotASecretPassword!');
+//     cy.get('.fa').click();
+//     cy.get('#flash').should('contain', 'is invalid');
+// });
+
+it('Logout from app and assert you successfully logged out', () => {
     cy.visit('https://the-internet.herokuapp.com/login');
-});
-
-it('User can login with valid creds (tomsmith/SuperSecretPassword!)', () => {
-
+    cy.get('#username').type('tomsmith').should('have.value', 'tomsmith');
+    cy.get('#password').type('SuperSecretPassword!').should('have.value', 'SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
+    cy.get('.button').click();
+    cy.get('#flash').should('contain', 'You logged out of the secure area!');
 });
