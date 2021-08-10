930816cd72f749fcbc4a86a906c905be04c46ad4
Pavlo Starynskyi <starinskiy@gmail.com>
2021-08-10 16:54:07 +0300

    after review

--- cypress/integration/HomeworkStarynskyi.spec.js
+++ cypress/integration/HomeworkStarynskyi.spec.js
@@ -6,17 +6,23 @@ describe('Check logging with valid and invalid credentials', () => {
       cy.visit('https://the-internet.herokuapp.com/login');
   
     });
-    it('Follow the next steps', () => {
+
+    it('Check logging with correct credentials', () => {
       cy.get('#username').type('tomsmith');
       cy.get('#password').type('SuperSecretPassword!');
       cy.get('.radius').click();
       cy.get('.success').contains('You logged into a secure area!').should('exist');
+    });
+
+    it('Check logging out', () => {
       cy.get('.button').click();
       cy.get('.success').contains('You logged out of the secure area!').should('exist');
+    });
+
+    it('Check logging with incorrect credentials', () => {
       cy.get('#username').type('tomyorke');
       cy.get('#password').type('radiohead');
       cy.get('.radius').click();
       cy.get('.error').contains('Your username is invalid!').should('exist');
     });
   });
-  
