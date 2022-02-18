4378ac200ea4c6631eaa74c06daefe11c06e2e41
Yuriy Shvorob <zyuraz1993@gmail.com>
2022-02-18 17:06:13 +0200

    Solution Yuriy Shvorob

--- cypress/integration/signIn.spec.js
+++ cypress/integration/signIn.spec.js
@@ -1,1 +1,51 @@
 /// <reference types="cypress" />
+
+describe("Sign in page", () => {
+  
+  beforeEach(() => {
+    cy.visit('/login')
+  })
+
+
+  it("Login with valid creds", () => {
+    cy.get('#username')
+    .type('tomsmith')
+
+    cy.get('#password')
+    .type('SuperSecretPassword!')
+
+    cy.get('button').contains('Login')
+    .click()
+
+    cy.get('.flash.success').should('contain', 'You logged into a secure area!')
+  });
+  
+  it("Login with invalid creds", () => {
+    cy.get('#username')
+    .type('tom')
+
+    cy.get('#password')
+    .type('SuperSecretPassword!')
+
+    cy.get('button').contains('Login')
+    .click()
+
+    cy.get('.flash.error').should('contain', 'Your username is invalid!')
+  });
+
+  it("Logout", () => {
+    cy.get('#username')
+    .type('tomsmith')
+
+    cy.get('#password')
+    .type('SuperSecretPassword!')
+
+    cy.get('button').contains('Login')
+    .click()
+
+    cy.get('[href="/logout"]')
+    .click()
+
+    cy.get('.flash.success').should('contain', 'You logged out of the secure area!')
+  });
+});
