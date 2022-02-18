1ce91c77158a3d26a83aaca4952b7defe6de4913
Igor Lega <lega.igor@gmail.com>
2022-02-18 15:05:46 +0200

    Final solution

--- cypress/integration/signIn.spec.js
+++ cypress/integration/signIn.spec.js
@@ -2,23 +2,31 @@
 
 describe('Sign In', () => {
     it('Should log in with valid creds', () => {
-        cy.visit("https://the-internet.herokuapp.com/login");
+        cy.visit("/login");
         cy.get('[type=text]').type('tomsmith');
         cy.get('[type=password]').type('SuperSecretPassword!' + '{enter}');
         cy.get('#flash').should('contain.text', 'You logged into a secure area!');
         cy.url().should('include', 'secure');
     })
 
-    it('Shouldn`t log in with invalid creds', () => {
-        cy.visit("https://the-internet.herokuapp.com/login");
-        cy.get('[type=text]').type('Tomsmith');
-        cy.get('[type=password]').type('NotThatSecretPassword!' + '{enter}');
+    it('Shouldn`t log in with invalid username', () => {
+        cy.visit("/login");
+        cy.get('[type=text]').type('Tom');
+        cy.get('[type=password]').type('SuperSecretPassword!' + '{enter}');
         cy.get('#flash').should('contain.text', 'Your username is invalid!');
         cy.url().should('include', 'login');
     })
 
+    it('Shouldn`t log in with invalid password', () => {
+        cy.visit("/login");
+        cy.get('[type=text]').type('tomsmith');
+        cy.get('[type=password]').type('NotThatSecretPassword!' + '{enter}');
+        cy.get('#flash').should('contain.text', 'Your password is invalid!');
+        cy.url().should('include', 'login');
+    })
+
     it('Logged in user should be able to log out', () => {
-        cy.visit("https://the-internet.herokuapp.com/login");
+        cy.visit("/login");
         cy.get('[type=text]').type('tomsmith');
         cy.get('[type=password]').type('SuperSecretPassword!' + '{enter}');
         cy.get('[href="/logout"]').click();
