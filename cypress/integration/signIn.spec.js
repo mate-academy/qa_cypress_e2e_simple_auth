324a50ee5513feb1e0c265621a2f4af069a1ba9c
Bohdan Aleinikov <bogdan.aleynikov@gmail.com>
2022-02-20 23:58:07 +0200

    Solution

--- cypress/integration/signIn.spec.js
+++ cypress/integration/signIn.spec.js
@@ -1,1 +1,27 @@
 /// <reference types="cypress" />
+describe('', () => {
+    it('', () => {
+      cy.visit('https://the-internet.herokuapp.com/login')
+      cy.get('#username')
+      .type('tomsmith')
+      cy.get('#password')
+      .type('SuperSecretPassword!')
+      cy.get('.fa')
+      .click()
+      cy.get('#flash')
+      .should('contain', 'You logged into a secure area!')
+      cy.get('.button')
+      .click()
+      cy.get('#flash')
+      .should('contain', 'You logged out of the secure area!')
+
+      cy.get('#username')
+      .type('tomsmit')
+      cy.get('#password')
+      .type('SuperSecretPassword!')
+      cy.get('.fa')
+      .click()
+      cy.get('#flash')
+      .should('contain','Your username is invalid!')
+    });
+  });
