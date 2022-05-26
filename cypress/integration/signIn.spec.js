782660c9e0fe4a0fad1da4f005c36ce67c1f41de
Roman Stas <romastas1996@gmail.com>
2022-05-26 12:16:00 +0300

    FIrst task solution

--- cypress/integration/signIn.spec.js
+++ cypress/integration/signIn.spec.js
@@ -1,1 +1,21 @@
 /// <reference types="cypress" />
+
+describe('Login', () => {
+  it('with valid data', () => {
+
+    cy.visit("https://the-internet.herokuapp.com/login")
+
+    cy.get('#username')
+      .type('tomsmith')
+
+    cy.get('#password')
+      .type('SuperSecretPassword!')
+
+    cy.get('.fa')
+      .click()
+
+    cy.get('#flash')
+      .should('contain', 'You logged into a secure area!')
+
+  })
+})
