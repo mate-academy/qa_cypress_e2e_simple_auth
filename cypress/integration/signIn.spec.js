1a529544297fe828b0b2b61b20b8768d3b17d856
Roman Stas <romastas1996@gmail.com>
2022-05-26 16:04:06 +0300

    solution all task

--- cypress/integration/signIn.spec.js
+++ cypress/integration/signIn.spec.js
@@ -1,9 +1,12 @@
 /// <reference types="cypress" />
 
-describe('Login', () => {
-  it('with valid data', () => {
 
-    cy.visit("https://the-internet.herokuapp.com/login")
+
+describe('Login', () => {
+  beforeEach(() => {
+    cy.visit("/login")
+  })
+  it('with valid data', () => {
 
     cy.get('#username')
       .type('tomsmith')
@@ -11,11 +14,49 @@ describe('Login', () => {
     cy.get('#password')
       .type('SuperSecretPassword!')
 
-    cy.get('.fa')
+    cy.get('button[type="submit"]')
+      .click()
+
+    cy.get('#flash')
+      .should('contain', 'You logged into a secure area!')
+  })
+
+  it('with invalid data', () => {
+
+    cy.get('#username')
+      .type('not_tomsmith')
+
+    cy.get('#password')
+      .type('Not_SuperSecretPassword!')
+
+    cy.get('button[type="submit"]')
+      .click()
+
+    cy.get('#flash')
+      .should('contain', 'Your username is invalid!')
+  })
+
+  it('and logout after', () => {
+
+    cy.get('#username')
+      .type('tomsmith')
+
+    cy.get('#password')
+      .type('SuperSecretPassword!')
+
+    cy.get('button[type="submit"]')
       .click()
 
     cy.get('#flash')
       .should('contain', 'You logged into a secure area!')
 
+    cy.get('.button  .icon-signout')
+      .click()
+
+    cy.get('#flash')
+      .should('contain', 'You logged out of the secure area!')
+    
+    cy.url()
+      .should('include', 'login')
   })
 })
