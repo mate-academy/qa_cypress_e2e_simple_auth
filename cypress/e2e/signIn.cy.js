7209df0bc8bedcfccfa40317e1616c78a7a1692a
Zoryana Blashchuk <zblashchuk@gmail.com>
2022-07-06 22:37:12 +0300

    Solution

---
+++ cypress/e2e/signIn.cy.js
@@ -0,0 +1,71 @@
+/// <reference types="cypress" />
+
+describe('Login with valid creds', () => {
+  beforeEach(()=>{ 
+  });
+  it('User should log in with data have registrate before ', () => {
+  const username = 'tomsmith'
+  const password = 'SuperSecretPassword!'
+  cy.visit('/login')
+  cy.get('#username')
+    .type(username)
+    cy.get('#password')
+    .type(password)
+
+    cy.contains('button', 'Login')
+    .click();
+  });
+  
+  it('User should see a message You logged into a secure area! ', () => {
+    cy.loginUser()
+    cy.get('#flash').should('contain.text', 'You logged into a secure area!')
+
+  });
+  it('User should be secure page', () => {
+    cy.loginUser()
+   cy.assertPageUrl('/secure')
+  
+  });
+  it('User should have a link for logout', () => {
+    cy.loginUser()
+    cy.get('.button')
+    .should('contain', 'Logout')
+  
+  });
+});
+
+describe('Login with invalid creds and check validation error', () => {
+  beforeEach(()=>{ 
+  });
+  it('User should see message Your username is invalid!, when login with invalid username', () => {
+    cy.loginUserInvalidName()
+    cy.get('#flash').should('contain.text', 'Your username is invalid!')
+  });  
+  
+  it('User should see message Your password is invalid!, when login with invalid username', () => {
+    cy.loginUserInvalidPassword()
+    cy.get('#flash').should('contain.text', 'Your password is invalid!')
+  });  
+});
+
+describe('Logout from the app and assert you successfully logged out.', () => {
+    beforeEach(()=>{ 
+      cy.loginUser()
+      cy.get('.button')
+      .should('contain', 'Logout').click();
+    });
+
+    it('User should see a message You logged out of the secure area!', () => {
+      cy.get('#flash').should('contain.text', 'You logged out of the secure area!')
+  
+    });
+    
+    it('User should be login page', () => {
+     cy.assertPageUrl('/login')
+    });
+
+    it('User should have a link for login', () => {
+      cy.get('button')
+      .should('contain', 'Login')
+    });
+  });
