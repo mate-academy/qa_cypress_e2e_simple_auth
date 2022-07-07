dc521fd123152f73ad3f9f3b03cdbeee209f0dbc
Zoryana Blashchuk <zblashchuk@gmail.com>
2022-07-07 21:14:22 +0300

    fixed

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -7,20 +7,20 @@ it('User should see a message You logged into a secure area!, be secure page and
   const password = 'SuperSecretPassword!'
   cy.visit('/login')
   cy.get('#username')
-  .type(username)
+    .type(username)
   cy.get('#password')
-  .type(password)
+    .type(password)
 
   cy.contains('button', 'Login')
-  .click();
+    .click();
 
   cy.get('#flash')
-  .should('contain.text', 'You logged into a secure area!')
+    .should('contain.text', 'You logged into a secure area!')
 
   cy.loginUser()
   cy.assertPageUrl('/secure')
   cy.get('.button')
-  .should('contain', 'Logout')
+    .should('contain', 'Logout')
   
   });
 });
@@ -30,12 +30,12 @@ describe('Login with invalid creds and check validation error', () => {
     cy.loginUserInvalidName()
 
     cy.get('#flash')
-    .should('contain.text', 'Your username is invalid!')
+      .should('contain.text', 'Your username is invalid!')
 
     cy.loginUserInvalidPassword()
     
     cy.get('#flash')
-    .should('contain.text', 'Your password is invalid!')
+      .should('contain.text', 'Your password is invalid!')
   });  
 });
 
@@ -43,15 +43,16 @@ describe('Logout from the app and assert you successfully logged out.', () => {
     beforeEach(()=>{ 
       cy.loginUser()
       cy.get('.button')
-      .should('contain', 'Logout').click();
+        .should('contain', 'Logout').click();
     });
 
   it('User should see a message You logged out of the secure area!, be login page, have a link for login', () => {
-    cy.get('#flash').should('contain.text', 'You logged out of the secure area!')
+    cy.get('#flash')
+      .should('contain.text', 'You logged out of the secure area!')
   
     cy.assertPageUrl('/login')
   
     cy.get('button')
-    .should('contain', 'Login')
+      .should('contain', 'Login')
     });
   });
