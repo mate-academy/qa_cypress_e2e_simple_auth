0d6f1701dd937ff4960ae5756f929c340c9995d4
sonyapolya <sofiya.sonya0707@gmail.com>
2022-08-26 13:54:03 +0300

    Solution

---
+++ cypress/e2e/signIn.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+describe('Sign In', () => {
+
+    it('should login user', () => {
+      cy.visit('https://the-internet.herokuapp.com/login');
+  
+      cy.get('[name="username"]')
+      .type('tomsmith');
+  
+      cy.get('[name="password"]')
+      .type('SuperSecretPassword!');
+  
+      cy.get('button.radius')
+      .click();
+
+      cy.url()
+      .should('include','https://the-internet.herokuapp.com/secure');
+
+      cy.contains('div#flash.flash.success','You logged into a secure area!')
+      .should('exist');
+
+      cy.contains('a.button.secondary.radius', 'Logout')
+      .should('exist');
+    });
+  });
+  
