c07b2d9d0c9489197613642fb40bbd5004e447c5
Rostyk Reshetar <rostik.210111@gmail.com>
2022-08-29 14:07:43 +0300

    solution

---
+++ cypress/support/testdata.js
@@ -0,0 +1,10 @@
+
+function testData() {
+  const username = 'cypresstest1'
+  const email = 'cypresstest.cypresstest@gmail.com'
+  const password = 'cypresstest'
+
+  return { email, password, username };
+}
+
+module.exports = { testData };
