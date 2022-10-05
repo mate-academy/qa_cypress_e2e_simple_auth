217f41c161fc69bdf7e7bcd8fd118b72ebc0dc92
Anna Kononuchenko <horsepower619@gmail.com>
2022-10-05 11:30:03 +0300

    Solution of Second HW

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -7,43 +7,43 @@ describe('User should', () => {
       password : 'SuperSecretPassword!'
    };
   it('to Login with valid creds', () => {
-    cy.get('#username')
+    cy.get('[id="username"]')
       .type(validCreds.username);
-    cy.get('#password')
+    cy.get('[id="password"]')
       .type(validCreds.password);
     cy.get('.radius')
       .click();
-    cy.get('#flash')
+      cy.get('[id="flash"]')
       .should('contain', 'You logged into a secure area!')
   });
   it('not to Login with invalid Username', () => {
-    cy.get('#username')
+    cy.get('[id="username"]')
       .type(validCreds.username+'invalid');
-    cy.get('#password')
+    cy.get('[id="password"]')
       .type(validCreds.password);
     cy.get('.radius')
       .click();
-    cy.get('#flash')
+    cy.get('[id="flash"]')
       .should('contain', 'Your username is invalid!')
   });
   it('not to Login with invalid Password', () => {
-    cy.get('#username')
+    cy.get('[id="username"]')
       .type(validCreds.username);
-    cy.get('#password')
+    cy.get('[id="password"]')
       .type(validCreds.password+'invalid');
     cy.get('.radius')
       .click();
-    cy.get('#flash')
+    cy.get('[id="flash"]')
       .should('contain', 'Your password is invalid!')
   });
   it('to Logout from the app', () => {
-    cy.get('#username')
+    cy.get('[id="username"]')
       .type(validCreds.username);
-    cy.get('#password')
+    cy.get('[id="password"]')
       .type(validCreds.password);
     cy.get('.radius')
       .click();
-    cy.get('#flash')
+    cy.get('[id="flash"]')
       .should('contain', 'You logged into a secure area!')
     cy.get('.button')
       .should('contain', 'Logout')
