48055fc2851ad823f2fac038794308c2be23a662
nicatino <103503824+nicatino@users.noreply.github.com>
2022-10-05 12:16:59 +0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,3 @@
-/* Cypress.Commands.add('loginUser', () => {
-
-  cy.get('#username')
-    .type('tomsmith');
-  cy.get('#password')
-    .type('SuperSecretPassword!');
-
-  cy.get('.fa.fa-2x.fa-sign-in')
-    .click();
-
-});  */
 
 Cypress.Commands.add('loginUser', (username, password) => {
 
