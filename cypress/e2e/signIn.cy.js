45dac41562ba616cbbb0d1909386e325767c1046
Ivan Shulym <funnutua@gmail.com>
2022-10-05 12:40:27 +0300

    Edited Homework 2 for review

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -6,31 +6,31 @@ describe('User shoul be able', () => {
     });
 
     it('to login with a valid credentials', () => {
-        cy.get('#username').type('tomsmith');
-        cy.get('#password').type('SuperSecretPassword!');
+        cy.get('[id="username"]').type('tomsmith');
+        cy.get('[id="password"]').type('SuperSecretPassword!');
         cy.get('[class="radius"]').click();
-        cy.get('#flash').should('contain.text', 'You logged into a secure area!');
+        cy.get('[id="flash"]').should('contain.text', 'You logged into a secure area!');
     });
 
     it('to login with an invalid username', () => {
-        cy.get('#username').type('tomsmitha');
-        cy.get('#password').type('SuperSecretPassword!');
+        cy.get('[id="username"]').type('tomsmitha');
+        cy.get('[id="password"]').type('SuperSecretPassword!');
         cy.get('[class="radius"]').click();
-        cy.get('#flash').should('contain.text', 'Your username is invalid!');
+        cy.get('[id="flash"]').should('contain.text', 'Your username is invalid!');
     });
 
     it('to login with an invalid password', () => {
-        cy.get('#username').type('tomsmith');
-        cy.get('#password').type('SuperSecretPass');
+        cy.get('[id="username"]').type('tomsmith');
+        cy.get('[id="password"]').type('SuperSecretPass');
         cy.get('[class="radius"]').click();
-        cy.get('#flash').should('contain.text', 'Your password is invalid!');
+        cy.get('[id="flash"]').should('contain.text', 'Your password is invalid!');
     });
 
     it('to logout successfully from the app', () => {
-        cy.get('#username').type('tomsmith');
-        cy.get('#password').type('SuperSecretPassword!');
+        cy.get('[id="username"]').type('tomsmith');
+        cy.get('[id="password"]').type('SuperSecretPassword!');
         cy.get('[class="radius"]').click();
         cy.get('[href="/logout"]').click();
-        cy.get('#flash').should('contain.text', 'You logged out of the secure area!');
+        cy.get('[id="flash"]').should('contain.text', 'You logged out of the secure area!');
     });
 });
