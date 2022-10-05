aa6809558ea6e070ad9db5253b5a9c4c8dc7dd7e
nicatino <epicjew.nougoi@gmail.com>
2022-10-05 12:16:16 +0300

    Added commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,23 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/* Cypress.Commands.add('loginUser', () => {
+
+  cy.get('#username')
+    .type('tomsmith');
+  cy.get('#password')
+    .type('SuperSecretPassword!');
+
+  cy.get('.fa.fa-2x.fa-sign-in')
+    .click();
+
+});  */
+
+Cypress.Commands.add('loginUser', (username, password) => {
+
+  cy.get('#username')
+    .type(`${username}`);
+  cy.get('#password')
+    .type(`${password}`);
+
+  cy.get('.fa.fa-2x.fa-sign-in')
+    .click();
+
+}); 
