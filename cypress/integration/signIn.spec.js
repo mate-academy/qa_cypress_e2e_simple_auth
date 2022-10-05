8c5064f31d6c87f21ec91a94078b578999d2bf50
Oleksandra Polishchuk <alex.wazaa@gmail.com>
2022-10-05 11:23:28 +0300

    Second HW

--- cypress/integration/signIn.spec.js
+++ cypress/integration/signIn.spec.js
@@ -1,35 +1,71 @@
 /// <reference types="cypress" />
-describe('User shoul be able', () => {
-    beforeEach(() => {
-        cy.visit('https://the-internet.herokuapp.com/login');
-    });
+beforeEach(() => {
+  cy.visit('https://the-internet.herokuapp.com/login');
+})
 
-    it('to login with a valid credentials', () => {
-        cy.get('#username').type('tomsmith');
-        cy.get('#password').type('SuperSecretPassword!');
-        cy.get('[class="radius"]').click();
-        cy.get('#flash').should('contain.text', 'You logged into a secure area!');
-    });
+describe('Successful Login test', () => {
+  const userName = 'tomsmith';
+  const password = 'SuperSecretPassword!';
+  it('Successful login test', () => {
+      cy.get('label[for="username"]')
+          .should('contain.text', 'Username');
+      cy.get('input[name="username"]')
+          .type(userName);
 
-    it('to login with an invalid username', () => {
-        cy.get('#username').type('tomsmitha');
-        cy.get('#password').type('SuperSecretPassword!');
-        cy.get('[class="radius"]').click();
-        cy.get('#flash').should('contain.text', 'Your username is invalid!');
-    });
+      cy.get('label[for="password"]')
+          .should('contain.text', 'Password');
+      cy.get('input[name="password"]')
+          .type(password);
 
-    it('to login with an invalid password', () => {
-        cy.get('#username').type('tomsmith');
-        cy.get('#password').type('SuperSecretPass');
-        cy.get('[class="radius"]').click();
-        cy.get('#flash').should('contain.text', 'Your password is invalid!');
-    });
+      cy.get('button[type="submit"]')
+          .click();
 
-    it('to logout successfully from the app', () => {
-        cy.get('#username').type('tomsmith');
-        cy.get('#password').type('SuperSecretPassword!');
-        cy.get('[class="radius"]').click();
-        cy.get('[href="/logout"]').click();
-        cy.get('#flash').should('contain.text', 'You logged out of the secure area!');
-    });
+      cy.get('div[class="flash success"]')
+          .should('contain.text', 'You logged into a secure area!');
+  });
+
+  it('Login with correct password and incorrect username', () => {
+      cy.get('input[name="username"]')
+          .type(userName + 's');
+
+      cy.get('input[name="password"]')
+          .type(password);
+
+      cy.get('button[type="submit"]')
+          .click();
+
+      cy.get('div[class="flash error"]')
+          .should('contain.text', 'Your username is invalid!');
+  });
+
+  it('Login with correct username and incorrect password', () => {
+      cy.get('input[name="username"]')
+          .type(userName);
+
+      cy.get('input[name="password"]')
+          .type(password + '77');
+
+      cy.get('button[type="submit"]')
+          .click();
+
+      cy.get('div[class="flash error"]')
+          .should('contain.text', 'Your password is invalid!');
+  });
+
+  it('Successful Logout test', () => {
+      cy.get('input[name="username"]')
+          .type(userName);
+
+      cy.get('input[name="password"]')
+          .type(password);
+
+      cy.get('button[type="submit"]')
+          .click();
+
+      cy.get('.button')
+          .click();
+
+      cy.get('div[class="flash success"]')
+          .should('contain.text', 'You logged out of the secure area!');
+  });
 });
