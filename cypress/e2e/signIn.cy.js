d22e83516c2ab718dfc12f14fe0c451b40d1a818
Elina Nechaieva <elina.19.ua@gmail.com>
2022-10-06 14:54:26 +0300

    Cypress HW#2. Fixed version [Elina Nechaieva]

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -12,15 +12,15 @@ describe("Sign In page", () => {
     password: "invalidPassword",
   };
   it("Should allow to log in with valid data", () => {
-    cy.get("#username").type(validData.username);
-    cy.get("#password").type(validData.password);
+    cy.get("[id = 'username']").type(validData.username);
+    cy.get("[id = 'password']").type(validData.password);
     cy.get(".fa").click();
     cy.get("#flash").should("contain.text", "You logged into a secure area!");
   });
 
   it("Should allow to logout", () => {
-    cy.get("#username").type(validData.username);
-    cy.get("#password").type(validData.password);
+    cy.get("[id = 'username']").type(validData.username);
+    cy.get("[id = 'password']").type(validData.password);
     cy.get(".fa").click();
     cy.get(".button").click();
     cy.get('[id="flash"]').should(
@@ -29,14 +29,14 @@ describe("Sign In page", () => {
     );
   });
   it("Shouldn`t allow to log in with invalid username", () => {
-    cy.get("#username").type(invalidData.username);
-    cy.get("#password").type(validData.password);
+    cy.get("[id = 'username']").type(invalidData.username);
+    cy.get("[id = 'password']").type(validData.password);
     cy.get(".fa").click();
     cy.get("#flash").should("contain.text", "Your username is invalid!");
   });
   it("Shouldn`t allow to log in with invalid password", () => {
-    cy.get("#username").type(validData.username);
-    cy.get("#password").type(invalidData.password);
+    cy.get("[id = 'username']").type(validData.username);
+    cy.get("[id = 'password']").type(invalidData.password);
     cy.get(".fa").click();
     cy.get("#flash").should("contain.text", "Your password is invalid!");
   });
