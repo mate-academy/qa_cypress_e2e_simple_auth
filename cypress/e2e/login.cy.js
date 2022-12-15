02c3339dec1e28f8909b67d0ee79ef0be1a25f08
Kateryna Kharytonova <katekharytonova@gmail.com>
2022-12-15 16:36:18 +0200

    testing

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,31 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit('/login');
   });
 
-  it('', () => {
-    
+  it('should login user with registerd credentials', () => {
+
+  cy.get('[name="username"]').click().type('tomsmith');
+  cy.get('[name="password"]').click().type('SuperSecretPassword!');
+  cy.get('.fa').click();
+
+  cy.get('.flash').should('contain','You logged into a secure area!');
+  });
+
+  it('should not login user with invalid credentials', () => {
+
+  cy.get('[name="username"]').click().type('anasmith');
+  cy.get('[name="password"]').click().type('SuperPassword!');
+  cy.get('.fa').click();
+
+  cy.get('.flash').should('contain','Your username is invalid!')
+  });
+
+  it('user should be able to log out', () => {
+    cy.get('[name="username"]').click().type('tomsmith');
+    cy.get('[name="password"]').click().type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.contains('.icon-2x', 'Logout');
   });
 });
