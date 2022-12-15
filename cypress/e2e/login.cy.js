eec624defa39f7c7dc1d3ddef36cfeffd22d5b35
Larysa Krynytska <larisa.k.avramenko@gmail.com>
2022-12-15 21:42:00 +0200

    Solution

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,76 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit('https://the-internet.herokuapp.com/login')
   });
 
-  it('', () => {
+  it('should login user with registered credentials', () => {
     
+    //fill in the fields
+    cy.get('#username')
+      .type('tomsmith');
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    //click on the button
+    cy.get('.radius')
+      .should('contain.text', 'Login')
+      .click();
+
+    //check the result
+    cy.url()
+      .should('include', 'secure');
+    cy.get('#flash')
+      .should('contain.text', 'You logged into a secure area!');
+  });
+
+  it(`shouldn't login user with unregistered credentials`, () => {
+    
+    //fill in the fields
+    cy.get('#username')
+      .type('tomsmith1');
+
+    cy.get('#password')
+      .type('SuperSecretPassword!1');
+
+    //click on the button
+    cy.get('.radius')
+      .should('contain.text', 'Login')
+      .click();
+
+    //check the result
+    cy.get('#flash')
+      .should('contain.text', 'Your username is invalid!')
+  });
+
+  it('should login user with registered credentials and log out', () => {
+    
+    //fill in the fields
+    cy.get('#username')
+      .type('tomsmith');
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    //click on the button
+    cy.get('.radius')
+      .should('contain.text', 'Login')
+      .click();
+
+    //check the result
+    cy.url()
+      .should('include', 'secure');
+    cy.get('#flash')
+      .should('contain.text', 'You logged into a secure area!');
+
+    //click on the button
+    cy.get('.radius')
+      .should('contain.text', 'Logout')
+      .click();
+
+    //check the result
+      cy.get('#flash')
+      .should('contain.text', 'You logged out of the secure area!');
   });
 });
