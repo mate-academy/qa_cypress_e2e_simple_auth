026af4f23c407d54919355e0cf4ea8a7864a88a4
Larysa Krynytska <larisa.k.avramenko@gmail.com>
2022-12-15 22:07:47 +0200

    Solution

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,10 +8,10 @@ describe('Login page', () => {
   it('should login user with registered credentials', () => {
     
     //fill in the fields
-    cy.get('#username')
+    cy.get('[id="username"]')
       .type('tomsmith');
 
-    cy.get('#password')
+    cy.get('[id="password"]')
       .type('SuperSecretPassword!');
 
     //click on the button
@@ -26,14 +26,14 @@ describe('Login page', () => {
       .should('contain.text', 'You logged into a secure area!');
   });
 
-  it(`shouldn't login user with unregistered credentials`, () => {
+  it(`shouldn't login user with unregistered username`, () => {
     
     //fill in the fields
-    cy.get('#username')
+    cy.get('[id="username"]')
       .type('tomsmith1');
 
-    cy.get('#password')
-      .type('SuperSecretPassword!1');
+    cy.get('[id="password"]')
+      .type('SuperSecretPassword!');
 
     //click on the button
     cy.get('.radius')
@@ -45,13 +45,32 @@ describe('Login page', () => {
       .should('contain.text', 'Your username is invalid!')
   });
 
+  it(`shouldn't login user with unregistered password`, () => {
+    
+    //fill in the fields
+    cy.get('[id="username"]')
+      .type('tomsmith');
+
+    cy.get('[id="password"]')
+      .type('SuperSecretPassword!1');
+
+    //click on the button
+    cy.get('.radius')
+      .should('contain.text', 'Login')
+      .click();
+
+    //check the result
+    cy.get('#flash')
+      .should('contain.text', 'Your password is invalid!')
+  });
+
   it('should login user with registered credentials and log out', () => {
     
     //fill in the fields
-    cy.get('#username')
+    cy.get('[id="username"]')
       .type('tomsmith');
 
-    cy.get('#password')
+    cy.get('[id="password"]')
       .type('SuperSecretPassword!');
 
     //click on the button
@@ -66,7 +85,7 @@ describe('Login page', () => {
       .should('contain.text', 'You logged into a secure area!');
 
     //click on the button
-    cy.get('.radius')
+    cy.get('[href="/logout"]')
       .should('contain.text', 'Logout')
       .click();
 
