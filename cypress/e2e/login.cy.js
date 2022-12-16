e6b87fbd556ce0d443ba3a3ae624fd195d3d161e
WIN-BRNUOG3IOSI\Home <igorcurt@gmail.com>
2022-12-16 15:32:57 +0200

    simpleAuthE2e2

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -23,12 +23,29 @@ describe('Login page', () => {
     cy.get('[id="flash"]')
       .should('contain', ' You logged out of the secure area!');
   });
+  
 
-  it('Login with invalid creds', () => {
+  it('Login with invalid username', () => {
     const { email, password, username } = generateUser();
 
     cy.get('[id="username"]')
       .type(username);
+
+    cy.get('[id="password"]')
+      .type('SuperSecretPassword!');
+
+    cy.get('[type="submit"]').click();
+
+    cy.get('[id="flash"]')
+      .should('contain', 'Your username is invalid!');
+
+  });
+
+  it('Login with invalid password', () => {
+    const { email, password, username } = generateUser();
+
+    cy.get('[id="username"]')
+      .type('tomsmith');
 
     cy.get('[id="password"]')
       .type(password);
@@ -36,7 +53,7 @@ describe('Login page', () => {
     cy.get('[type="submit"]').click();
 
     cy.get('[id="flash"]')
-      .should('contain', 'Your username is invalid!');
+      .should('contain', 'Your password is invalid!');
 
   });
 });
