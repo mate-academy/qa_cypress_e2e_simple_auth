088d39582d3c16b7e87c4a8b1f07f9f5008501b9
Sergii Sinichenko <sinichenkosergey@gmail.com>
2022-12-16 10:32:32 +0200

    solution

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,30 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit('/login');
   });
 
-  it('', () => {
+  it('should login user with valid creds', () => {
     
+    cy.get ('[name="username"]').click().type('tomsmith')
+    cy.get('[name="password"]').click().type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get ('#flash').should('contain', 'You logged into a secure area!')
   });
-});
+
+  it('should not login user with invalid credentials', () => {
+
+    cy.get('[name="username"]').click().type('petrosmith');
+    cy.get('[name="password"]').click().type('SuperPassword!');
+    cy.get('.fa').click();
+  
+    cy.get('.flash').should('contain','Your username is invalid!')
+    });
+    it('user should be able to log out', () => {
+      cy.get('[name="username"]').click().type('tomsmith');
+      cy.get('[name="password"]').click().type('SuperSecretPassword!');
+      cy.get('.fa').click();
+      cy.contains('.icon-2x', 'Logout').click();
+      cy.get ('#flash').should('contain', 'You logged out of the secure area!')
+    });
+  });
