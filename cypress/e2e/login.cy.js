c70975c2cd0fd4e6396f573e7950378715625ec4
Oleksandra Vlasyuk <oleksandra.vl30@gmail.com>
2022-12-17 22:26:35 +0200

    Solution

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,79 @@
 /// <reference types="cypress" />
+const { generateUser } = require("../support/commands");
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit('https://the-internet.herokuapp.com/login')
   });
 
-  it('', () => {
-    
+  it.only('should provide an ability to login with valid creds', () => {
+    cy.get('#username')
+      .should('exist')
+      .type('tomsmith');
+
+    cy.get('#password')
+      .should('exist')
+      .type('SuperSecretPassword!');
+
+    cy.get('button').contains('Login')
+      .click();
+
+    cy.get('h4')
+      .should('contain', 'Welcome');
+
+    cy.get('#flash')
+      .should('contain', 'secure area');
+
+    cy.url()
+      .should('include', 'secure');
+  });
+
+  it.only('should provide a inability to login with invalid creds', () => {
+    cy.get('#username')
+    .should('exist')
+    .type('tomassmith');
+
+  cy.get('#password')
+    .should('exist')
+    .type('SuperSecretPassword!');
+
+  cy.get('button').contains('Login')
+    .click();
+
+    cy.get('#flash')
+      .should('contain', 'Your username is invalid!');
+
+      cy.get('#username')
+    .should('exist')
+    .type('tomsmith');
+
+  cy.get('#password')
+    .should('exist')
+    .type('SuperSuperSecretPassword!');
+
+  cy.get('button').contains('Login')
+    .click();
+
+    cy.get('#flash')
+      .should('contain', 'Your password is invalid!');
+  });
+
+  it.only('should be able to logout from the app', () => {
+    cy.get('#username')
+      .type('tomsmith');
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('button').contains('Login')
+      .click();
+
+    cy.get('[href="/logout"]')
+      .should('exist')
+      .should('contain', 'Logout')
+      .click();
+
+    cy.url()
+    .should('include', 'login');
   });
 });
