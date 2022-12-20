5926cc015bba9f9012d8f805237a8ffba855fc7d
Iryna Sulym <irinasulim4@gmail.com>
2022-12-20 13:14:27 +0100

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,8 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
     },
-   
 
+    
   },
 })
 
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,33 +2,41 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    cy.visit('https://the-internet.herokuapp.com/login')
-    
-  });
-    
-
-  it('Login with valid creds and logout', () => {
-    
-    cy.get('[id="username"]').type('tomsmith')
-    cy.get('[id="password"]').type('SuperSecretPassword!')
-    cy.get('[type="submit"]').click()
-
-    cy.url().should('contain','/secure')
-    cy.get('[id="flash"]').contains('You logged into a secure area!')
-
-    cy.get('[href="/logout"]').click()
-    cy.get('[id="flash"]').contains('You logged out of the secure area!')
-
-  });
-
-  it('Login with invalid creds', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+  }); 
   
-    cy.get('[id="username"]').type('invalid Username')
-    cy.get('[id="password"]').type('invalid Password')
-    cy.get('[type="submit"]').click()
+  it('should login with valid creds', () => {
+    
+    cy.get('[id="username"]').type('tomsmith');
+    cy.get('[id="password"]').type('SuperSecretPassword!');
+    cy.get('[type="submit"]').click();
 
-    cy.url().should('contain','/login')
-    cy.get('.flash.error').contains('Your username is invalid!')
+    cy.url().should('contain','/secure');
+    cy.get('[id="flash"]').contains('You logged into a secure area!');
 
   });
+
+  it('should login with invalid creds', () => {
+  
+    cy.get('[id="username"]').type('invalid Username');
+    cy.get('[id="password"]').type('invalid Password');
+    cy.get('[type="submit"]').click();
+
+    cy.url().should('contain','/login');
+    cy.get('.flash.error').contains('Your username is invalid!');
+
+  });
+
+  it('login user should be able logout from the app', () => {
+    cy.get('[id="username"]').type('tomsmith');
+    cy.get('[id="password"]').type('SuperSecretPassword!');
+    cy.get('[type="submit"]').click();
+
+    cy.url().should('contain','/secure');
+    cy.get('[id="flash"]').contains('You logged into a secure area!');
+
+    cy.get('[href="/logout"]').click();
+    cy.get('[id="flash"]').contains('You logged out of the secure area!');
+
+    });
 });
