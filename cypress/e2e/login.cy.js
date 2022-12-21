0ba40f2453292046a2eb6dc7d45d5df9fbb55513
Kateryna Kharytonova <katekharytonova@gmail.com>
2022-12-21 16:31:57 +0200

    resolution

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,7 +2,7 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    cy.visit('/login');
+  cy.visit('/login');
   });
 
   it('should login user with registerd credentials', () => {
@@ -17,17 +17,25 @@ describe('Login page', () => {
   it('should not login user with invalid credentials', () => {
 
   cy.get('[name="username"]').click().type('anasmith');
-  cy.get('[name="password"]').click().type('SuperPassword!');
+  cy.get('[name="password"]').click().type('SuperSecretPassword!');
   cy.get('.fa').click();
 
   cy.get('.flash').should('contain','Your username is invalid!')
+
+
+  cy.get('[name="username"]').click().type('tomsmith');
+  cy.get('[name="password"]').click().type('SuperPassword!');
+  cy.get('.fa').click();
+
+  cy.get('.flash').should('contain','Your password is invalid!')
   });
 
+
   it('user should be able to log out', () => {
-    cy.get('[name="username"]').click().type('tomsmith');
-    cy.get('[name="password"]').click().type('SuperSecretPassword!');
-    cy.get('.fa').click();
-    cy.contains('.icon-2x', 'Logout').click();
-    cy.get('.flash').should('contain','You logged out of the secure area!')
+  cy.get('#username').click().type('tomsmith');
+  cy.get('[name="password"]').click().type('SuperSecretPassword!');
+  cy.get('.fa').click();
+  cy.contains('.icon-2x', 'Logout').click();
+  cy.get('.flash').should('contain','You logged out of the secure area!')
   });
 });
