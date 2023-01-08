e5774bc265bdc9320ec162afd438c5f4cca3af29
Marina TORONYI <marina.toronyi>
2023-01-08 19:29:43 +0100

    Solution

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,71 @@
 /// <reference types="cypress" />
 
+
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit(`/login`);
   });
 
-  it('', () => {
+  it('should allow USER to Login with valid creds', () => {
+    cy.get('#username')
+      .type('tomsmith');
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
     
+    cy.contains('.radius', " Login")
+      .click();
+
+    cy.get('#flash')
+    .should('contain.text','You logged into a secure area!');
+
+    cy.assertUrl('secure'); 
+
+  });
+
+  it('should does not allow USER to Login with invalid creds', () => {
+
+    const randomNuber = Math.random().toString().slice(2,10);
+    const username = `test${randomNuber}`;
+    
+    const password = `${randomNuber}asd!Q`;
+
+    cy.get('#username')
+    .type(username);
+
+  cy.get('#password')
+    .type(password);
+  
+  cy.contains('.radius', " Login")
+    .click();
+
+    cy.assertUrl('login'); 
+
+    cy.get('#flash')
+    .should('contain.text',' Your username is invalid!');
+
+  });
+
+
+  it('should allow USER to Logout from the app', () => {   
+    cy.get('#username')
+    .type('tomsmith');
+
+  cy.get('#password')
+    .type('SuperSecretPassword!');
+  
+  cy.contains('.radius', " Login")
+    .click();
+
+    cy.contains('.icon-2x', 'Logout')
+      .click();
+
+    cy.assertUrl('login'); 
+
+    cy.get('#flash')
+    .should('contain.text',' You logged out of the secure area!');
+
+    cy.get('h2')
+      .should('contain.text','Login Page'); 
   });
 });
