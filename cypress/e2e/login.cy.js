21fed9bbad0515ca7ce40af6cb9f6c63ea1494f1
Anna Simko <108935534+Ann1120-ua@users.noreply.github.com>
2023-01-20 06:50:14 -0600

    test2

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,79 @@
 /// <reference types="cypress" />
 
+const { generateUser } = require("../support/generate");
+
 describe('Login page', () => {
+  const userId = 'tomsmith';
+  const password = 'SuperSecretPassword!';
+
   beforeEach(() => {
+    cy.visit('/login');
+  });
+
+  it('should login with valid credentials', () => {
+    
+    cy.get('#username')
+      .type(userId);
+    
+    cy.get('#password')
+      .type(password)
+
+      cy.contains('.fa', 'Login')
+        .click()
+      
+      cy.get('#flash')
+        .should('contain.text', 'You logged into a secure area!')
+  });
+
+  it('should not login with invalid username', () => {
+
+    cy.get('#username')
+    .type(userId + '123');
+  
+  cy.get('#password')
+    .type(password)
+
+    cy.contains('.fa', 'Login')
+      .click()
+    
+      cy.get('#flash')
+      .should('contain.text', 'Your username is invalid!')
     
   });
 
-  it('', () => {
+  it('should not login with invalid password', () => {
+
+    cy.get('#username')
+    .type(userId);
+  
+  cy.get('#password')
+    .type(password + '123') 
+
+    cy.contains('.fa', 'Login')
+      .click()
+    
+      cy.get('#flash')
+      .should('contain.text', 'Your password is invalid!')
     
   });
+
+  it('should be able to log out', () => {
+
+  cy.get('#username')
+    .type(userId);
+  
+  cy.get('#password')
+    .type(password)
+
+  cy.contains('.fa', 'Login')
+      .click()
+  
+  cy.contains('.icon-2x', 'Logout')
+    .click()
+
+  cy.get('#flash')
+    .should('contain.text','You logged out of the secure area!' )
+    
+       });
+
 });
