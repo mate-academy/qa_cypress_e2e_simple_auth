1ad8b57aeeb78aa2b1df6e187efcbd7a1bf05fe5
Bobuliak Viktor <bobuliakviktor@gmail.com>
2023-02-04 20:06:00 +0200

    Simple Auth Assertions

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,28 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
-    
+  it('Login Page, logged into a secure area', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
+  });
+
+  it('Login with invalid creds', () => {
+    cy.get('#username').type('invalid Username');
+    cy.get('#password').type('invalid Password');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain', 'Your username is invalid!');
+  });
+
+  it('Logout from the app', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('.button').click();
+    cy.get('#flash').should('contain','You logged out of the secure area!');
   });
 });
