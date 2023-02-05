f9c12393ec2db10a5670c6b467265926987ef64a
Yuliia Sokolova <yulyasokolova.lvov@gmail.com>
2023-02-05 19:42:02 +0200

    Solution

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,63 @@
 /// <reference types="cypress" />
 
 describe('Login page', () => {
+  const username = 'tomsmith'
+  const password = 'SuperSecretPassword!'
+
   beforeEach(() => {
-    
+    cy.visit('/login')
   });
 
-  it('', () => {
-    
+  it('should be able to visit login page', () => {
+    cy.url()
+      .should('include', '/login')
   });
+
+  it('should be able to login with valid creds', () => {
+    cy.get('[name="username"]')
+      .type(username)
+
+    cy.get('[name="password"]')
+      .type(password)
+
+    cy.contains('.fa', 'Login')
+      .click()
+      
+    cy.get('#flash')
+      .should('contain.text', 'You logged into a secure area')
+    });
+    
+  it('should not be able to login with invalid creds', () => {
+    cy.get('[name="username"]')
+      .type(username + '_invalid')
+
+    cy.get('[name="password"]')
+      .type(password + '_invalid')
+
+    cy.contains('.fa', 'Login')
+      .click()
+      
+    cy.get('#flash')
+      .should('contain.text', 'Your username is invalid!')
+    })
+
+      it('should be able to logout', () => {
+        cy.get('[name="username"]')
+          .type(username)
+    
+        cy.get('[name="password"]')
+          .type(password)
+    
+        cy.contains('.fa', 'Login')
+          .click()
+          
+        cy.get('#flash')
+          .should('contain.text', 'You logged into a secure area')
+           
+        cy.contains('.icon-2x', 'Logout')
+          .click()
+
+          cy.get('#flash')
+          .should('contain.text', 'You logged out of the secure area')
+          });
 });
