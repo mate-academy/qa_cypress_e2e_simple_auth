b5d2b8014fa1bc2ba5d25ac537b3c52c9c0dc6dd
Nataliia Khoruzhenko <natelis.nv@gmail.comm>
2023-02-06 18:23:17 +0200

    simple auth test added

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,39 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
-    
+  it('Login page. Login with valid creds', () => {
+    cy.get('#username')
+      .type('tomsmith');
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+    cy.get('.fa')
+      .click();
+    cy.get('#flash')
+      .should('contain', 'You logged into a secure area!');
+  });
+
+  it('Login page. Login with invalid creds', () => {
+    cy.get('#username')
+      .type('tomsmi');
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+    cy.get('.fa')
+      .click();
+    cy.get('#flash')
+      .should('contain', 'Your username is invalid!');
+  });
+
+  it('Login page. Logout from the app', () => {
+    cy.get('#username')
+      .type('tomsmith');
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+    cy.get('.fa')
+      .click();
+    cy.get('#flash')
+      .should('contain', 'You logged into a secure area!');
   });
 });
