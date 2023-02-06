cb5bacd9a7d32eed3d7c2cba650e29a823c959c4
yuliia <yuliiamyhashko@gmail.com>
2023-02-06 10:04:57 +0200

    Solution

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,23 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    // cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
-    
+  it('Invalid credentials', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('#username').type('tomsmith11');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain', 'Your username is invalid!');
   });
+  it('Log in and log out', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
+    cy.get('.icon-2x').click();
+    cy.get('#flash').should('contain', 'You logged out of the secure area!');
+});
 });
