c2adca3b73fc70ffbade73cabda754779c7d75bf
Triukhan <tryukhan.d@gmail.com>
2023-02-06 15:39:55 +0200

    Solution

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,41 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit('/login');
   });
 
-  it('', () => {
+  it('Should sign in with valid data', () => {
+    cy.get('#username').type('tomsmith');
     
+    cy.get('#password').type('SuperSecretPassword!');
+
+    cy.contains('.fa', 'Login').click();
+
+    cy.get('#flash').should('contain.text', 'You logged into a secure area!');
   });
+
+  it('Shouldn\'t sign in with invalid data', () => {
+    cy.get('#username').type('mishasmith');
+
+    cy.get('#password').type('SuperSecretPassword!');
+
+    cy.contains('.fa', 'Login').click();
+
+    cy.get('#flash').should('contain.text', 'Your username is invalid!');
+  });
+
+  it('Should log out from the app', () => {
+    cy.get('#username').type('tomsmith');
+    
+    cy.get('#password').type('SuperSecretPassword!');
+
+    cy.contains('.fa', 'Login').click();
+
+    cy.get('#flash').should('contain.text', 'You logged into a secure area!');
+
+    cy.contains('.button', 'Logout').click();
+
+    cy.get('#flash').should('contain.text', 'You logged out of the secure area!');
+  });
+
 });
