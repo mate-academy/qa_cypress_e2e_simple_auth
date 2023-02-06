bd4f0c0396b0a05dc83f1517d18186b163aae4e8
Solomia Biletska <solomiya100506@gmail.com>
2023-02-06 21:58:40 +0200

    Solution e2e_simple_auth

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,73 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit('https://the-internet.herokuapp.com/login')
   });
 
-  it('', () => {
-    
+  it('login with valid creds', () => {
+    cy.get('#username')
+      .type('tomsmith');
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.contains('.fa', 'Login')
+      .click();
+  });
+
+  it('logout', () => {
+    cy.get('#username')
+      .type('tomsmith');
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.contains('.fa', 'Login')
+      .click();
+
+    cy.contains('.button', 'Logout')
+      .click();
+  });
+
+  it('login with invalid username', () => {
+    cy.get('#username')
+      .type('tester333');
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.contains('.fa', 'Login')
+      .click();
+
+    cy.get('#flash')
+      .should('contain','Your username is invalid!');
+  });
+
+  it('login with invalid password', () => {
+    cy.get('#username')
+      .type('tomsmith');
+
+    cy.get('#password')
+      .type('invalidpassword');
+
+    cy.contains('.fa', 'Login')
+      .click();
+
+    cy.get('#flash')
+      .should('contain','Your password is invalid!');
+  });
+
+  it('login with invalid creds', () => {
+    cy.get('#username')
+      .type('tester333');
+
+    cy.get('#password')
+      .type('invalidpassword');
+
+    cy.contains('.fa', 'Login')
+      .click();
+
+    cy.get('#flash')
+      .should('contain','Your username is invalid!');
   });
 });
