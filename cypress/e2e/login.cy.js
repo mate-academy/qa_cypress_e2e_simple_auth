d16510c738e19ce30d127a27d4e34897cd47b4b9
Nataliia Khoruzhenko <natelis.nv@gmail.comm>
2023-02-07 09:12:40 +0200

    invalid creds check split

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -26,6 +26,17 @@ describe('Login page', () => {
     cy.get('#flash')
       .should('contain', 'Your username is invalid!');
   });
+  
+  it('Login page. Login with invalid creds', () => {
+    cy.get('#username')
+      .type('tomsmith');
+    cy.get('#password')
+      .type('invalid Password!');
+    cy.get('.fa')
+      .click();
+    cy.get('#flash')
+      .should('contain', 'Your password is invalid!');
+  });
 
   it('Login page. Logout from the app', () => {
     cy.get('#username')
@@ -34,7 +45,9 @@ describe('Login page', () => {
       .type('SuperSecretPassword!');
     cy.get('.fa')
       .click();
+    cy.get('.button')
+      .click();
     cy.get('#flash')
-      .should('contain', 'You logged into a secure area!');
+      .should('contain', 'You logged out of the secure area!');
   });
 });
