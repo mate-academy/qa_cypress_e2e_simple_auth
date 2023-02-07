0dd7cfdca2144b5c83fa611e7297efa64bdf4a7d
Oleksandr Udovichenko <oleksandr.dovichenko@gmail.com>
2023-02-07 14:22:51 +0200

    Solution Cypress simple auth after review

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,69 +5,66 @@ describe('Login page', () => {
     cy.visit(`https://the-internet.herokuapp.com/login`)
   });
 
-  it('The login page should be up and running', () => {
-    cy.url().should('eq', 'https://the-internet.herokuapp.com/login');
-  })
-
+  
   it('should provide an ability to log in with the valid creds and show validation message "You logged into a secure area!"', () => {
-    cy.get('input#username')
+    cy.get('[id=username]')
       .type(`tomsmith`)
 
-    cy.get('input#password')
+    cy.get('[id=password]')
       .type(`SuperSecretPassword!`)
 
-    cy.get('i.fa.fa-2x.fa-sign-in')
+    cy.get('.fa')
       .click()
 
-    cy.get('div.flash.success')
+    cy.get('[id=flash]')
       .should('contain.text', 'You logged into a secure area!')
 
   });
 
   it('should show validation message "Your username is invalid!" for the invalid userName', () => {
-    cy.get('input#username')
+    cy.get('[id=username]')
       .type(`tomsmith1`)
 
-    cy.get('input#password')
+    cy.get('[id=password]')
       .type(`SuperSecretPassword!`)
 
-    cy.get('i.fa.fa-2x.fa-sign-in')
+    cy.get('.fa')
       .click()
 
-    cy.get('div.flash.error')
+    cy.get('[id=flash]')
       .should('contain.text', 'Your username is invalid!')
 
   });  
 
   it('should show validation message "Your password is invalid!" for the invalid Password', () => {
-    cy.get('input#username')
+    cy.get('[id=username]')
       .type(`tomsmith`)
 
-    cy.get('input#password')
+    cy.get('[id=password]')
       .type(`SuperSecretPassword`)
 
-    cy.get('i.fa.fa-2x.fa-sign-in')
+    cy.get('.fa')
       .click()
 
-    cy.get('div.flash.error')
+    cy.get('[id=flash]')
       .should('contain.text', 'Your password is invalid!')
 
   });
 
   it('should provide an ability to log out and show validation message "You logged out of the secure area!"', () => {
-    cy.get('input#username')
+    cy.get('[id=username]')
       .type(`tomsmith`)
 
-    cy.get('input#password')
+    cy.get('[id=password]')
       .type(`SuperSecretPassword!`)
 
-    cy.get('i.fa.fa-2x.fa-sign-in')
+    cy.get('.fa')
       .click()
 
     cy.get('i.icon-2x.icon-signout')
       .click()
     
-    cy.get('div.flash.success')
+    cy.get('[id=flash]')
       .should('contain.text', 'You logged out of the secure area!')
     
 
