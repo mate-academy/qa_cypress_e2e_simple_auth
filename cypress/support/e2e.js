333d4e9d13bf7e4d18ce16c8e21326e941207b34
Yaroslav <adolas666@gmail.com>
2023-02-07 15:12:46 +0200

    removed requests loggin

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,5 +16,15 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+const app = window.top;
+if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
+  const style = app.document.createElement('style');
+  style.innerHTML =
+    '.command-name-request, .command-name-xhr { display: none }';
+  style.setAttribute('data-hide-command-log-request', '');
+
+  app.document.head.appendChild(style);
+};
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
