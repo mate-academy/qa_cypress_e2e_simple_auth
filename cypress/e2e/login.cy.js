7a4575c7b7931fa104e528b212b4f36b0ce81e0a
Nataliia Khoruzhenko <natelis.nv@gmail.comm>
2023-02-07 14:54:54 +0200

    test functions names fixed

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,48 +5,35 @@ describe('Login page', () => {
     cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('Login page. Login with valid creds', () => {
-    cy.get('#username')
-      .type('tomsmith');
-    cy.get('#password')
-      .type('SuperSecretPassword!');
-    cy.get('.fa')
-      .click();
+  it('should log in the user with valid credentials', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
     cy.get('#flash')
       .should('contain', 'You logged into a secure area!');
   });
 
-  it('Login page. Login with invalid creds', () => {
-    cy.get('#username')
-      .type('tomsmi');
-    cy.get('#password')
-      .type('SuperSecretPassword!');
-    cy.get('.fa')
-      .click();
+  it('should log in the user with invalid username', () => {
+    cy.get('#username').type('tomsmi');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
     cy.get('#flash')
       .should('contain', 'Your username is invalid!');
   });
-  
-  it('Login page. Login with invalid creds', () => {
-    cy.get('#username')
-      .type('tomsmith');
-    cy.get('#password')
-      .type('invalid Password!');
-    cy.get('.fa')
-      .click();
+
+  it('should log in the user with invalid password', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('invalid Password!');
+    cy.get('.fa').click();
     cy.get('#flash')
       .should('contain', 'Your password is invalid!');
   });
 
-  it('Login page. Logout from the app', () => {
-    cy.get('#username')
-      .type('tomsmith');
-    cy.get('#password')
-      .type('SuperSecretPassword!');
-    cy.get('.fa')
-      .click();
-    cy.get('.button')
-      .click();
+  it('should provide an ability to log out', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('.button').click();
     cy.get('#flash')
       .should('contain', 'You logged out of the secure area!');
   });
