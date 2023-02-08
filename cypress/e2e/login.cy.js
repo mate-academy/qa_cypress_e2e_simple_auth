2791627c9bae3a2239aad66b7ac35220cfff7964
Dmytro Kubarskyi <kubarskyidmytro@gmail.com>
2023-02-08 20:22:25 +0200

    solution

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,26 @@
 /// <reference types="cypress" />
 
 describe('Login page', () => {
-  beforeEach(() => {
-    
-  });
+
+  const validUserName = 'tomsmith';
+  const validPassword = 'SuperSecretPassword!';
+  const invalidUserName = 'intomsmith';
+  const invalidPassword = 'inSuperSecretPassword!';
 
   it('', () => {
+
+    cy.visit('/login');
+    cy.get('#username').type(invalidUserName);
+    cy.get('#password').type(invalidPassword);
+    cy.contains('.fa', 'Login').click();
+    cy.get('#flash').should('contain', 'Your username is invalid!');
     
+    cy.visit('/login');
+    cy.get('#username').type(validUserName);
+    cy.get('#password').type(validPassword);
+    cy.contains('.fa', 'Login').click();
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
+
+    cy.contains('.button', 'Logout').click();
   });
 });
