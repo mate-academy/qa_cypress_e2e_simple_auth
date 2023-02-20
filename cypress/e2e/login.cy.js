a828b0d15692950dfd4cf0915fd6c42d6c12bdcc
Andrii Dydyk <andriidydyk622@gmail.com>
2023-02-20 22:11:23 +0100

    Solution

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,76 @@
-/// <reference types="cypress" />
-
+/// <reference types = "cypress" />
+import { faker } from '@faker-js/faker';
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit('/login');
   });
 
-  it('', () => {
-    
+  it(`should contains main parts Login page`, () => {
+    cy.get('h2')
+      .should('contain.text', 'Login Page');
+
+    cy.get('#username')
+      .should('exist');
+
+    cy.get('#password')
+      .should('exist');
+
+    cy.contains('button', 'Login').should('exist');
+  });
+
+  it(`should allow to login with valid creds`, () => {
+    const validUsername = 'tomsmith';
+    const validPassword = 'SuperSecretPassword!'
+    cy.findById('username')
+      .type(validUsername);
+
+    cy.findById('password')
+      .type(validPassword);
+
+    cy.contains('button', 'Login')
+      .should('exist')
+      .click();
+
+    cy.findById('flash')
+      .should('contain.text', 'You logged into a secure area!');
+  });
+
+  it(`shouldn't allow to login with invalid creds`, () => {
+    const invalidUsername = faker.internet.userName();
+    const invalidPassword = faker.internet.password();
+    cy.findById('username')
+      .type(invalidUsername);
+
+    cy.findById('password')
+      .type(invalidPassword);
+
+    cy.contains('button', 'Login')
+      .should('exist')
+      .click();
+
+    cy.findById('flash')
+      .should('contain.text', 'Your username is invalid!');
+  });
+
+  it(`should allow to logout from the app`, () => {
+    const validUsername = 'tomsmith';
+    const validPassword = 'SuperSecretPassword!'
+    cy.findById('username')
+      .type(validUsername);
+
+    cy.findById('password')
+      .type(validPassword);
+
+    cy.contains('button', 'Login')
+      .should('exist')
+      .click();
+
+    cy.contains('a', 'Logout')
+      .should('exist')
+      .click();
+
+    cy.findById('flash')
+      .should('contain.text', 'You logged out of the secure area!');
+
   });
 });
