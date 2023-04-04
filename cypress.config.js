fe0102574571fd22d2036563f92319f5c6707372
Max Nikiforov <maxxnikiforov@gmail.com>
2023-04-04 18:38:25 +0700

    tests for log in, log out added

--- cypress.config.js
+++ cypress.config.js
@@ -4,5 +4,6 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
     },
+  baseUrl: 'https://the-internet.herokuapp.com',
   },
 })
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,54 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit('/login')
   });
 
-  it('', () => {
-    
+  it('should be able to log in with correct data and log out', () => {
+    cy.get('[name="username"]')
+      .type('tomsmith')
+
+    cy.get('[name="password"]')
+      .type('SuperSecretPassword!')
+
+    cy.contains('button', 'Login')
+      .click()
+
+    cy.get('h2')
+      .should('contain.text', 'Secure Area')
+
+    cy.contains('a', 'Logout')
+      .click()
+
+    cy.get('h2')
+      .should('contain.text', 'Login Page')
+  });
+
+  it('should be not able to log in with uncorrect name', () => {
+    cy.get('[name="username"]')
+      .type('tom')
+
+    cy.get('[name="password"]')
+      .type('SuperSecretPassword!')
+
+    cy.contains('button', 'Login')
+      .click()
+
+    cy.get('[id="flash"]')
+      .should('contain.text', 'Your username is invalid!')
+  });
+
+  it('should be not able to log in with uncorrect password', () => {
+    cy.get('[name="username"]')
+      .type('tomsmith')
+
+    cy.get('[name="password"]')
+      .type('Password!')
+
+    cy.contains('button', 'Login')
+      .click()
+
+    cy.get('[id="flash"]')
+      .should('contain.text', 'Your password is invalid!')
   });
 });
