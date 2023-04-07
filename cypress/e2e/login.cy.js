e96bf3ac698f12a641e5e091c0d61866a6caceb0
Yurii <dmukhailo@gmail.com>
2023-04-07 23:21:05 +0300

    Solution

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,27 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit("https://the-internet.herokuapp.com/login");
   });
 
-  it('', () => {
-    
+  it('login with valid inputs', () => {
+    cy.get("#username").type("tomsmith");
+    cy.get("#password").type("SuperSecretPassword!");
+    cy.get(".fa").click();
+    cy.get("#flash").should("include.text", " You logged into a secure area!");
+    cy.get(".button").click();
+    cy.get("#flash").should("include.text", " You logged out of the secure area!");
+  });
+  it('login with invalid username', () => {
+    cy.get("#username").type("tomsmith23");
+    cy.get("#password").type("SuperSecretPassword!");
+    cy.get(".fa").click();
+    cy.get("#flash").should("include.text", "Your username is invalid!");
+  });
+  it("login with invalid password", () => {
+    cy.get("#username").type("tomsmith");
+    cy.get("#password").type("SuperSecretPassword123!");
+    cy.get(".fa").click();
+    cy.get("#flash").should("include.text", "Your password is invalid!");
   });
 });
