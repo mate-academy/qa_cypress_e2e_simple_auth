fa14ac87e9bde9d0e5037e912c769c89b2c7e96c
LuxuriaCode <100286381+LuxuriaCode@users.noreply.github.com>
2023-04-11 16:20:38 +0300

    Login Cypress Test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,74 @@
-/// <reference types="cypress" />
+/// <reference types = 'cypress' />
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit('https://the-internet.herokuapp.com/login')
   });
 
-  it('', () => {
-    
+  it('should allow to login a user', () => {
+    cy.get('#username')
+      .type('tomsmith');
+
+    cy.get('input#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('.radius')
+      .should('contain.text', 'Login')
+      .click();
+
+    cy.get('#flash')
+      .should('contain.text', 'You logged into a secure area!');
+  });
+
+  it('should not allow to log in with invalid Username', () => {
+    cy.get('input#username')
+      .type('tomashgood');
+
+    cy.get('input#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('.radius')
+      .should('contain.text', 'Login')
+      .click();
+
+    cy.get('#flash')
+      .should('contain.text', 'Your username is invalid!');
+  });
+
+  it('should check that the user cannot log in with invalid Password', () => {
+    cy.get('input#username')
+      .type('tomsmith');
+
+    cy.get('input#password')
+      .type('Qwerty1234!');
+
+    cy.get('.radius')
+      .should('contain.text', 'Login')
+      .click();
+
+    cy.get('#flash')
+      .should('contain.text', 'Your password is invalid!');
+  });
+
+  it('should allow to log out a user', () => {
+    cy.get('input#username')
+      .type('tomsmith');
+
+    cy.get('input#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('.radius')
+      .should('contain.text', 'Login')
+      .click();
+
+    cy.get('#flash')
+      .should('contain.text', 'You logged into a secure area!');
+
+    cy.get('.button')
+      .should('contain.text', 'Logout')
+      .click();
+
+    cy.get('#flash')
+      .should('contain.text', 'You logged out of the secure area!');
   });
 });
