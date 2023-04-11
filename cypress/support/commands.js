cd405473b1e02f6471b853d7612dcf82332d0bee
Bohdan Karpenko <19bogdank99@gmail.com>
2023-04-11 14:31:07 +0300

    Homework

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,14 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('findById', (id) => {
+    cy.get(`[id="${id}"]`)
+});
+
+Cypress.Commands.add('assertMessage', (message) => {
+    cy.get('.flash')
+      .should('contain.text', `${message}`); 
+});
+
+Cypress.Commands.add('clickButton', (button) => {
+    cy.contains('.radius', `${button}`)  
+      .click();
+});
