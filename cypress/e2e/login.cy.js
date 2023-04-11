1d26ba025c31a44dcdb3da71594c8d226bd6cac0
Oleksandra <alexandra.brightsed@gmail.com>
2023-04-11 14:24:12 +0300

    e2e_simple_auth_solution

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,54 @@
 /// <reference types="cypress" />
 
 describe('Login page', () => {
+  const username = "tomsmith";
+  const password = "SuperSecretPassword!";
+
   beforeEach(() => {
-    
+  cy.visit("https://the-internet.herokuapp.com/login")
   });
 
-  it('', () => {
-    
+  it('it should allow user to log in', () => {
+    cy.get('#username').type(username);
+
+    cy.get('#password').type(password);
+
+    cy.get('.fa').should('contain.text', 'Login').click();
+
+    cy.get('#flash').should('contain.text', 'You logged into a secure area!');
   });
+
+  it('should not allow to log in with invalid username', () => {
+    cy.get('#username').type("invalidUserName");
+
+    cy.get('#password').type(password);
+
+    cy.get('.fa').should('contain.text', 'Login').click();
+
+    cy.get('#flash').should('contain.text', 'Your username is invalid!');
+  });
+
+  it('should not allow to log in with invalid password', () => {
+    cy.get('#username').type(username);
+
+    cy.get('#password').type("invalidPassword");
+
+    cy.get('.fa').should('contain.text', 'Login').click();
+
+    cy.get('#flash').should('contain.text', 'Your password is invalid!');
+  });
+
+  it('should allow to log out a user', () => {
+    cy.get('#username').type('tomsmith')
+
+    cy.get('#password').type('SuperSecretPassword!')
+    
+    cy.get('.fa').should('contain.text', 'Login').click()
+
+    cy.get('#flash').should('contain.text', 'You logged into a secure area!')
+
+    cy.get('.button').should('contain.text', 'Logout').click()
+
+    cy.get('#flash').should('contain.text', 'You logged out of the secure area!')
+  })
 });
