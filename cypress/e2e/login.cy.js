caa1598d4a52398b5f10bd5a9d97ca6eef6ca114
Maryna Oliinyk <ukrmartin22@gmail.com>
2023-04-11 21:18:38 +0300

    Add an assertion

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,21 +9,21 @@ describe('Login page', () => {
     cy.get('#username').type('tomsmith');
     cy.get('#password').type('SuperSecretPassword!');
     cy.contains('button', 'Login').click();
-    cy.contains('.subheader', 'Welcome to the Secure Area. When you are done click logout below.');
+    cy.contains('.subheader', 'Welcome to the Secure Area. When you are done click logout below.').should('exist');
   });
 
   it('Login with invalid Username', () => {
     cy.get('#username').type('invalidName');
     cy.get('#password').type('SuperSecretPassword!');
     cy.contains('button', 'Login').click();
-    cy.contains('.flash.error', 'Your username is invalid!');
+    cy.contains('.flash.error', 'Your username is invalid!').should('exist');
   });
 
   it('Login with invalid Password', () => {
     cy.get('#username').type('tomsmith');
     cy.get('#password').type('invalidPassword');
     cy.contains('button', 'Login').click();
-    cy.contains('.flash.error', 'Your password is invalid!');
+    cy.contains('.flash.error', 'Your password is invalid!').should('exist');
   });
 
   it('Logout from the app', () => {
@@ -32,6 +32,6 @@ describe('Login page', () => {
     cy.contains('button', 'Login').click();
     cy.contains('.subheader', 'Welcome to the Secure Area. When you are done click logout below.');
     cy.contains('.icon-2x.icon-signout', 'Logout').click();
-    cy.contains('#flash', 'You logged out of the secure area!');
+    cy.contains('#flash', 'You logged out of the secure area!').should('exist');
   });
 });
