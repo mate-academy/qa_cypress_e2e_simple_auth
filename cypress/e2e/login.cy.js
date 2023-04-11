64de0bd613a8c1f96b4c7def48a6d66d0e699d0c
Kristina Kulakivska <kristina.kuprin@gmail.com>
2023-04-11 18:17:09 +0300

    Testing simple_auth

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,27 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit('https://the-internet.herokuapp.com/login')
   });
 
-  it('', () => {
-    
+  it('login with valid data', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.radius').click();
+    cy.get('h4').should('contain.text', 'Welcome to the Secure Area');
+  });
+  it('login with not valid data', () => {
+    cy.get('#username').type('invalid Username');
+    cy.get('#password').type('invalid Password');
+    cy.get('.radius').click();
+    cy.get('#flash.error').should('contain.text', 'Your username is invalid!');
+  });
+  it('logout', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.radius').click();
+    cy.get('h4').should('contain.text', 'Welcome to the Secure Area');
+    cy.get('.button.secondary.radius').click();
+    cy.get('.flash.success').should('contain.text', 'You logged out of the secure area!');
   });
 });
