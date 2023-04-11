d4236dae4548e8903665683c3ef2f714c7b4ab35
Maryna Iziumova <marina.omelyanenko.96@gmail.com>
2023-04-11 14:17:29 +0200

    Login3

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,23 +7,30 @@ describe('Login page', () => {
     cy.visit('/login')
   });
 
-  it('Login with valid creds', () => {
+  it('Login with valid data', () => {
     cy.get('#username').type('tomsmith');
     cy.get('#password').type(password);
     cy.get('.fa.fa-2x.fa-sign-in').click();
     cy.get('.flash.success').should('contain.text', 'You logged into a secure area!');
     cy.get('.button.secondary.radius').click();
     cy.get('.flash.success').should('contain.text', 'You logged out of the secure area!');
-    cy.get('[id="username"]').type('tomsmith123');
-    cy.get('[id="password"]').type(password);
-    cy.get('.fa.fa-2x.fa-sign-in').click();
-    cy.get('.flash.error').should('contain.text', 'Your username is invalid!');
+  
   });
 
-  it('Login with invalid creds', () => {
+  it('Login with invalid data', () => {
     cy.get('#username').type('tomsmith123');
     cy.get('#password').type(password);
     cy.get('.fa.fa-2x.fa-sign-in').click();
     cy.get('.flash.error').should('contain.text', 'Your username is invalid!');
+  });
+
+  it('Logg out', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type(password);
+    cy.get('.fa.fa-2x.fa-sign-in').click();
+    cy.get('.flash.success').should('contain.text', 'You logged into a secure area!');
+    cy.get('.button.secondary.radius').click();
+    cy.get('.flash.success').should('contain.text', 'You logged out of the secure area!');
+    
   });
 });
