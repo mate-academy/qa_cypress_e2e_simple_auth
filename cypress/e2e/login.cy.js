cf9c8b41f7ff0578b2de001cb29e60143a817742
Angelina Pavlushenko <perfectstranger355@gmail.com>
2023-04-11 15:51:36 +0300

    Solution Simple_auth

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,37 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit('https://the-internet.herokuapp.com/login')
   });
 
-  it('', () => {
-    
+  it('should let a user to log in', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.radius').click();
+    cy.get('#flash')
+      .should('contain.text', "You logged into a secure area!");
+  });
+
+  it('should not let a user to log in', () => {
+    cy.get('#username').type('notomsmith');
+    cy.get('#password').type('noSuperSecretPassword!');
+    cy.get('.radius').click();
+    cy.get('#flash')
+      .should('contain.text', "Your username is invalid!");
+  });
+
+  it('should let a user to log in', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.radius').click();
+    cy.get('#flash')
+      .should('contain.text', "You logged into a secure area!");
+  });
+
+  it('should let a user to log out', () => {
+    cy.url()
+      .should('include', '/login');
+    cy.get('.fa')
+      .should('contain.text', 'Login');
   });
 });
