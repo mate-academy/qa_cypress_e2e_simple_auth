c4f34fc337e4d69712b09cbd49fcda42702e19bd
HalynaKomar <komarg007@gmail.com>
2023-04-11 15:28:16 +0300

    Cypress: Simple Auth Assertions Halyna Komar

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,29 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit(`https://the-internet.herokuapp.com/login`)
   });
 
-  it('', () => {
-    
+  it('successfully logs in with valid credentials', () => {
+    cy.get('#username').type(`tomsmith`);
+    cy.get('#password').type(`SuperSecretPassword!`);
+    cy.get('.radius').click();
+    cy.contains('You logged into a secure area!').should('be.visible');
+  });
+
+  it('logs in with invalid credentials validation errors', () => {
+    cy.get('#username').type(`tomsmith22`);
+    cy.get('#password').type(`SuperSecretPassword!22`);
+    cy.get('.radius').click();
+    cy.contains('Your username is invalid!').should('be.visible');
+  });
+
+  it('successfully logs in with valid credentials', () => {
+    cy.get('#username').type(`tomsmith`);
+    cy.get('#password').type(`SuperSecretPassword!`);
+    cy.get('.radius').click();
+    cy.contains('You logged into a secure area!').should('be.visible');
+    cy.get('.button').click();
+    cy.contains(' You logged out of the secure area!').should('be.visible');
   });
 });
