cc1e436a7546b77d84619e7ec97f833f7166cc0d
Maryna Iziumova <marina.omelyanenko.96@gmail.com>
2023-04-11 12:13:26 +0200

    Login2

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,8 +8,8 @@ describe('Login page', () => {
   });
 
   it('Login with valid creds', () => {
-    cy.get('[id="username"]').type('tomsmith');
-    cy.get('[id="password"]').type(password);
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type(password);
     cy.get('.fa.fa-2x.fa-sign-in').click();
     cy.get('.flash.success').should('contain.text', 'You logged into a secure area!');
     cy.get('.button.secondary.radius').click();
@@ -21,8 +21,8 @@ describe('Login page', () => {
   });
 
   it('Login with invalid creds', () => {
-    cy.get('[id="username"]').type('tomsmith123');
-    cy.get('[id="password"]').type(password);
+    cy.get('#username').type('tomsmith123');
+    cy.get('#password').type(password);
     cy.get('.fa.fa-2x.fa-sign-in').click();
     cy.get('.flash.error').should('contain.text', 'Your username is invalid!');
   });
