fd307b48f442b42ce184b14ab070653675c51c83
Ihor Huliaiev <guliver2410@gmail.com>
2023-04-12 15:37:33 +0300

    Made changes to qa_cypress_e2e_simple_auth

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://the-internet.herokuapp.com',
     setupNodeEvents(on, config) {
     },
   },
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,7 +6,7 @@ const password = 'SuperSecretPassword!';
 
 describe('Login page', () => {
   beforeEach(() => {
-    cy.visit('https://the-internet.herokuapp.com/login')
+    cy.visit('/login')
   });
 
   it('Login with valid creds', () => {
