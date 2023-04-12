2d8443eb554bd9af12ad1d944f6822f83ed14f67
Angelina Pavlushenko <perfectstranger355@gmail.com>
2023-04-12 21:09:32 +0300

    New simple auth

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -11,28 +11,23 @@ describe('Login page', () => {
     cy.get('.radius').click();
     cy.get('#flash')
       .should('contain.text', "You logged into a secure area!");
+    cy.get('.icon-2x.icon-signout').click();
+    cy.get("#flash").should("include.text", "You logged out of the secure area!");
   });
 
-  it('should not let a user to log in', () => {
+  it('should not let a user to log in with invalid username', () => {
     cy.get('#username').type('notomsmith');
-    cy.get('#password').type('noSuperSecretPassword!');
+    cy.get('#password').type('SuperSecretPassword!');
     cy.get('.radius').click();
     cy.get('#flash')
       .should('contain.text', "Your username is invalid!");
   });
 
-  it('should let a user to log in', () => {
+  it('should not let a user to log in with invalid password', () => {
     cy.get('#username').type('tomsmith');
-    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('#password').type('noSuperSecretPassword!');
     cy.get('.radius').click();
     cy.get('#flash')
-      .should('contain.text', "You logged into a secure area!");
-  });
-
-  it('should let a user to log out', () => {
-    cy.url()
-      .should('include', '/login');
-    cy.get('.fa')
-      .should('contain.text', 'Login');
+      .should('contain.text', "Your password is invalid!");
   });
 });
