780da208af70bb1847f74bb65c597da2eff3c39d
Anastasiia Stelmakh <s.anastasiia@ukr.net>
2023-04-12 16:07:03 +0300

    Second task

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,28 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
-    
-  });
+  it('should provide an ability to log in', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.radius').click();
+    cy.get('h4.subheader').should('contains.text', 'Welcome');
+     });
+
+  it('should display a validation message if creds are invalid', () => {
+    cy.get('#username').type('Anastasiia');
+    cy.get('#password').type('Stelmakh');
+    cy.get('.radius').click();
+    cy.get('.flash.error').should('contains.text', 'invalid');
+      });   
+
+  it('should provide an ability to log out for logged in users', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.radius').click();
+    cy.get('.icon-2x.icon-signout').click();
+    cy.get('.flash.success').should('contains.text', 'You logged out');
+      });        
 });
