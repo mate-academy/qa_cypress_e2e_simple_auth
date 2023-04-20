b219ad6d96b4aad14ac367fde476779ee92a675f
Vladyslav Hobysh <lampachilo@gmail.com>
2023-04-20 17:03:57 +0300

    simple auth assertions

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -14,11 +14,13 @@ it('Login with invalid creds (Username)', () => {
    cy.get('#username').type('amogsus');
    cy.get('#password').type('SuperSecretPassword!');
    cy.contains('button', 'Login').click();
+   cy.get('#flash').should('contains.text', 'Your username is invalid!');
 })
 it('Login with invalid creds (Password)', () => {
    cy.get('#username').type('tomsmith');
    cy.get('#password').type('amogsus');
    cy.contains('button', 'Login').click();
+   cy.get('#flash').should('contains.text','Your password is invalid!');
 })
 it('Sign out after logged in', () => {
    cy.get('#username').type('tomsmith');
