381d7afd985ad6c0805a5857291eaa10b9b023a1
Vitalia <89596289+VitaDemian@users.noreply.github.com>
2023-05-30 15:49:07 +0300

    Solution

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,52 @@
 
 describe('Login page', () => {
   beforeEach(() => {
+    cy.visit('https://the-internet.herokuapp.com/login')
     
   });
 
-  it('', () => {
-    
+  it('Login in with valid creds', () => {
+    cy.contains('Login Page');
+    cy.contains('This is where you can log into the secure area. Enter tomsmith for the username and SuperSecretPassword! for the password. If the information is wrong you should see error messages.');
+    cy.get('#username')
+      .type('tomsmith');
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+    cy.get('.fa-sign-in')
+      .click();
+
+    cy.get('#flash')
+      .should('contain', ' You logged into a secure area!')
   });
+
+  it('Login with invalid creds', () => {
+    cy.contains('Login Page');
+    cy.contains('This is where you can log into the secure area. Enter tomsmith for the username and SuperSecretPassword! for the password. If the information is wrong you should see error messages.');
+    cy.get('#username')
+      .type('tomsmith4');
+    cy.get('#password')
+      .type('SuperSecretPassword!4');
+    cy.get('.fa-sign-in')
+      .click();
+
+    cy.get('#flash')
+      .should('contain', 'Your username is invalid!')
+
+  })
+
+  it('Loguot from the app', () => {
+    cy.contains('Login Page');
+    cy.contains('This is where you can log into the secure area. Enter tomsmith for the username and SuperSecretPassword! for the password. If the information is wrong you should see error messages.');
+    cy.get('#username')
+      .type('tomsmith');
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+    cy.get('.fa-sign-in')
+      .click();
+    cy.get('.icon-signout')
+      .click()
+    cy.get('#flash')
+      .should('contain', ' You logged out of the secure area!');
+
+  })
 });
