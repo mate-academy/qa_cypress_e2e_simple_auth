a1f65a007fdde1ab9a4eb98ba4b737b274f1c1c6
Olga Akimenko <olga.katsion@gmail.com>
2023-05-30 13:53:28 +0300

    Auto tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,28 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit('https://the-internet.herokuapp.com/login')
   });
 
-  it('', () => {
-    
+  it('Login with valid creds', () => {
+    cy.get('#username').type('tomsmith')
+    cy.get('#password').type('SuperSecretPassword!')
+    cy.get('.fa-sign-in').click()
+    cy.contains('Welcome to the Secure Area').should('be.visible')
+  });
+
+  it('Login with invalid creds', () => {
+    cy.get('#username').type('invalidUsername')
+    cy.get('#password').type('invalidPassword')
+    cy.get('.fa-sign-in').click()
+    cy.contains('Your username is invalid!').should('be.visible')
+  });
+
+  it('Logout from the app', () => {
+    cy.get('#username').type('tomsmith')
+    cy.get('#password').type('SuperSecretPassword!')
+    cy.get('.fa-sign-in').click()
+    cy.get('.icon-2x.icon-signout').click()
+    cy.contains('You logged out of the secure area!').should('be.visible')
   });
 });
