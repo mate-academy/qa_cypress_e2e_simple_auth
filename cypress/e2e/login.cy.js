4e9139cc26d278f4263bb1f516ce2b7f6d7ceec2
Roman Vytrykush <qwertywo2000@gmail.com>
2023-05-30 16:44:54 +0300

    new update

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,10 @@
 /// <reference types="cypress" />
 
 const baseURL = 'https://the-internet.herokuapp.com/login';
+const username = 'tomsmith';
+const password = 'SuperSecretPassword!';
+const invalidUsername = 'trustMe';
+const invalidPassword = '30914';
 
 describe('Login page', () => {
   beforeEach(() => {
@@ -8,6 +12,58 @@ describe('Login page', () => {
   });
 
   it('Should log in with valid credentials', () => {
-    
+    cy.get('#username')
+    .clear()
+    .type(username);
+
+    cy.get('#password')
+    .clear
+    .type(password);
+
+    cy.get('[type = submit]')
+    .click();
+
+    cy.get('#flash')
+    .contains('You logged into a secure area!')
+  });
+
+  it('Should be able to logout', () => {
+    cy.get('.button')
+    .click();
+
+    cy.get('#flash')
+    .contains('You logged out of the secure area!');
+  });
+
+  it('Should not allow to log in with invalid username', () => {
+    cy.get('#username')
+    .clear()
+    .type(invalidUsername);
+
+    cy.get('#password')
+    .clear
+    .type(password);
+
+    cy.get('[type = submit]')
+    .click();
+
+    cy.get('#flash')
+    .contains('Your username is invalid!');
+  });
+
+  it('Should not allow to log in with invalid password', () => {
+    cy.get('#username')
+    .clear()
+    .type(username);
+
+    cy.get('#password')
+    .clear
+    .type(invalidPassword);
+
+    cy.get('[type = submit]')
+    .click();
+
+    cy.get('#flash')
+    .contains('Your password is invalid!');
   });
 });
