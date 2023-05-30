788b2fd6151013ae245aaacb5b5d8cab0133ef0a
Iryna <novydom23@gmail.com>
2023-05-30 14:41:26 +0300

    Login and Logout Test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,36 @@
 /// <reference types="cypress" />
 
-describe('Login page', () => {
+describe('Login with valid creds', () => {
   beforeEach(() => {
+    cy.visit('https://the-internet.herokuapp.com/login')
+    .should('exist')
+    cy.get('#username')
+    .type('tomsmith')
+    cy.get('#password')
+    .type('SuperSecretPassword!')
+    cy.get('.fa.fa-2x.fa-sign-in')
+    .should('contain','Login')
+    .click();
     
+    cy.get('#flash')
+    .should('contain','You logged into a secure area!')
+    cy.get('[href="/logout"]')
+    .click();
+  })
+  it('Logout from the app', () => {
+    cy.get('#flash.flash.success')
+    .should('contain','You logged out of the secure area!')
   });
+  it('Login with invalid creds', () => {
+    cy.get('#username')
+    .type('tomsmith1')
+    cy.get('#password')
+    .type('SuperSecretPassword1!')
 
-  it('', () => {
-    
+    cy.get('.fa.fa-2x.fa-sign-in')
+    .should('contain','Login')
+    .click()
+    cy.get('#flash.flash.error')
+    .should('contain','Your username is invalid!');
   });
 });
