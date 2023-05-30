28fec840b2a41d5f1e92ad0f413b19e94ef705c6
Viktoriia <volvik24@gmail.com>
2023-05-30 14:54:17 +0300

    solution

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,30 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
-    
+  it('should provide an ability to register with valid credentials', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.contains('button.radius', 'Login').click();
+    cy.contains('#flash-messages', 'You logged into a secure area!');
+  });
+
+  it('should not provide an ability to register with invalid credentials', () => {
+    cy.get('#username').type('some_username');
+    cy.get('#password').type('Some_password!');
+    cy.contains('button.radius', 'Login').click();
+    cy.contains('#flash-messages', 'Your username is invalid!');
+  });
+
+  it.only('should not provide an ability to logout', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.contains('button.radius', 'Login').click();
+    cy.contains('#flash-messages', 'You logged into a secure area!');
+
+    cy.get('[href="/logout"]').click();
+    cy.contains('#flash-messages', 'You logged out of the secure area!');
   });
 });
