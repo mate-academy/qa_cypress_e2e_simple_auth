38fa3857fc929b96b79051b283681c44dcb86973
Roksolana Skrypets <roksolana.skrypets.mpvpr.2021@lpnu>
2023-05-30 14:27:29 +0300

    Solution (login)

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,51 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit('https://the-internet.herokuapp.com/login');
+  });
+ 
+  it('should login with valid creds', () => {
+    cy.get('[name="username"]')
+      .type('tomsmith');
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.contains('.fa', 'Login')
+      .click();
+
+    cy.get('#flash')
+      .should('contain', 'You logged into a secure area!');
   });
 
-  it('', () => {
-    
+  it('should fail login with invalid creds', () => {
+    cy.get('#username')
+      .type('InvalidUsername');
+
+    cy.get('#password')
+      .type('InvalidPassword');
+
+    cy.contains('.fa', 'Login')
+      .click();
+
+    cy.get('#flash')
+      .should('contain', 'Your username is invalid!');
+  });
+
+  it('should logout', () => {
+    cy.get('#username')
+      .type('tomsmith');
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.contains('.fa', 'Login')
+      .click();
+
+    cy.contains('.button', 'Logout')
+      .click();
+
+    cy.get('#flash')
+      .should('contain', 'You logged out of the secure area!');
   });
 });
