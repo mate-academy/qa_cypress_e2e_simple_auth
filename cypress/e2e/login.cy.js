f830dd57ffed368ded577872d87cb4dbf67214f6
Kyrylo Aleinykov <kyrryloaleynikov@gmail.com>
2023-05-30 14:03:44 +0300

    Simple_Auth_Aleinykov

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,79 @@
 
 describe('Login page', () => {
   beforeEach(() => {
-    
+    cy.visit({url:'https://the-internet.herokuapp.com/login', method:'GET'})
   });
 
-  it('', () => {
-    
+  it('Login with valid creds and assert you successful logged in', () => {
+    cy.get('#username')
+      .type('tomsmith')
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('.radius').contains('Login')
+      .click();
+
+    cy.get('h2')
+      .should('contain', 'Secure Area');
+
+    cy.get('#flash')
+      .should('contain', 'You logged into a secure area!');
+      
   });
+
+  it('Log in with invalid username', () => {
+
+    cy.get('#username')
+      .type('Invalidtomsmith')
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('.radius').contains('Login')
+      .click();
+    
+    cy.get('#flash')
+      .should('contain', 'Your username is invalid!');
+    
+});
+
+it('Log in with invalid password', () => {
+
+  cy.get('#username')
+    .type('tomsmith')
+
+  cy.get('#password')
+    .type('InvalidSuperSecretPassword!');
+
+  cy.get('.radius').contains('Login')
+    .click();
+  
+  cy.get('#flash')
+    .should('contain', 'Your password is invalid!');
+});
+
+it('Logout from account with asser about successful logout', () => {
+
+  cy.get('#username')
+      .type('tomsmith')
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('.radius').contains('Login')
+      .click();
+
+    cy.get('h2')
+      .should('contain', 'Secure Area')
+
+    cy.get('#flash')
+      .should('contain', 'You logged into a secure area!');
+
+    cy.contains('.button', 'Logout')
+      .click();
+
+    cy.get('#flash')
+      .should('contain', 'You logged out of the secure area!');
+});
 });
