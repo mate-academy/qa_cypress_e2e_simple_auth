9da5f3ca2a56cf01c7eec5435265a80fd40b9c2d
Viktoriia <volvik24@gmail.com>
2023-05-30 15:02:11 +0300

    some changes

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -12,11 +12,18 @@ describe('Login page', () => {
     cy.contains('#flash-messages', 'You logged into a secure area!');
   });
 
-  it('should not provide an ability to register with invalid credentials', () => {
+  it('should not provide an ability to register with invalid username', () => {
     cy.get('#username').type('some_username');
-    cy.get('#password').type('Some_password!');
+    cy.get('#password').type('SuperSecretPassword!');
     cy.contains('button.radius', 'Login').click();
     cy.contains('#flash-messages', 'Your username is invalid!');
+  });
+
+  it('should not provide an ability to register with invalid password', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('Some_password!');
+    cy.contains('button.radius', 'Login').click();
+    cy.contains('#flash-messages', 'Your password is invalid!');
   });
 
   it.only('should not provide an ability to logout', () => {
