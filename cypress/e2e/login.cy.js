511bcff6f123394e6283d8e0037944fd2421d4ed
Iryna <novydom23@gmail.com>
2023-05-31 09:44:07 +0300

    Fix Test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,34 +3,42 @@
 describe('Login with valid creds', () => {
   beforeEach(() => {
     cy.visit('https://the-internet.herokuapp.com/login')
-    .should('exist')
     cy.get('#username')
     .type('tomsmith')
     cy.get('#password')
     .type('SuperSecretPassword!')
-    cy.get('.fa.fa-2x.fa-sign-in')
-    .should('contain','Login')
+    cy.contains('button','Login')
     .click();
     
-    cy.get('#flash')
-    .should('contain','You logged into a secure area!')
-    cy.get('[href="/logout"]')
-    .click();
+    cy.contains('You logged into a secure area!')
+     .should('be.visible');
+    
   })
-  it('Logout from the app', () => {
-    cy.get('#flash.flash.success')
-    .should('contain','You logged out of the secure area!')
-  });
+ 
   it('Login with invalid creds', () => {
+    cy.visit('https://the-internet.herokuapp.com/login')
     cy.get('#username')
     .type('tomsmith1')
     cy.get('#password')
     .type('SuperSecretPassword1!')
 
-    cy.get('.fa.fa-2x.fa-sign-in')
-    .should('contain','Login')
+    cy.contains('button','Login')
     .click()
-    cy.get('#flash.flash.error')
-    .should('contain','Your username is invalid!');
+    cy.contains('Your username is invalid!')
+    .should('be.visible');
   });
-});
+
+    it('Logout from the app', () => {
+      cy.visit('https://the-internet.herokuapp.com/login')
+      cy.get('#username')
+      .type('tomsmith')
+      cy.get('#password')
+      .type('SuperSecretPassword!')
+      cy.contains('button','Login')
+      .click();
+      cy.get('[href="/logout"]')
+    .click();
+      cy.contains('You logged out of the secure area!')
+      .should('be.visible')
+    });
+  });
