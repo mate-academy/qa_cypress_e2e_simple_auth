63fd20d4dd37dbffc4431403fce854fd3e15c24a
Sofiia Katiukha <katuhasofia10@gmail.com>
2023-07-14 18:49:34 +0300

    Solution cypress selectors

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,62 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  beforeEach(() => {
+  const username = 'tomsmith';
+  const password = 'SuperSecretPassword!';
 
+  beforeEach(() => {
+    cy.visit('/login');
+
+    cy.url().should('include', '/login');
   });
 
-  it('', () => {
+  it('should allows to log in with valid credentials', () => {
+    cy.get('#username').type(username);
 
+    cy.get('#password').type(password);
+
+    cy.contains('.radius', 'Login').click();
+
+    cy.get('#flash')
+      .should('contains.text', 'You logged into a secure area!');
+
+    cy.url().should('include', '/secure');
+  });
+
+  it('should not allow to log in with invalid Username', () => {
+    cy.get('#username').type(username + 'q');
+
+    cy.get('#password').type(password);
+
+    cy.contains('.radius', 'Login').click();
+
+    cy.get('#flash')
+      .should('contains.text', 'Your username is invalid!');
+  });
+
+  it('should not allow to log in with invalid Password', () => {
+    cy.get('#username').type(username);
+
+    cy.get('#password').type(password + '0');
+
+    cy.contains('.radius', 'Login').click();
+
+    cy.get('#flash')
+      .should('contains.text', 'Your password is invalid!');
+  });
+
+  it.only('should allows to logout', () => {
+    cy.get('#username').type(username);
+
+    cy.get('#password').type(password);
+
+    cy.contains('.radius', 'Login').click();
+
+    cy.contains('.button', 'Logout').click();
+
+    cy.get('#flash')
+      .should('contains.text', 'You logged out of the secure area!');
+
+    cy.url().should('include', '/login');
   });
 });
