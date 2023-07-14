86f913b7cce7ce5427f1eb58a5235cf6c7d21b98
Valerij Kozyrenko <devilofwater777@gmail.com>
2023-07-14 14:19:50 +0300

    SolutionSA2

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('cypress');
-
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://the-internet.herokuapp.com',
--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,14 +1,17 @@
 /// <reference types="cypress" />
 
 describe('Login page', () => {
+  const username = 'tomsmith';
+  const password = 'SuperSecretPassword!';
+
   beforeEach(() => {
     cy.visit('/login');
     cy.url().should('include', '/login');
   });
 
   it('should allow to log in a user', () => {
-    cy.get(`#username`).type(`tomsmith`);
-    cy.get(`#password`).type(`SuperSecretPassword!` + `{enter}`);
+    cy.get(`#username`).type(username);
+    cy.get(`#password`).type(password + `{enter}`);
     cy.get(`h2`).should(`contain.text`, ` Secure Area`);
     cy.get(`h4`).should(`contain.text`, `Welcome to the Secure Area. When you are done click logout below.`);
     cy.get(`i.icon-2x.icon-signout`).should(`be.visible`);
@@ -16,21 +19,21 @@ describe('Login page', () => {
 
   it('should not allow to log in with invalid Username', () => {
     cy.get(`#username`).type(`invalid`);
-    cy.get(`#password`).type(`SuperSecretPassword!` + `{enter}`);
+    cy.get(`#password`).type(password + `{enter}`);
     cy.get(`#flash`).should(`contain.text`, `Your username is invalid!`);
     cy.get(`h2`).should(`contain.text`, `Login Page`);
   });
 
   it('should not allow to log in with invalid Password', () => {
-    cy.get(`#username`).type(`tomsmith`);
+    cy.get(`#username`).type(username);
     cy.get(`#password`).type(`invalidPassword1!` + `{enter}`);
     cy.get(`#flash`).should(`contain.text`, `Your password is invalid!`);
     cy.get(`h2`).should(`contain.text`, `Login Page`);
   });
 
   it('should allow to log out a user', () => {
-    cy.get(`#username`).type(`tomsmith`);
-    cy.get(`#password`).type(`SuperSecretPassword!` + `{enter}`);
+    cy.get(`#username`).type(username);
+    cy.get(`#password`).type(password + `{enter}`);
     cy.get(`h2`).should(`contain.text`, ` Secure Area`);
     cy.get(`h4`).should(`contain.text`, `Welcome to the Secure Area. When you are done click logout below.`);
     cy.get(`i.icon-2x.icon-signout`).should(`be.visible`);
