773b8796588d63ea49ec0caaabc3a20875c5d22e
Oleksandr Melnyk <snchkmelnyk@gmail.com>
2023-07-14 19:46:41 +0300

    Add automated tests for sign-in flow new -solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,14 +1,14 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  const username = 'tomsmith';
+  const password = 'SuperSecretPassword!';
+
   beforeEach(() => {
     cy.visit('https://the-internet.herokuapp.com/login');
   });
 
   it('should log in with valid credentials', () => {
-    const username = 'tomsmith';
-    const password = 'SuperSecretPassword!';
-
     cy.get('#username').type(username);
     cy.get('#password').type(password);
     cy.get('button[type="submit"]').click();
@@ -16,18 +16,15 @@ describe('Sign In page', () => {
   });
 
   it('should show validation errors with invalid credentials', () => {
-    const username = 'invalid_username';
-    const password = 'invalid_password';
-
-    cy.get('#username').type(username);
-    cy.get('#password').type(password);
+    cy.get('#username').type(`${username}+1`);
+    cy.get('#password').type(`${password}+r`);
     cy.get('button[type="submit"]').click();
     cy.get('#flash').should('contain', 'Your username is invalid!');
   });
 
   it('should show validation errors with invalid credentials', () => {
-    cy.get('#username').type('invalid_username');
-    cy.get('#password').type('invalid_password');
+    cy.get('#username').type(`${username}+1`);
+    cy.get('#password').type(`${password}+r`);
     cy.get('button[type="submit"]').click();
     cy.get('#flash').should('contain', 'Your username is invalid!');
   });
