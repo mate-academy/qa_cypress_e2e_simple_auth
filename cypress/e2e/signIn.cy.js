e1a388eb8942aaa3036b64dac3f322ac2b08e610
Vasyl <vasylsymets@gmail.com>
2023-07-14 21:35:05 +0300

    Solution2test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,31 +1,42 @@
-/// <reference types="cypress" />
+/// <reference types='cypress' />
 
 describe('Sign In page', () => {
+  const username = 'tomsmith';
+  const password = 'SuperSecretPassword!';
+
   beforeEach(() => {
     cy.visit('/login');
   });
 
   it('Login with valid creds', () => {
-    cy.get('[id="username"]').type('tomsmith');
-    cy.get('[id="password"]').type('SuperSecretPassword!');
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
     cy.get('#login > button > i').click();
 
     cy.get('#flash').should('be.visible');
     cy.contains('You logged into a secure area!').should('be.visible');
   });
 
-  it('Login with invalid creds', () => {
-    cy.get('[id="username"]').type('invalid Username');
-    cy.get('[id="password"]').type('invalid Password');
+  it('Login with invalid username', () => {
+    cy.get('#username').type(`${username}_1`);
+    cy.get('#password').type(password);
     cy.get('#login > button > i').click();
 
     cy.get('#flash').should('be.visible');
     cy.contains('Your username is invalid!').should('be.visible');
   });
 
+  it('Login with invalid password', () => {
+    cy.get('#username').type(username);
+    cy.get('#password').type(`${password}_1`);
+    cy.get('#login > button > i').click();
+
+    cy.get('#flash').should('be.visible');
+    cy.contains('Your password is invalid!').should('be.visible');
+  });
   it('Logout from the app', () => {
-    cy.get('[id="username"]').type('tomsmith');
-    cy.get('[id="password"]').type('SuperSecretPassword!');
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
     cy.get('#login > button > i').click();
 
     cy.get('#content > div > a > i').click();
