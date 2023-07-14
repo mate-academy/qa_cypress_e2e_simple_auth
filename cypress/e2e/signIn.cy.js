086119dbb4d5a0d74601500067c3635e9027bc6a
Vasyl <vasylsymets@gmail.com>
2023-07-14 19:51:14 +0300

    Solution2test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,33 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('/login');
   });
 
-  it('', () => {
+  it('Login with valid creds', () => {
+    cy.get('[id="username"]').type('tomsmith');
+    cy.get('[id="password"]').type('SuperSecretPassword!');
+    cy.get('#login > button > i').click();
 
+    cy.get('#flash').should('be.visible');
+    cy.contains('You logged into a secure area!').should('be.visible');
+  });
+
+  it('Login with invalid creds', () => {
+    cy.get('[id="username"]').type('invalid Username');
+    cy.get('[id="password"]').type('invalid Password');
+    cy.get('#login > button > i').click();
+
+    cy.get('#flash').should('be.visible');
+    cy.contains('Your username is invalid!').should('be.visible');
+  });
+
+  it('Logout from the app', () => {
+    cy.get('[id="username"]').type('tomsmith');
+    cy.get('[id="password"]').type('SuperSecretPassword!');
+    cy.get('#login > button > i').click();
+
+    cy.get('#content > div > a > i').click();
+    cy.contains('You logged out of the secure area!').should('be.visible');
   });
 });
