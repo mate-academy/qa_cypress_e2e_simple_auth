e9be719eead7848cbae7b35900c868c93d899eb7
OleksandraLevytska01 <alexandralevytska@gmail.com>
2023-07-14 17:49:24 +0300

    fixed comments task2

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,48 +1,53 @@
 describe('Sign In page', () => {
+  const username = 'tomsmith';
+  const password = 'SuperSecretPassword!';
+
   beforeEach(() => {
     cy.visit('https://the-internet.herokuapp.com/login');
   });
 
   it('should log in with valid credentials', () => {
-    cy.get('#username')
-      .type('tomsmith');
-    cy.get('#password')
-      .type('SuperSecretPassword!');
-    cy.get('button[type="submit"]')
-      .click();
+    cy.get('#username').type(username);
+
+    cy.get('#password').type(password);
+
+    cy.contains('button[type="submit"]', 'Login').click();
+
     cy.contains('You logged into a secure area!').should('be.visible');
   });
 
   it('should display validation errors with invalid credentials', () => {
-    cy.get('#username').type('tmsmith');
-    cy.get('#password').type('SuperSecretPassword!');
-    cy.get('button[type="submit"]').click();
+    cy.get('#username').type(username + '1');
+
+    cy.get('#password').type(password);
+
+    cy.contains('button[type="submit"]', 'Login')
+      .click();
 
     cy.contains('Your username is invalid!').should('be.visible');
   });
 
   it('should display validation errors with invalid credentials', () => {
-    cy.get('#username')
-      .type('tomsmith');
-    cy.get('#password')
-      .type('SuperSecretPassw!');
-    cy.get('button[type="submit"]')
+    cy.get('#username').type(username);
+
+    cy.get('#password').type(password + '1');
+
+    cy.contains('button[type="submit"]', 'Login')
       .click();
 
     cy.contains('Your password is invalid!').should('be.visible');
   });
 
   it('should log out successfully', () => {
-    cy.get('#username')
-      .type('tomsmith');
-    cy.get('#password')
-      .type('SuperSecretPassword!');
-    cy.get('button[type="submit"]')
+    cy.get('#username').type(username);
+
+    cy.get('#password').type(password);
+    cy.contains('button[type="submit"]', 'Login')
       .click();
 
     cy.contains('You logged into a secure area!').should('be.visible');
 
-    cy.get('.button.secondary.radius')
+    cy.contains('.button', 'Logout')
       .click();
     cy.contains('You logged out of the secure area!').should('be.visible');
   });
