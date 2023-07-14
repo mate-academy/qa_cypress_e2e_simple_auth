cbbe4e9d89c2cdc1c287c133804ea1dfa617fa07
Oleksandr Melnyk <snchkmelnyk@gmail.com>
2023-07-14 16:41:49 +0300

    Add automated tests for sign-in flow

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,28 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('should log in with valid credentials', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('button[type="submit"]').click();
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
+  });
 
+  it('should show validation errors with invalid credentials', () => {
+    cy.get('#username').type('invalid_username');
+    cy.get('#password').type('invalid_password');
+    cy.get('button[type="submit"]').click();
+    cy.get('#flash').should('contain', 'Your username is invalid!');
+  });
+
+  it('should log out successfully', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('button[type="submit"]').click();
+    cy.get('a[href="/logout"]').click();
+    cy.get('#flash').should('contain', 'You logged out of the secure area!');
   });
 });
