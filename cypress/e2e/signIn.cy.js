d3bae1a90778c01b72301e365f450a3be6a179bc
Oleksandr Melnyk <snchkmelnyk@gmail.com>
2023-07-14 20:02:54 +0300

    Add automated tests for sign-in flow extra  new -solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -15,17 +15,27 @@ describe('Sign In page', () => {
     cy.get('#flash').should('contain', 'You logged into a secure area!');
   });
 
-  it('should show validation errors with invalid credentials', () => {
+  it('should show validation errors with invalid Username', () => {
     cy.get('#username').type(`${username}+1`);
-    cy.get('#password').type(`${password}+r`);
+    cy.get('#password').type(password);
     cy.get('button[type="submit"]').click();
     cy.get('#flash').should('contain', 'Your username is invalid!');
   });
 
-  it('should show validation errors with invalid credentials', () => {
-    cy.get('#username').type(`${username}+1`);
-    cy.get('#password').type(`${password}+r`);
+  it('should show validation errors with invalid Password', () => {
+    cy.get('#username').type(username);
+    cy.get('#password').type(`${password}+9`);
     cy.get('button[type="submit"]').click();
-    cy.get('#flash').should('contain', 'Your username is invalid!');
+    cy.get('#flash').should('contain', 'Your password is invalid!');
+  });
+
+  it('should allows to logout', () => {
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
+    cy.contains('.radius', 'Login').click();
+    cy.contains('.button', 'Logout').click();
+    cy.get('#flash')
+      .should('contains.text', 'You logged out of the secure area!');
+    cy.url().should('include', '/login');
   });
 });
