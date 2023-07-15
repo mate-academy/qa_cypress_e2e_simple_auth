1d40c703987db90f3a7f933c2bc298bef2978a44
Natasha <kava221998@gmail.com>
2023-07-15 21:27:45 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,37 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
-
-  it('', () => {
-
+  it('should allow user to login with valid creds', () => {
+    const password = 'SuperSecretPassword!';
+    const username = 'tomsmith';
+    cy.get('[id="username"]').type(username);
+    cy.get('[id="password"]').type(password);
+    cy.get('[type="submit"]').click();
+    cy.get('[class="flash success"]')
+      .should('contain', 'You logged into a secure area!');
+    cy.get('[href="/logout"]').click();
+    cy.get('[class="flash success"]')
+      .should('contain', 'You logged out of the secure area!');
+  });
+  it('should not allow user to login with invalid creds', () => {
+    const password = 'invalid Password';
+    const username = 'invalid Username';
+    cy.get('[id="username"]').type(username);
+    cy.get('[id="password"]').type(password);
+    cy.get('[type="submit"]').click();
+    cy.get('[class="flash error"]')
+      .should('contain', 'Your username is invalid!');
+  });
+  it('should allow user to logout from the app', () => {
+    const password = 'SuperSecretPassword!';
+    const username = 'tomsmith';
+    cy.get('[id="username"]').type(username);
+    cy.get('[id="password"]').type(password);
+    cy.get('[type="submit"]').click();
+    cy.get('[href="/logout"]').click();
+    cy.get('[class="flash success"]')
+      .should('contain', 'You logged out of the secure area!');
   });
 });
