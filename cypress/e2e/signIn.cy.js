19b5ef2f7417b15936867bb3f2459010be2089a9
Denys Mandebura <mandebura1802@gmail.com>
2023-07-16 19:28:37 +0300

    changed e2e test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,73 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  beforeEach(() => {
+const { password } = require("pg/lib/defaults");
 
+describe('Sign In page', () => {
+  const username = "tomsmith";
+  const password = "SuperSecretPassword!";
+
+  const invalidUsername = "tosmit1h"
+  const invalidPassword = "SuperSecretPassword!1"
+
+
+  beforeEach(() => {
+    cy.visit('/login');
+    cy.url().should('include', '/login')
   });
 
-  it('', () => {
+  it('should login with valid credits', () => {
+    cy.get('#username').
+    click();
+    cy.get('#username').
+    type(username);
 
+    cy.get('#password').
+    click();
+    cy.get('#password').
+    type(password);
+
+    cy.get('[class="fa fa-2x fa-sign-in"]').
+    click(); 
+
+    cy.get('#flash').should('contain.text', 'You logged into a secure area!')
+  });
+
+  it('should login with invalid creds', () => {
+    cy.get('#username').
+    click();
+    cy.get('#username').
+    type(invalidUsername);
+
+    cy.get('#password').
+    click();
+    cy.get('#password').
+    type(invalidPassword);
+
+    cy.get('[class="fa fa-2x fa-sign-in"]').
+    click(); 
+
+    cy.get('#flash').
+    should('contain.text', ' Your username is invalid!');
+  });
+
+  it('should logout from the app', () => {
+    cy.get('#username').
+    click();
+    cy.get('#username').
+    type(username);
+
+    cy.get('#password').
+    click();
+    cy.get('#password').
+    type(password);
+
+    cy.get('[class="fa fa-2x fa-sign-in"]').
+    click(); 
+
+    cy.get('[href="/logout"]').
+    click('');
+
+    cy.get('#flash').
+    should('contain.text', 'You logged out of the secure area!');
   });
 });
