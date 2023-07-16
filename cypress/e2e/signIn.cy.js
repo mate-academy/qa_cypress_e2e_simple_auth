2d028fb0835197277810fe8a9ce17cb6667fbda2
Natasha <kava221998@gmail.com>
2023-07-16 16:51:14 +0300

    Fix values

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,38 +1,36 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  const password = 'SuperSecretPassword!';
+  const username = 'tomsmith';
   beforeEach(() => {
     cy.visit('https://the-internet.herokuapp.com/login');
   });
   it('should allow user to login with valid creds', () => {
-    const password = 'SuperSecretPassword!';
-    const username = 'tomsmith';
     cy.get('[id="username"]').type(username);
     cy.get('[id="password"]').type(password);
     cy.get('[type="submit"]').click();
     cy.get('[class="flash success"]')
       .should('contain', 'You logged into a secure area!');
-    cy.get('[href="/logout"]').click();
     cy.get('[class="flash success"]')
-      .should('contain', 'You logged out of the secure area!');
+      .should('contain', 'You logged into a secure area!');
+    cy.url().should('include', '/secure');
   });
   it('should not allow user to login with invalid creds', () => {
-    const password = 'invalid Password';
-    const username = 'invalid Username';
-    cy.get('[id="username"]').type(username);
-    cy.get('[id="password"]').type(password);
+    cy.get('[id="username"]').type(username + 1);
+    cy.get('[id="password"]').type(password + 1);
     cy.get('[type="submit"]').click();
     cy.get('[class="flash error"]')
       .should('contain', 'Your username is invalid!');
+    cy.url().should('include', '/login');
   });
   it('should allow user to logout from the app', () => {
-    const password = 'SuperSecretPassword!';
-    const username = 'tomsmith';
     cy.get('[id="username"]').type(username);
     cy.get('[id="password"]').type(password);
     cy.get('[type="submit"]').click();
     cy.get('[href="/logout"]').click();
     cy.get('[class="flash success"]')
       .should('contain', 'You logged out of the secure area!');
+    cy.url().should('include', '/login');
   });
 });
