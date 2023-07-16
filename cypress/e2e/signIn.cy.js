9d56d497b2062ca0f2da72a9878a11927720708f
neshtaa <you@example.com>
2023-07-16 23:16:50 +0300

    test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -13,11 +13,18 @@ describe('Sign In page', () => {
     cy.contains('Welcome to the Secure Area').should('be.visible');
   });
 
-  it('should not be able to log in with invalid credential data', () => {
+  it('should not be able to log in with invalid username', () => {
     cy.get('#username').type('tomsmith2');
-    cy.get('#password').type('SuperSecretPassword!2');
+    cy.get('#password').type('SuperSecretPassword!');
     cy.get('button.radius').click();
     cy.contains('Your username is invalid!').should('be.visible');
+  });
+
+  it('should not be able to log in with invalid password', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!2');
+    cy.get('button.radius').click();
+    cy.contains('Your password is invalid!').should('be.visible');
   });
 
   it('should be able to log out after successful log in', () => {
