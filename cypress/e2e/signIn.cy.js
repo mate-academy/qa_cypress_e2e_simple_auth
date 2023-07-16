2bcb058280692cb7d3c8dedf65f0f712edcd970b
neshtaa <you@example.com>
2023-07-16 22:52:39 +0300

    test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,30 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.url().should('include', '/login');
   });
 
-  it('', () => {
+  it('should be able to log in with valid credential data', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('button.radius').click();
+    cy.contains('Welcome to the Secure Area').should('be.visible');
+  });
 
+  it('should not be able to log in with invalid credential data', () => {
+    cy.get('#username').type('tomsmith2');
+    cy.get('#password').type('SuperSecretPassword!2');
+    cy.get('button.radius').click();
+    cy.contains('Your username is invalid!').should('be.visible');
+  });
+
+  it('should be able to log out after successful log in', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('button.radius').click();
+    cy.contains('Welcome to the Secure Area').should('be.visible');
+    cy.get('.button').click();
+    cy.contains('You logged out of the secure area!').should('be.visible');
   });
 });
