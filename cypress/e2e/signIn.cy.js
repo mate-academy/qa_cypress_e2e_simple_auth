95b8af6841f2d100e921ae24dc3a309fac610950
Denys Mandebura <mandebura1802@gmail.com>
2023-07-16 19:45:02 +0300

    deleted no-use string

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,73 +1,70 @@
 /// <reference types="cypress" />
 
-const { password } = require("pg/lib/defaults");
-
 describe('Sign In page', () => {
-  const username = "tomsmith";
-  const password = "SuperSecretPassword!";
+  const username = 'tomsmith';
+  const password = 'SuperSecretPassword!';
 
-  const invalidUsername = "tosmit1h"
-  const invalidPassword = "SuperSecretPassword!1"
-
+  const invalidUsername = 'tosmit1h';
+  const invalidPassword = 'SuperSecretPassword!1';
 
   beforeEach(() => {
     cy.visit('/login');
-    cy.url().should('include', '/login')
+    cy.url().should('include', '/login');
   });
 
   it('should login with valid credits', () => {
-    cy.get('#username').
-    click();
-    cy.get('#username').
-    type(username);
+    cy.get('#username')
+      .click();
+    cy.get('#username')
+      .type(username);
 
-    cy.get('#password').
-    click();
-    cy.get('#password').
-    type(password);
+    cy.get('#password')
+      .click();
+    cy.get('#password')
+      .type(password);
 
-    cy.get('[class="fa fa-2x fa-sign-in"]').
-    click(); 
+    cy.get('[class="fa fa-2x fa-sign-in"]')
+      .click();
 
-    cy.get('#flash').should('contain.text', 'You logged into a secure area!')
+    cy.get('#flash').should('contain.text', 'You logged into a secure area!');
   });
 
   it('should login with invalid creds', () => {
-    cy.get('#username').
-    click();
-    cy.get('#username').
-    type(invalidUsername);
+    cy.get('#username')
+      .click();
+    cy.get('#username')
+      .type(invalidUsername);
 
-    cy.get('#password').
-    click();
-    cy.get('#password').
-    type(invalidPassword);
+    cy.get('#password')
+      .click();
+    cy.get('#password')
+      .type(invalidPassword);
 
-    cy.get('[class="fa fa-2x fa-sign-in"]').
-    click(); 
+    cy.get('[class="fa fa-2x fa-sign-in"]')
+      .click();
 
-    cy.get('#flash').
-    should('contain.text', ' Your username is invalid!');
+    cy.get('#flash')
+      .should('contain.text', ' Your username is invalid!');
   });
 
   it('should logout from the app', () => {
-    cy.get('#username').
-    click();
-    cy.get('#username').
-    type(username);
+    cy.get('#username')
+      .click();
+    cy.get('#username')
+      .type(username);
 
-    cy.get('#password').
-    click();
-    cy.get('#password').
-    type(password);
+    cy.get('#password')
+      .click();
+    cy.get('#password')
+      .type(password);
 
-    cy.get('[class="fa fa-2x fa-sign-in"]').
-    click(); 
+    cy.get('[class="fa fa-2x fa-sign-in"]')
+      .click();
 
-    cy.get('[href="/logout"]').
-    click('');
+    cy.get('[href="/logout"]')
+      .click('');
 
-    cy.get('#flash').
-    should('contain.text', 'You logged out of the secure area!');
+    cy.get('#flash')
+      .should('contain.text', 'You logged out of the secure area!');
   });
 });
