228bc4e55646dd02de962a73503823e2b5afe5ec
Olena Lapa <olena.lapa@gmail.com>
2023-07-17 12:57:25 +0300

    New changes

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -10,31 +10,33 @@ describe('Sign In page', () => {
     cy.get('#username').type('username');
     cy.get('#password').type('password');
     cy.contains('.fa', 'Login').click();
+  });
 
-    it('should assert you successfully logged in', () => {
-      cy.get('#username').type('username');
-      cy.get('#password').type('password');
-      cy.contains('.fa', 'Login').click();
-      cy.get('have text', 'Secure Area');
-    });
+  it('should allow login with invalid username', () => {
+    cy.get('#username').type('tomsmithinvalid');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.contains('.fa', 'Login').click();
+    cy.get('[id="flash-messages"]')
+      .should('include.text', 'Your username is invalid!');
+    cy.url().should('include', Cypress.config().baseUrl + '/login');
+  });
 
-    it('should allow login with invalid credentials', () => {
-      cy.get('#username').type('tomsmithinvalid');
-      cy.get('#password').type('invalidPassword!');
-      cy.contains('.fa', 'Login').click();
-      cy.get('[id="flash-messages"]')
-        .should('include.text', 'Your username is invalid!');
-      cy.url().should('include', Cypress.config().baseUrl + '/login');
-    });
+  it('should allow login with invalid password', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('invalidPassword!');
+    cy.contains('.fa', 'Login').click();
+    cy.get('[id="flash-messages"]')
+      .should('include.text', 'Your password is invalid!');
+    cy.url().should('include', Cypress.config().baseUrl + '/login');
+  });
 
-    it('should allow user to logout', () => {
-      cy.get('#username').type('tomsmith');
-      cy.get('#password').type('SuperSecretPassword!');
-      cy.contains('.fa', 'Login').click();
-      cy.get('[href="/logout"]').click();
-      cy.get('[id="flash-messages"]')
-        .should('include.text', 'You logged out of the secure area!');
-      cy.url().should('include', Cypress.config().baseUrl + '/login');
-    });
+  it('should allow user to logout', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.contains('.fa', 'Login').click();
+    cy.get('[href="/logout"]').click();
+    cy.get('[id="flash-messages"]')
+      .should('include.text', 'You logged out of the secure area!');
+    cy.url().should('include', Cypress.config().baseUrl + '/login');
   });
 });
