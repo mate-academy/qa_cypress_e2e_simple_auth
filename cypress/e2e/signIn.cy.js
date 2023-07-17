decce922f0d2a82d24f5290589beb3d86f92ac33
Natasha <kava221998@gmail.com>
2023-07-17 10:52:55 +0300

    Fixed id and url

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -4,30 +4,38 @@ describe('Sign In page', () => {
   const password = 'SuperSecretPassword!';
   const username = 'tomsmith';
   beforeEach(() => {
-    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.visit('/login');
   });
   it('should allow user to login with valid creds', () => {
-    cy.get('[id="username"]').type(username);
-    cy.get('[id="password"]').type(password);
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
     cy.get('[type="submit"]').click();
-    cy.get('[class="flash success"]')
+    cy.get('#flash')
       .should('contain', 'You logged into a secure area!');
     cy.url().should('include', '/secure');
   });
-  it('should not allow user to login with invalid creds', () => {
-    cy.get('[id="username"]').type(username + 1);
-    cy.get('[id="password"]').type(password + 1);
+  it('should not allow user to login with invalid username', () => {
+    cy.get('#username').type(username + 1);
+    cy.get('#password').type(password);
     cy.get('[type="submit"]').click();
-    cy.get('[class="flash error"]')
+    cy.get('#flash')
       .should('contain', 'Your username is invalid!');
     cy.url().should('include', '/login');
   });
+  it('should not allow user to login with invalid password', () => {
+    cy.get('#username').type(username);
+    cy.get('#password').type(password + 1);
+    cy.get('[type="submit"]').click();
+    cy.get('#flash')
+      .should('contain', 'Your password is invalid!');
+    cy.url().should('include', '/login');
+  });
   it('should allow user to logout from the app', () => {
-    cy.get('[id="username"]').type(username);
-    cy.get('[id="password"]').type(password);
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
     cy.get('[type="submit"]').click();
     cy.get('[href="/logout"]').click();
-    cy.get('[class="flash success"]')
+    cy.get('#flash')
       .should('contain', 'You logged out of the secure area!');
     cy.url().should('include', '/login');
   });
