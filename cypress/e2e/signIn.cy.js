7e9f402c8b7195d1179087c24865e4bfb69cb2c2
Viktoriia Tykhomyrova <viktoriavintonak0@gmail.com>
2023-08-26 18:10:39 +0200

    cypress\e2e\signIn.cy.js

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,22 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
+describe('Sign In and Logout Flow', () => {
   beforeEach(() => {
-
+    cy.visit('https://react-redux.realworld.io/#/login?_k=hn88rp');
   });
 
-  it('', () => {
-
+  it('should login with valid and invalid creds, and finally logout', () => {
+    cy.get(':nth-child(1) > .form-control').type('tomsmith!@gmail.com');
+    cy.get(':nth-child(2) > .form-control').type('SuperSecretPassword!');
+    cy.get('.btn').click();
+    cy.contains('a', 'Settings').click();
+    cy.contains('button', 'Or click here to logout').click();
+    cy.contains('a.nav-link', 'Sign in').click();
+    cy.get(':nth-child(1) > .form-control').type('Jojo123@gmail.com');
+    cy.get(':nth-child(2) > .form-control').type('Jojo123');
+    cy.get('.btn').click();
+    cy.get('.error-messages > li')
+      .should('contain.text', 'email or password is invalid');
+    cy.get('.navbar-brand').contains('conduit').click();
   });
 });
