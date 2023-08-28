88446c5ba3337e951506aaa1c82f2656f28ab538
Viktoriia Tykhomyrova <viktoriavintonak0@gmail.com>
2023-08-28 21:59:22 +0200

    cypress\e2e\signIn.cy.js

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,22 +1,25 @@
 /// <reference types="cypress" />
-
 describe('Sign In and Logout Flow', () => {
   beforeEach(() => {
-    cy.visit('https://react-redux.realworld.io/#/login?_k=hn88rp');
+    cy.visit('/login');
   });
 
-  it('should login with valid and invalid creds, and finally logout', () => {
-    cy.get(':nth-child(1) > .form-control').type('tomsmith!@gmail.com');
-    cy.get(':nth-child(2) > .form-control').type('SuperSecretPassword!');
-    cy.get('.btn').click();
+  it('should login with valid creds', () => {
+    cy.loginWithCredentials();
+
     cy.contains('a', 'Settings').click();
     cy.contains('button', 'Or click here to logout').click();
+  });
+
+  it('should login with invalid creds', () => {
     cy.contains('a.nav-link', 'Sign in').click();
-    cy.get(':nth-child(1) > .form-control').type('Jojo123@gmail.com');
-    cy.get(':nth-child(2) > .form-control').type('Jojo123');
-    cy.get('.btn').click();
+    cy.loginWithInvalidCredentials();
+
     cy.get('.error-messages > li')
       .should('contain.text', 'email or password is invalid');
+  });
+
+  it('should return to home page', () => {
     cy.get('.navbar-brand').contains('conduit').click();
   });
 });
