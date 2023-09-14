bd4733af421ddedbe5ad5a830ed4cec52454555c
Iryna Vakula <iravakula28@gmail.com>
2023-09-14 15:53:57 +0200

    Solution simple_auth

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,37 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('should provide the ability to sign in', () => {
+    cy.get('[id="username"]').type('tomsmith');
 
+    cy.get('[id="password"]').type('SuperSecretPassword!');
+
+    cy.get('[type="submit"]').click();
+
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
+  });
+  it('should provide the ability to successful logout ', () => {
+    cy.get('[id="username"]').type('tomsmith');
+
+    cy.get('[id="password"]').type('SuperSecretPassword!');
+
+    cy.get('[type="submit"]').click();
+
+    cy.get('[class="icon-2x icon-signout"]').click();
+
+    cy.get('#flash').should('contain', 'You logged out of the secure area!');
+  });
+
+  it('should not provide the ability to sign in with invalid creds', () => {
+    cy.get('[id="username"]').type('invalid Username');
+
+    cy.get('[id="password"]').type('invalid Password');
+
+    cy.get('[type="submit"]').click();
+
+    cy.get('#flash').should('contain', 'Your username is invalid!');
   });
 });
