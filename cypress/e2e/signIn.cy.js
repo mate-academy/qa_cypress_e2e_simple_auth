45f076ac4b7a87e61e32e2b38339821113d82cfc
Artur <masterarturgames@gmail.com>
2023-09-14 17:23:27 +0300

    Sign In

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,36 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
+    cy.visit("/login")
+  });
+  
+  const userNmae = 'tomsmith'
+  const password = 'SuperSecretPassword!'
 
+  it('should Login with valid creds', () => {
+    cy.get('#username').type(userNmae);
+    cy.get('#password').type(password);
+    cy.get('.fa').click()
+    cy.get('#flash').should('contain.text','You logged into a secure area!')
+    cy.url().should('equal', Cypress.config().baseUrl + "secure")
   });
 
-  it('', () => {
-
+  it('should Login with invalid creds', () => {
+    cy.get('#username').type(userNmae + '_sobaka');
+    cy.get('#password').type(password + '_sobaka');
+    cy.get('.fa').click()
+    cy.get('#flash').should('contain.text', 'Your username is invalid!')
   });
+
+  it('Logout from the app',()=>{
+    cy.get('#username').type(userNmae);
+    cy.get('#password').type(password);
+    cy.get('.fa').click()
+    cy.get('#flash').should('contain.text','You logged into a secure area!')
+    cy.url().should('equal', Cypress.config().baseUrl + "secure")
+    cy.get('.icon-2x').click()
+    cy.get('#flash').should('contain.text', 'You logged out of the secure area!')
+  })
 });
+
+  
