241ebcbd15cc1419bf7e34856a9b454aece9fe78
Dmytro Ponomarov <ponomarovdmitry13@gmail.com>
2023-09-14 15:33:55 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,31 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+  });
+  const username = 'tomsmith';
+  const password = 'SuperSecretPassword!';
+  const invalidusername = 'fakename';
+  const invalidpassword = 'fakepassword';
 
+  it('should provide an ability to log in with valid creds', () => {
+    cy.get('#username').type(username);
+    cy.get('#password').type(password + `{Enter}`);
+    cy.get('#flash').should('contain.text', 'You logged into a secure area!');
   });
 
-  it('', () => {
+  it('should not provide an ability to log in with invalid creds', () => {
+    cy.get('#username').type(invalidusername);
+    cy.get('#password').type(invalidpassword + `{Enter}`);
+    cy.get('#flash').should('contain.text', 'Your username is invalid!');
+  });
 
+  it('should provide an ability to logged out', () => {
+    cy.get('#username').type(username);
+    cy.get('#password').type(password + `{Enter}`);
+    cy.get('#flash').should('contain.text', 'You logged into a secure area!');
+    cy.get('[href="/logout"]').click();
+    cy.get('#flash')
+      .should('contain.text', 'You logged out of the secure area!');
   });
 });
