2451e095b8428462dde407fbc8514183218ab128
Dmytro Ponomarov <ponomarovdmitry13@gmail.com>
2023-09-14 18:18:09 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -4,25 +4,31 @@ describe('Sign In page', () => {
   beforeEach(() => {
     cy.visit('https://the-internet.herokuapp.com/login');
   });
-  const username = 'tomsmith';
+  const userName = 'tomsmith';
   const password = 'SuperSecretPassword!';
-  const invalidusername = 'fakename';
-  const invalidpassword = 'fakepassword';
+  const invalidUserName = 'fakename';
+  const invalidPassword = 'fakepassword';
 
   it('should provide an ability to log in with valid creds', () => {
-    cy.get('#username').type(username);
+    cy.get('#username').type(userName);
     cy.get('#password').type(password + `{Enter}`);
     cy.get('#flash').should('contain.text', 'You logged into a secure area!');
   });
 
-  it('should not provide an ability to log in with invalid creds', () => {
-    cy.get('#username').type(invalidusername);
-    cy.get('#password').type(invalidpassword + `{Enter}`);
+  it('should not provide an ability to log in with invalid username', () => {
+    cy.get('#username').type(invalidUserName);
+    cy.get('#password').type(password + `{Enter}`);
     cy.get('#flash').should('contain.text', 'Your username is invalid!');
   });
 
+  it('should not provide an ability to log in with invalid password', () => {
+    cy.get('#username').type(userName);
+    cy.get('#password').type(invalidPassword + `{Enter}`);
+    cy.get('#flash').should('contain.text', 'Your password is invalid!');
+  });
+
   it('should provide an ability to logged out', () => {
-    cy.get('#username').type(username);
+    cy.get('#username').type(userName);
     cy.get('#password').type(password + `{Enter}`);
     cy.get('#flash').should('contain.text', 'You logged into a secure area!');
     cy.get('[href="/logout"]').click();
