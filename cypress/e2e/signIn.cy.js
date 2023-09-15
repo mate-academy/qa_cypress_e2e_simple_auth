117bdc6ce717ab5fd68512120ad84e7351953991
Yana <7949309@stud.nau.edu.ua>
2023-09-15 12:19:31 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -5,28 +5,28 @@ describe('Sign In page', () => {
     cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('should able login user', () => {
+  it('should log in user with valid creds', () => {
     cy.get('#username').type('tomsmith');
     cy.get('#password').type('SuperSecretPassword!');
     cy.get('.radius').click();
     cy.get('#flash').should('contain', 'You logged into a secure area!');
   });
 
-  it('Unsuccessful login user with invalid username', () => {
+  it('should not log in with invalid username', () => {
     cy.get('#username').type('tomsmith1');
     cy.get('#password').type('SuperSecretPassword!');
     cy.get('.radius').click();
     cy.get('#flash').should('contain', 'Your username is invalid');
   });
 
-  it('Unsuccessful login user with invalid password', () => {
+  it('should not log in with invalid password', () => {
     cy.get('#username').type('tomsmith');
     cy.get('#password').type('SuperSecretPassword!1');
     cy.get('.radius').click();
     cy.get('#flash').should('contain', 'Your password is invalid');
   });
 
-  it('should able logout user', () => {
+  it('should successfully log out a user', () => {
     cy.get('#username').type('tomsmith');
     cy.get('#password').type('SuperSecretPassword!');
     cy.get('.radius').click();
