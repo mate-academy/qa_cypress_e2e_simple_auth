126cb04a6cc08d47fb5ae5db4591fbae24b2e986
Iryna Vakula <iravakula28@gmail.com>
2023-09-15 11:15:14 +0200

    Solution simple_auth

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,23 +1,27 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  const validUsername = 'tomsmith';
+  const validPassword = 'SuperSecretPassword!';
+  const invalidUsername = 'invalid Username';
+  const invalidPassword = 'invalid Password';
   beforeEach(() => {
     cy.visit('https://the-internet.herokuapp.com/login');
   });
 
   it('should provide the ability to sign in', () => {
-    cy.get('[id="username"]').type('tomsmith');
+    cy.get('[id="username"]').type(validUsername);
 
-    cy.get('[id="password"]').type('SuperSecretPassword!');
+    cy.get('[id="password"]').type(validPassword);
 
     cy.get('[type="submit"]').click();
 
     cy.get('#flash').should('contain', 'You logged into a secure area!');
   });
   it('should provide the ability to successful logout ', () => {
-    cy.get('[id="username"]').type('tomsmith');
+    cy.get('[id="username"]').type(validUsername);
 
-    cy.get('[id="password"]').type('SuperSecretPassword!');
+    cy.get('[id="password"]').type(validPassword);
 
     cy.get('[type="submit"]').click();
 
@@ -26,13 +30,23 @@ describe('Sign In page', () => {
     cy.get('#flash').should('contain', 'You logged out of the secure area!');
   });
 
-  it('should not provide the ability to sign in with invalid creds', () => {
-    cy.get('[id="username"]').type('invalid Username');
+  it('should not provide the ability to sign in with invalid username', () => {
+    cy.get('[id="username"]').type(invalidUsername);
 
-    cy.get('[id="password"]').type('invalid Password');
+    cy.get('[id="password"]').type(validPassword);
 
     cy.get('[type="submit"]').click();
 
     cy.get('#flash').should('contain', 'Your username is invalid!');
+  });
+
+  it('should not provide the ability to sign in with invalid password', () => {
+    cy.get('[id="username"]').type(validUsername);
+
+    cy.get('[id="password"]').type(invalidPassword);
+
+    cy.get('[type="submit"]').click();
+
+    cy.get('#flash').should('contain', 'Your password is invalid!');
   });
 });
