a79365cf3ab602c3d315d44f7f869220d721cf38
SGabida <sergey.gabida@gmail.com>
2023-09-15 11:58:56 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -15,14 +15,21 @@ describe('Sign In page', () => {
   it('should test unuccessful login with invalid credentials', () => {
     cy.get('#username').type('qa_gsn');
     cy.get('#password').type('NotSoSecretPassword!');
-    cy.get('.fa-sign-in').click();
+    cy.get('.radius').click();
     cy.get('#flash').should('contain.text', 'Your username is invalid!');
+  });
+
+  it('should test unuccessful login with valid login & invalid pass', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('NotSoSecretPassword!');
+    cy.get('.radius').click();
+    cy.get('#flash').should('contain.text', 'Your password is invalid!');
   });
 
   it('should test successful login and successful logout', () => {
     cy.get('#username').type('tomsmith');
     cy.get('#password').type('SuperSecretPassword!');
-    cy.get('.fa-sign-in').click();
+    cy.get('.radius').click();
     cy.get('#flash').should('contain.text', 'You logged into a secure area!');
     cy.get('.icon-signout').click();
     cy.get('#flash').should('contain.text', 'You logged out of the secure');
