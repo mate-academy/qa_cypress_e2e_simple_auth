e32cdaa9be88988c41b9ccea49be5363dbf7fc4f
okan3 <okan_ivan@ukr.net>
2023-11-01 15:01:51 +0200

    add task solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,45 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('Login with valid creds', () => {
+    cy.get('#username')
+      .type('tomsmith');
 
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('.radius').click();
+
+    cy.get('#flash')
+      .should('contain', 'You logged into a secure area!');
+  });
+
+  it('Login with invalid creds', () => {
+    cy.get('#username')
+      .type('tomsmit');
+
+    cy.get('#password')
+      .type('SuperSecretPasswor!');
+
+    cy.get('.radius').click();
+
+    cy.get('#flash')
+      .should('contain', 'Your username is invalid!');
+  });
+
+  it('Logout from the app', () => {
+    cy.get('#username')
+      .type('tomsmith');
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('.radius').click();
+
+    cy.get('#flash')
+      .should('contain', 'You logged into a secure area!');
   });
 });
