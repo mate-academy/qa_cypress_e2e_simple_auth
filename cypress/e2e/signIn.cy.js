0b5f9e9480c3e8aa04ecb9ffcb49e6ac3e4f5304
Mykyta Vyskrebentsev <nikitavyskrebentsev1@gmail.com>
2023-11-01 17:32:52 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -16,15 +16,26 @@ describe('Sign In page', () => {
       .should('contain', 'You logged into a secure area!');
   });
 
-  it('Login with invalid creds (invalid Username, invalid Password)', () => {
+  it('Login with invalid Username', () => {
     cy.get('#username')
       .type('qwerty');
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+    cy.get('.radius')
+      .click();
+    cy.get('#flash')
+      .should('contain', 'Your username is invalid!');
+  });
+
+  it('Login with invalid Password', () => {
+    cy.get('#username')
+      .type('tomsmith');
     cy.get('#password')
       .type('qwerty');
     cy.get('.radius')
       .click();
     cy.get('#flash')
-      .should('contain', 'Your username is invalid!');
+      .should('contain', 'Your password is invalid!');
   });
 
   it('Logout from the app', () => {
