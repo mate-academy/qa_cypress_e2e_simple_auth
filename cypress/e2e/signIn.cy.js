9b9d98ae83b3d2b58ef78a5acd2eaeea46c709dc
Alex <horonhozov@gmail.com>
2023-11-01 14:40:52 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,54 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+   cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('should login with valid creds', () => {
+   cy.get('#username')
+   .type('tomsmith');
 
+   cy.get('#password')
+    .type('SuperSecretPassword!');
+
+   cy.get('.fa')
+    .click();
+   
+    cy.get('#flash')
+    .should('contain', 'You logged into a secure area!');
   });
+
+  it('should login with invalid creds', () => {
+   cy.get('#username')
+   .type('tomsmith0');
+
+   cy.get('#password')
+    .type('SuperSecretPassword0!');
+
+   cy.get('.fa')
+    .click();
+   
+    cy.get('#flash')
+    .should('contain', 'Your username is invalid!');
+  });
+
+  it('should logout from the app', () => {
+   cy.get('#username')
+   .type('tomsmith');
+
+   cy.get('#password')
+    .type('SuperSecretPassword!');
+
+   cy.get('.fa')
+    .click();
+   
+    cy.get('#flash')
+    .should('contain', 'You logged into a secure area!');
+
+    cy.get('.icon-2x')
+    .click();
+
+    cy.get("#flash")
+    .should('contain', 'You logged out of the secure area!')
+  }); 
 });
