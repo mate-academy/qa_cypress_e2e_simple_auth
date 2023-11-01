8115f9d6cd17e999f07b955588c069f2e904e061
Vitaliia Dovbak <vitaliyaadovbak+1@gmail.com>
2023-11-01 13:23:23 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,54 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('/login');
   });
 
-  it('', () => {
+  it('should provide an ability to log in with valid credentials', () => {
+    cy.get('#username')
+      .type('tomsmith');
 
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('.fa')
+      .click();
+
+    cy.get('#flash')
+      .should('contain.text', 'You logged into a secure area!');
+  });
+
+  it('should not allow to log in with invalid creds', () => {
+    cy.get('#username')
+      .type('tom');
+
+    cy.get('#password')
+      .type('SuperSecret');
+
+    cy.get('.fa')
+      .click();
+
+    cy.get('#flash')
+      .should('contain.text', 'Your username is invalid!');
+  });
+
+  it('should provide an ability to log out from the app', () => {
+    cy.get('#username')
+      .type('tomsmith');
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('.fa')
+      .click();
+
+    cy.get('#flash')
+      .should('contain.text', 'You logged into a secure area!');
+
+    cy.get('.icon-2x')
+      .click();
+
+    cy.get('#flash')
+      .should('contain.text', 'You logged out of the secure area!');
   });
 });
