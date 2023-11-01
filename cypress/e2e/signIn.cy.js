1e190a4a5402f566246b6fafb141ab534a65aacd
Iryna <sheliryna278@gmail.com>
2023-11-01 15:19:26 +0200

    Cypress: Simple Auth Assertions

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -4,7 +4,6 @@ describe('Sign In page', () => {
   beforeEach(() => {
     cy.visit('https://the-internet.herokuapp.com/login');
   });
-
   it('Login with valid creds', () => {
     cy.get('#username')
       .type('tomsmith');
@@ -14,17 +13,24 @@ describe('Sign In page', () => {
     cy.get('#flash')
       .should('contain', ' You logged into a secure area!');
   });
-
-  it('Login with invalid creds', () => {
+  it('Login with not valid username', () => {
     cy.get('#username')
       .type('tomsmith1');
     cy.get('#password')
-      .type('SuperSecretPassword1!');
+      .type('SuperSecretPassword!');
     cy.get('.fa').click();
     cy.get('#flash')
       .should('contain', 'Your username is invalid!');
   });
-
+  it('Login with not valid password', () => {
+    cy.get('#username')
+      .type('tomsmith');
+    cy.get('#password')
+      .type('SuperSecretPassword1!');
+    cy.get('.fa').click();
+    cy.get('#flash')
+      .should('contain', 'Your password is invalid!');
+  });
   it('Logout from the app', () => {
     cy.get('#username')
       .type('tomsmith');
