b648b2849cdbfbfe2fd60e49b2b7b0651c08b767
qasep23-OksanaShumylko <lirokon37kop90@gmail.com>
2023-11-01 14:01:23 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,73 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
+describe('Sign in page', ()=> {
   beforeEach(() => {
+    cy.visit('https://the-internet.herokuapp.com/login')
+  })
 
-  });
+  it.only('should provide an ability to login with registered data', () => {
+    cy.get('#username')
+    .type('tomsmith');
 
-  it('', () => {
+    cy.get('#password')
+    .type('SuperSecretPassword!');
+    
+    cy.get('.radius')
+    .click();
 
-  });
-});
+    cy.get('#flash')
+    .should('contain.text', 'You logged into a secure area');
+  })
+
+  it.only('should not provide ability to login with invalid data', () => {
+    cy.get('#username')
+    .type('new_user890');
+
+    cy.get('#password')
+    .type('FakePasswordForNewUser');
+
+    cy.get('.radius')
+    .click();
+
+    cy.get('#flash')
+    .should('contain.text', 'Your username is invalid!');
+  })
+
+  it.only('should provide an ability to logout from the app', () => {
+    cy.get('#username')
+    .type('tomsmith');
+
+    cy.get('#password')
+    .type('SuperSecretPassword!');
+    
+    cy.get('.radius')
+    .click();
+
+    cy.get('#flash')
+    .should('contain.text', 'You logged into a secure area');
+
+    cy.get('[href="/logout"]')
+    .click();
+
+    cy.get('#flash')
+    .should('contain.text', 'You logged out of the secure area!');
+  })
+})
+
+    /*cy.get(':nth-child(2) > .nav-link')
+    .click();
+
+    cy.get(':nth-child(1) > .form-control')
+    .type('tyui90@gmail.com');
+
+    cy.get(':nth-child(2) > .form-control')
+    .type('123456789testing')
+
+    cy.get('.btn')
+    .click();
+
+    cy.get(':nth-child(4) > .nav-link')
+    .should('contain.text', 'tyuio_user');
+  })
+})*/
+
