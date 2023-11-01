0ead88a7e56bcbe79db19baf169fb71a9256caf9
Solomiia Martyniuk <solomiia.e@gmail.com>
2023-11-01 13:59:08 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,35 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login')
   });
 
-  it('', () => {
-
+  it('should provide the ability to login', () => {
+    cy.get('#username')
+      .type('tomsmith');
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+    cy.get('.fa')
+      .click();
   });
+
+  it('should provide validation errors after login with invali data', () => {
+    cy.get('#username')
+      .type('tomsmit');
+    cy.get('#password')
+      .type('SuperSecretPasswor!');
+    cy.get('.fa')
+      .click();
+  })
+
+  it('should provide the ability to logout', () => {
+    cy.get('#username')
+      .type('tomsmith');
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+    cy.get('.fa')
+      .click();
+    cy.get('.icon-2x')
+      .click()
+  })
 });
