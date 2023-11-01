4c02b4381c4a3d6bce16fa51eb52e8654097fd91
Solomiia Martyniuk <solomiia.e@gmail.com>
2023-11-01 15:42:50 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+const { ValidationErrorItemType } = require("sequelize");
+
 describe('Sign In page', () => {
   beforeEach(() => {
     cy.visit('https://the-internet.herokuapp.com/login')
@@ -12,25 +14,47 @@ describe('Sign In page', () => {
       .type('SuperSecretPassword!');
     cy.get('.fa')
       .click();
+
+    cy.get('#flash.flash.success')
+      .should('have.text', 'You logged into a secure area!')
   });
 
-  it('should provide validation errors after login with invali data', () => {
+  it('should provide validation errors after login with invali username', () => {
     cy.get('#username')
       .type('tomsmit');
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+    cy.get('.fa')
+      .click();
+
+    cy.get('#flash', ValidationErrorItemType)
+      .should('have.text', 'Your username is invalid!')
+  })
+
+  it('should resieve validation errors after login with invali password', () => {
+    cy.get('#username')
+      .type('tomsmith');
     cy.get('#password')
       .type('SuperSecretPasswor!');
     cy.get('.fa')
       .click();
+
+    cy.get('#flash', ValidationErrorItemType)
+      .should('have.text', 'Your password is invalid!')
   })
 
-  it('should provide the ability to logout', () => {
+  it('should resieve the ability to logout', () => {
     cy.get('#username')
       .type('tomsmith');
     cy.get('#password')
       .type('SuperSecretPassword!');
     cy.get('.fa')
       .click();
+    
     cy.get('.icon-2x')
-      .click()
+      .click();
+
+    cy.get('#flash.flash.success')
+      .should('have.text', 'You logged out of the secure area!')
   })
 });
