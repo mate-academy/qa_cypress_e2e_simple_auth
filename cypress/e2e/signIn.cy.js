3b69f974a87fdb51ada9f0199135af812a63e648
Mykyta Vyskrebentsev <nikitavyskrebentsev1@gmail.com>
2023-11-01 15:57:44 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,43 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('/login');
   });
 
-  it('', () => {
+  it('Login with valid creds', () => {
+    cy.get('#username')
+      .type('tomsmith');
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+    cy.get('.radius')
+      .click();
+    cy.get('#flash')
+      .should('contain', 'You logged into a secure area!');
+  });
 
+  it('Login with invalid creds (invalid Username, invalid Password)', () => {
+    cy.get('#username')
+      .type('qwerty');
+    cy.get('#password')
+      .type('qwerty');
+    cy.get('.radius')
+      .click();
+    cy.get('#flash')
+      .should('contain', 'Your username is invalid!');
+  });
+
+  it('Logout from the app', () => {
+    cy.get('#username')
+      .type('tomsmith');
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+    cy.get('.radius')
+      .click();
+    cy.get('#flash')
+      .should('contain', 'You logged into a secure area!');
+    cy.get('.button')
+      .click();
+    cy.get('#flash')
+      .should('contain', 'You logged out of the secure area!');
   });
 });
