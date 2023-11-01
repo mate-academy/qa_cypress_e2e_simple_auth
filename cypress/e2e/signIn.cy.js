9cc64003046669608f2f742df3f8841df9dd3234
Iryna <sheliryna278@gmail.com>
2023-11-01 15:36:59 +0200

    Cypress: Simple Auth Assertions

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -9,7 +9,8 @@ describe('Sign In page', () => {
       .type('tomsmith');
     cy.get('#password')
       .type('SuperSecretPassword!');
-    cy.get('.fa').click();
+    cy.get('.fa')
+      .click();
     cy.get('#flash')
       .should('contain', ' You logged into a secure area!');
   });
@@ -18,7 +19,8 @@ describe('Sign In page', () => {
       .type('tomsmith1');
     cy.get('#password')
       .type('SuperSecretPassword!');
-    cy.get('.fa').click();
+    cy.get('.fa')
+      .click();
     cy.get('#flash')
       .should('contain', 'Your username is invalid!');
   });
@@ -27,7 +29,8 @@ describe('Sign In page', () => {
       .type('tomsmith');
     cy.get('#password')
       .type('SuperSecretPassword1!');
-    cy.get('.fa').click();
+    cy.get('.fa')
+      .click();
     cy.get('#flash')
       .should('contain', 'Your password is invalid!');
   });
@@ -36,10 +39,12 @@ describe('Sign In page', () => {
       .type('tomsmith');
     cy.get('#password')
       .type('SuperSecretPassword!');
-    cy.get('.fa').click();
+    cy.get('.fa')
+      .click();
     cy.get('#flash')
       .should('contain', ' You logged into a secure area!');
-    cy.get('.icon-2x').click();
+    cy.get('.icon-2x')
+      .click();
     cy.get('#flash')
       .should('contain', ' You logged out of the secure area!');
   });
