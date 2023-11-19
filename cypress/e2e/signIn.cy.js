12449ec92a90148494c3c81056b58fda1b5368d8
VladSlob <141657806+VladSlob@users.noreply.github.com>
2023-11-19 07:49:53 +0200

    signIn.cy.js

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,37 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login'); // visit the login page before each test
   });
 
-  it('', () => {
+  it('should login with valid credentials', () => {
+    cy.get('[data-cy="username-field"]').type('tomsmith'); // replace with the actual selectors
+    cy.get('[data-cy="password-field"]').type('SuperSecretPassword!');
+    cy.get('[data-cy="submit-btn"]').click();
 
+    // assert you successfully logged in
+    cy.url().should('include', '/secure'); // replace with the actual URL after login
+  });
+
+  it('should show validation error with invalid credentials', () => {
+    cy.get('[data-cy="username-field"]').type('invalidUsername');
+    cy.get('[data-cy="password-field"]').type('invalidPassword');
+    cy.get('[data-cy="submit-btn"]').click();
+
+    // assert validation errors
+    cy.get('.flash.error').should('be.visible'); // replace with the actual selector for the error message
+  });
+
+  it('should logout from the app', () => {
+    // login first
+    cy.get('[data-cy="username-field"]').type('tomsmith');
+    cy.get('[data-cy="password-field"]').type('SuperSecretPassword!');
+    cy.get('[data-cy="submit-btn"]').click();
+
+    // then logout
+    cy.get('[data-cy="logout-btn"]').click(); // replace with the actual selector for the logout button
+
+    // assert you successfully logged out
+    cy.url().should('include', '/login'); // replace with the actual URL after logout
   });
 });
