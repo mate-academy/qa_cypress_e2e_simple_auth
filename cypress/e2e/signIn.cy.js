cd81aa233a4e9fa33179c55e1f452d006fc75c4e
VladSlob <141657806+VladSlob@users.noreply.github.com>
2023-11-20 13:18:16 +0200

    signIn.cy.js

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -6,12 +6,11 @@ describe('Sign In page', () => {
   });
 
   it('should login with valid credentials', () => {
-    cy.get('[data-cy="username-field"]').type('tomsmith'); // replace with the actual selectors
+    cy.get('[data-cy="username-field"]').type('tomsmith');
     cy.get('[data-cy="password-field"]').type('SuperSecretPassword!');
     cy.get('[data-cy="submit-btn"]').click();
 
-    // assert you successfully logged in
-    cy.url().should('include', '/secure'); // replace with the actual URL after login
+    cy.url().should('include', '/secure');
   });
 
   it('should show validation error with invalid credentials', () => {
@@ -19,20 +18,17 @@ describe('Sign In page', () => {
     cy.get('[data-cy="password-field"]').type('invalidPassword');
     cy.get('[data-cy="submit-btn"]').click();
 
-    // assert validation errors
-    cy.get('.flash.error').should('be.visible'); // replace with the actual selector for the error message
+    cy.get('.flash.error').should('be.visible');
   });
 
   it('should logout from the app', () => {
-    // login first
+
     cy.get('[data-cy="username-field"]').type('tomsmith');
     cy.get('[data-cy="password-field"]').type('SuperSecretPassword!');
     cy.get('[data-cy="submit-btn"]').click();
 
-    // then logout
-    cy.get('[data-cy="logout-btn"]').click(); // replace with the actual selector for the logout button
+    cy.get('[data-cy="logout-btn"]').click();
 
-    // assert you successfully logged out
-    cy.url().should('include', '/login'); // replace with the actual URL after logout
+    cy.url().should('include', '/login');
   });
 });
