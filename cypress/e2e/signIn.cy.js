12b6e1a1559d331c17f360763ce35eda070435ca
Noemi <noemi.anna.fic@gmail.com>
2023-11-29 15:01:55 +0100

    sign in tests

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,51 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('/login');
   });
 
-  it('', () => {
+  it('should provide an ability to log in with valid creds', () => {
+    cy.get('#username')
+      .type('tomsmith');
 
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('.radius')
+      .click();
+
+    cy.get('#flash')
+      .should('exist');
+  });
+
+  it('should NOT provide an ability to log in with invalid creds', () => {
+    cy.get('#username')
+      .type('invalidusername');
+
+    cy.get('#password')
+      .type('invalidpassword');
+
+    cy.get('.radius')
+      .click();
+
+    cy.get('#flash')
+      .should('exist');
+  });
+
+  it('should provide an ability to log out', () => {
+    cy.get('#username')
+      .type('tomsmith');
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('.radius')
+      .click();
+
+    cy.get('.button')
+      .click();
+
+    cy.get('#flash')
+      .should('exist');
   });
 });
