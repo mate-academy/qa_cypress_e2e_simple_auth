42dbe55fe5ab83d19bad77149059aaff8fcc7257
Wojciech Sęk <wojtasskate@wp.pl>
2023-12-04 23:43:09 +0100

    version3

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,39 +1,45 @@
 /// <reference types="cypress" />
+const validUsername = 'tomsmith';
+const validPassword = 'SuperSecretPassword!';
+const invalidUsername = 'badguyhacker';
+const invalidPassword = 'villainpassword';
 
 describe('Sign In page', () => {
   beforeEach(() => {
     cy.visit('https://the-internet.herokuapp.com/login');
   });
-  it('should provide an ability to log in with valid credentials', () => {
+  it('should provide ability to log in and out with valid credentials', () => {
     cy.get('input[name = "username"]')
-      .type('tomsmith');
+      .type(validUsername);
     cy.get('input[name = "password"]')
-      .type('SuperSecretPassword!');
+      .type(validPassword);
     cy.get('button[type="submit"]')
       .click();
     cy.get('#flash-messages')
       .should('contain', 'You logged into a secure area!');
+    cy.get('.button')
+      .click();
+    cy.get('#flash-messages')
+      .should('contain', 'You logged out of the secure area!');
   });
-  it(`shouldn't provide the ability to login with invalid credentials`, () => {
+  it(`shouldn't provide the ability to login with invalid username`, () => {
     cy.get('input[name = "username"]')
-      .type('badguyhacker');
+      .type(invalidUsername);
     cy.get('input[name = "password"]')
-      .type('villainpassword');
+      .type(validPassword);
     cy.get('button[type="submit"]')
       .click();
     cy.get('#flash-messages')
       .should('contain', 'Your username is invalid!');
   });
-  it('should provide an ability to log out after succesfull login', () => {
+  it(`shouldn't provide the ability to login with invalid password`, () => {
     cy.get('input[name = "username"]')
-      .type('tomsmith');
+      .type(validUsername);
     cy.get('input[name = "password"]')
-      .type('SuperSecretPassword!');
+      .type(invalidPassword);
     cy.get('button[type="submit"]')
       .click();
-    cy.get('.button')
-      .click();
     cy.get('#flash-messages')
-      .should('contain', 'You logged out of the secure area!');
+      .should('contain', 'Your password is invalid!');
   });
 });
