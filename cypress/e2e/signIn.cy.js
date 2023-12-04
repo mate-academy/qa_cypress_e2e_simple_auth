b7c6571e8cca058e4991f41fb3af7fc6a87e1b08
Wojciech Sęk <wojtasskate@wp.pl>
2023-12-04 16:23:19 +0100

    version2,5-beautified

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -5,26 +5,48 @@ describe('Sign In page', () => {
     cy.visit('https://react-redux.realworld.io');
   });
   it('should provide an ability to log in with valid credentials', () => {
-    cy.get('.navbar a').contains('Sign in').click();
-    cy.get('input[type="email"]').type('kredki@kredki.com');
-    cy.get('input[type ="password"]').type('kredki');
-    cy.get('button[type="submit"]').click();
-    cy.get('.navbar').should('contain', 'kredki');
+    cy.get('.navbar a')
+      .contains('Sign in')
+      .click();
+    cy.get('input[type="email"]')
+      .type('kredki@kredki.com');
+    cy.get('input[type ="password"]')
+      .type('kredki');
+    cy.get('button[type="submit"]')
+      .click();
+    cy.get('.navbar')
+      .should('contain', 'kredki');
   });
   it(`shouldn't provide the ability to login with invalid credentials`, () => {
-    cy.get('.navbar a').contains('Sign in').click();
-    cy.get('input[type="email"]').type('kredkivillain@kredki.com');
-    cy.get('input[type ="password"]').type('kredkibadguy');
-    cy.get('button[type="submit"]').click();
-    cy.get('ul').should('contain', 'email or password is invalid');
+    cy.get('.navbar a')
+      .contains('Sign in')
+      .click();
+    cy.get('input[type="email"]')
+      .type('kredkivillain@kredki.com');
+    cy.get('input[type ="password"]')
+      .type('kredkibadguy');
+    cy.get('button[type="submit"]')
+      .click();
+    cy.get('ul')
+      .should('contain', 'email or password is invalid');
   });
   it('should provide an ability to log out after succesfull login', () => {
-    cy.get('.navbar a').contains('Sign in').click();
-    cy.get('input[type="email"]').type('kredki@kredki.com');
-    cy.get('input[type ="password"]').type('kredki');
-    cy.get('button[type="submit"]').click();
-    cy.get('.navbar a').contains('Settings').click();
-    cy.get('button').contains('Or click here to logout.').click();
-    cy.get('.navbar').should('not.contain', 'kredki');
+    cy.get('.navbar a')
+      .contains('Sign in')
+      .click();
+    cy.get('input[type="email"]')
+      .type('kredki@kredki.com');
+    cy.get('input[type ="password"]')
+      .type('kredki');
+    cy.get('button[type="submit"]')
+      .click();
+    cy.get('.navbar a')
+      .contains('Settings')
+      .click();
+    cy.get('button')
+      .contains('Or click here to logout.')
+      .click();
+    cy.get('.navbar')
+      .should('not.contain', 'kredki');
   });
 });
