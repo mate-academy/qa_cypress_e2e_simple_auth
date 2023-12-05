71b94e7ec7977cdc207135d1fe34c856b0ccc18c
Dariusz Bydolek <darek021982@gmail.com>
2023-12-05 07:15:01 +0100

    Solution v2

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -5,11 +5,23 @@ describe('Sign In page', () => {
     cy.visit('https://the-internet.herokuapp.com/login');
   });
 
+  it('Visits the Sign in page successfully', () => {
+    cy.url().should('include', '/login');
+  });
+
   it('Login with valid creds', () => {
     cy.get('#username').type('tomsmith');
     cy.get('#password').type('SuperSecretPassword!');
     cy.get('.radius').click();
     cy.get('.flash.success').should('be.visible');
+  });
+
+  it('Login with valid creds and check successful login message', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.radius').click();
+    cy.get('.flash.success').should('be.visible')
+      .and('contain.text', 'You logged into a secure area!');
   });
 
   it('Login with invalid creds show validation message for invalid username', () => {
@@ -26,11 +38,28 @@ describe('Sign In page', () => {
     cy.get('.flash.error').should('be.visible');
   });
 
+  it('Login with invalid creds and check validation error message', () => {
+    cy.get('#username').type('invalidUsername');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.radius').click();
+    cy.get('.flash.error').should('be.visible')
+      .and('contain.text', 'Your username is invalid!');
+  });
+
   it('Logout from the app', () => {
     cy.get('#username').type('tomsmith');
     cy.get('#password').type('SuperSecretPassword!');
     cy.get('.radius').click();
     cy.get('.button.secondary.radius').click();
     cy.get('#login').should('be.visible');
+  });
+
+  it('Logout from the app and check successful logout message', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.radius').click();
+    cy.get('.button.secondary.radius').click();
+    cy.get('.flash.success').should('be.visible')
+      .and('contain.text', 'You logged out of the secure area!');
   });
 });
