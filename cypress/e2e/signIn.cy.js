4c7409b2ff547f245767511e51a7a3cb937372f0
mat12722 <mat1272295@wp.pl>
2023-12-06 13:27:15 +0100

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,55 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
+  it('Login with valid credentials', () => {
+    cy.get('[id="username"]')
+      .type('tomsmith');
 
-  it('', () => {
+    cy.get('[id="password"]')
+      .type('SuperSecretPassword!');
 
+    cy.get('button[type="submit"]')
+      .click();
+
+    cy.get('[id="flash"]')
+      .contains('You logged into a secure area!')
+      .should('exist');
+  });
+  it('Login with invalid credentials', () => {
+    cy.get('[id="username"]')
+      .type('tomsmith123');
+
+    cy.get('[id="password"]')
+      .type('SuperSecretPassword!');
+
+    cy.get('button[type="submit"]')
+      .click();
+
+    cy.get('[id="flash"]')
+      .contains('Your username is invalid!')
+      .should('exist');
+  });
+  it('Succesful log out from the app', () => {
+    cy.get('[id="username"]')
+      .type('tomsmith');
+
+    cy.get('[id="password"]')
+      .type('SuperSecretPassword!');
+
+    cy.get('button[type="submit"]')
+      .click();
+
+    cy.get('[id="flash"]')
+      .contains('You logged into a secure area!')
+      .should('exist');
+
+    cy.get('.button')
+      .click();
+
+    cy.get('[id="flash"]')
+      .contains('You logged out of the secure area!')
+      .should('exist');
   });
 });
