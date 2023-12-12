b6319dba847c480838d97744507b6b8d108a94e5
Marta Kwiatkowska <marta.kwiatkowska.x@gmail.com>
2023-12-12 19:18:54 +0100

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://the-internet.herokuapp.com',
+    viewportHeight: 500,
+    viewportWidth: 1000,
     setupNodeEvents(on, config) {
     }
   }
--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -5,7 +5,46 @@ describe('Sign In page', () => {
 
   });
 
-  it('', () => {
+  it('should provide the ability to login with valid credentials', () => {
+    const userName = 'tomsmith';
+    const password = 'SuperSecretPassword!';
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('h2')
+      .should('contain.text', 'Login Page');
+    cy.get('body form')
+      .should('exist');
+    cy.get('#username')
+      .type(userName);
+    cy.get('#password')
+      .type(password);
+    cy.get('[type="submit"]')
+      .click();
 
+    cy.get('div #flash')
+      .should('contain.text', 'You logged into a secure area!');
+    cy.get('.button').contains('Logout')
+      .click();
+
+    cy.get('div #flash')
+      .should('contain.text', 'You logged out of the secure area!');
+  });
+
+  it('should not provide the ability to login with invalid credentials', () => {
+    const invalidUsername = 'tomsmid';
+    const invalidPassword = 'SuperNotSecretPassword';
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('h2')
+      .should('contain.text', 'Login Page');
+    cy.get('body form')
+      .should('exist');
+    cy.get('#username')
+      .type(invalidUsername);
+    cy.get('#password')
+      .type(invalidPassword);
+    cy.get('[type="submit"]')
+      .click();
+
+    cy.get('div #flash')
+      .should('contain.text', 'Your username is invalid!');
   });
 });
