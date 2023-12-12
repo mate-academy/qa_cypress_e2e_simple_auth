73aeac4203e08360c77c1ab051ec31d017f065b1
Marta Kwiatkowska <marta.kwiatkowska.x@gmail.com>
2023-12-12 20:55:46 +0100

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,50 +1,52 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  const userName = 'tomsmith';
+  const password = 'SuperSecretPassword!';
+  const invalidUsername = 'tomsmid';
+  const invalidPassword = 'SuperNotSecretPassword';
   beforeEach(() => {
-
+    cy.visit('/login');
   });
 
   it('should provide the ability to login with valid credentials', () => {
-    const userName = 'tomsmith';
-    const password = 'SuperSecretPassword!';
-    cy.visit('https://the-internet.herokuapp.com/login');
-    cy.get('h2')
-      .should('contain.text', 'Login Page');
-    cy.get('body form')
-      .should('exist');
-    cy.get('#username')
-      .type(userName);
-    cy.get('#password')
-      .type(password);
-    cy.get('[type="submit"]')
-      .click();
-
-    cy.get('div #flash')
+    // Go to Login Page and verify
+    cy.get('h2').should('contain.text', 'Login Page');
+    cy.get('body form').should('exist');
+    // Input valid data into form
+    cy.get('#username').type(userName);
+    cy.get('#password').type(password);
+    // Click Login button
+    cy.get('[type="submit"]').click();
+    // Verify successfull log in
+    cy.url().should('equal', 'https://the-internet.herokuapp.com/secure');
+    cy.get('div #flash-messages')
       .should('contain.text', 'You logged into a secure area!');
-    cy.get('.button').contains('Logout')
-      .click();
-
-    cy.get('div #flash')
+    // Verify successfull log out
+    cy.get('.button').contains('Logout').click();
+    cy.get('div #flash-messages')
       .should('contain.text', 'You logged out of the secure area!');
   });
 
   it('should not provide the ability to login with invalid credentials', () => {
-    const invalidUsername = 'tomsmid';
-    const invalidPassword = 'SuperNotSecretPassword';
-    cy.visit('https://the-internet.herokuapp.com/login');
-    cy.get('h2')
-      .should('contain.text', 'Login Page');
-    cy.get('body form')
-      .should('exist');
-    cy.get('#username')
-      .type(invalidUsername);
-    cy.get('#password')
-      .type(invalidPassword);
-    cy.get('[type="submit"]')
-      .click();
-
-    cy.get('div #flash')
+    // Go to Login Page and verify
+    cy.get('h2').should('contain.text', 'Login Page');
+    cy.get('body form').should('exist');
+    // Verify invalid username
+    cy.get('#username').type(invalidUsername);
+    cy.get('#password').type(password);
+    // Click Login button
+    cy.get('[type="submit"]').click();
+    // Verify validation error
+    cy.get('div .error')
       .should('contain.text', 'Your username is invalid!');
+    // Verify invalid password
+    cy.get('#username').type(userName);
+    cy.get('#password').type(invalidPassword);
+    // Click Login button
+    cy.get('[type="submit"]').click();
+    // Verify validation error
+    cy.get('div .error')
+      .should('contain.text', 'Your password is invalid!');
   });
 });
