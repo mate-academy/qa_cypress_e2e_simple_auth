e9a7c0a792c06c7215fb22b73f287ec3ddcb77fc
Orest Halushka <halslor@gmail.com>
2023-12-27 15:10:57 +0200

    tests edited

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -11,16 +11,25 @@ describe('Sign In page', () => {
 
     cy.get('.fa.fa-2x.fa-sign-in').click();
 
-    cy.get('#flash').should('contain', 'You logged into');
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
   });
 
-  it('validation errors should be displayed with invalid credits', () => {
+  it('validation error should be displayed with non-existing username', () => {
     cy.get('#username').type('Qwerty');
-    cy.get('#password').type('123');
+    cy.get('#password').type('SuperSecretPassword!');
 
     cy.get('.fa.fa-2x.fa-sign-in').click();
 
     cy.get('#flash').should('contain', 'Your username is invalid!');
+  });
+
+  it('validation error should be displayed with an incorrect password', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('Qwerty123456!');
+
+    cy.get('.fa.fa-2x.fa-sign-in').click();
+
+    cy.get('#flash').should('contain', 'Your password is invalid!');
   });
 
   it('log out should be successfull', () => {
