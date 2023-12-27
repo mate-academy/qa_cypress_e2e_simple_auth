73eace0970fc70912c99b64ca54ba31c6094d62c
Anna Mahda <148216090+AnnaMahda@users.noreply.github.com>
2023-12-27 15:29:02 +0200

    New signIn.cy.js

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,14 +1,30 @@
 describe('Login and Logout Flow', () => {
-  it('Login with valid credentials', () => {
+  beforeEach(() => {
     cy.visit('https://the-internet.herokuapp.com/login');
-    cy.get('[id="username"]').type('tomsmith');
-    cy.get('[id="password"]').type('SuperSecretPassword!');
-    cy.get('[class="fa fa-2x fa-sign-in"]').click();
+  });
+
+  it('Login with valid credentials', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
     cy.get('#flash').should('contain', 'secure area')
   });
 
+  it('Login with invalid username', () => {
+    cy.get('#username').type('fake');
+    cy.get('#password').type('SuperSecretPassword');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain', 'invalid')
+  });
+
+  it('Login with invalid password', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('fake');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain', 'invalid')
+  });
+
   it('Login with invalid credentials', () => {
-    cy.visit('https://the-internet.herokuapp.com/login');
     cy.get('#username').type('fake');
     cy.get('#password').type('fake');
     cy.get('.fa').click();
@@ -16,7 +32,6 @@ describe('Login and Logout Flow', () => {
   });
 
   it('Logout from the app', () => {
-    cy.visit('https://the-internet.herokuapp.com/login');
     cy.get('#username').type('tomsmith');
     cy.get('#password').type('SuperSecretPassword!');
     cy.get('.fa').click();
