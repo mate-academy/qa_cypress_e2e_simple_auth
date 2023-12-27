82fed53aa7dc4a8414cb205b8c83595d1bbaf8f8
Vasylyna Zvizlo <vasylynazvizlo@gmail.com>
2023-12-27 13:09:21 +0200

    test sign in page

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,31 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('should provide an ability to log in with valid creds', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('i').click();
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
+    cy.url().should('not.include', '/login');
+  });
 
+  it('should not provide an ability to log in with invalid creds', () => {
+    cy.get('#username').type('invalid Username');
+    cy.get('#password').type('invalid Password');
+    cy.get('i').click();
+    cy.get('#flash').should('contain', 'Your username is invalid!');
+    cy.url().should('include', '/login');
+  });
+
+  it('should provide an ability to log out', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('i').click();
+    cy.get('a i').click();
+    cy.get('#flash').should('contain', 'You logged out of the secure area!');
+    cy.url().should('include', '/login');
   });
 });
