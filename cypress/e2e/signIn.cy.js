da13a78fd2de6d7e14e1ff63e491f78fd38369b3
Dmytro_Yelisieiev <deliseev0482@gmail.com>
2023-12-27 13:03:50 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,65 @@
 /// <reference types="cypress" />
 
+const login = 'tomsmith';
+const password = 'SuperSecretPassword!';
+const invalidLogin = 'TestUser41';
+const invalidPassword = 'Qwerty123';
+
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('/login');
   });
 
-  it('', () => {
+  it('user should be able to login with valid credits', () => {
+    cy.get('h2')
+      .should('contain.text', 'Login Page');
 
+    cy.get('#username')
+      .type(login);
+
+    cy.get('#password')
+      .type(password);
+
+    cy.get('.fa').click();
+
+    cy.get('#flash')
+      .should('contain.text', 'You logged into a secure area!');
+  });
+
+  it('user shoudnt login with invalid credits', () => {
+    cy.get('h2')
+      .should('contain.text', 'Login Page');
+
+    cy.get('#username')
+      .type(invalidLogin);
+
+    cy.get('#password')
+      .type(invalidPassword);
+
+    cy.get('.fa').click();
+
+    cy.get('#flash')
+      .should('contain.text', 'Your username is invalid!');
+  });
+
+  it('user should be able to logout', () => {
+    cy.get('h2')
+      .should('contain.text', 'Login Page');
+
+    cy.get('#username')
+      .type(login);
+
+    cy.get('#password')
+      .type(password);
+
+    cy.get('.fa').click();
+
+    cy.get('#flash')
+      .should('contain.text', 'You logged into a secure area!');
+
+    cy.get('.button').click();
+
+    cy.get('#flash')
+      .should('contain.text', 'You logged out of the secure area!');
   });
 });
