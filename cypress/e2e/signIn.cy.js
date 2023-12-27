d6fb1e37dca84ca6004f81699805b9e2a82e1280
Alina Lynnyk <lnk3071129@gmail.com>
2023-12-27 18:44:38 +0200

    Update 2

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -9,21 +9,25 @@ describe('Sign In page', () => {
     cy.get('#username').type('tomsmith');
     cy.get('#password').type('SuperSecretPassword!');
     cy.get('.radius').click();
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
   });
-  it('shouldn`t provide an ability to log in with invalid email', () => {
+  it('shouldn`t provide an ability to log in with invalid usename', () => {
     cy.get('#username').type('Username');
     cy.get('#password').type('SuperSecretPassword');
     cy.get('.radius').click();
+    cy.get('#flash').should('contain', 'Your username is invalid!');
   });
   it('shouldn`t provide an ability to log in with invalid password', () => {
     cy.get('#username').type('tomsmith');
     cy.get('#password').type('Password');
     cy.get('.radius').click();
+    cy.get('#flash').should('contain', 'Your password is invalid!');
   });
   it('should provide an ability to log out', () => {
     cy.get('#username').type('tomsmith');
     cy.get('#password').type('SuperSecretPassword!');
     cy.get('.radius').click();
     cy.get('.button.secondary.radius').click();
+    cy.get('#flash').should('contain', 'You logged out of the secure area!');
   });
 });
