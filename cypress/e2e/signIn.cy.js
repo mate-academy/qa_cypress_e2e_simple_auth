0db1423eb52d9ab8e705c47e04b1b00d7f6cdfbc
Nikolaichuk Artem <tompsoq@gmail.com>
2023-12-27 14:07:15 +0200

    login cypress tests added

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,28 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
+describe('Login In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('Login with valid creds', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.radius').click();
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
+  });
 
+  it('Login with invalid creds', () => {
+    cy.get('#username').type('Username');
+    cy.get('#password').type('Password!');
+    cy.get('.radius').click();
+    cy.get('#flash').should('contain', 'Your username is invalid!');
+  });
+  it('Logout from the app', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.radius').click();
+    cy.get('.icon-2x').click();
+    cy.get('#flash').should('contain', 'You logged out of the secure area!');
   });
 });
