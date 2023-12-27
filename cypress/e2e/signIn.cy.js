d760785d75177f00582923cc00852b1e07f29164
Andrii Veselovsky <andrii.veselovsky@gmail.com>
2023-12-27 18:15:54 +0200

    Tests for signIn page

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,43 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('/login');
   });
 
-  it('', () => {
+  it.skip('should provide an ability to log in with valid creds', () => {
+    cy.get('h2:contains("Login Page")').should('be.visible');
+    cy.get('#username')
+      .type('tomsmith');
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+    cy.get('button.radius').click();
+    cy.get('#flash').should('contain', 'logged into');
+  });
 
+  it.skip('should not provide an ability to log in with invalid creds', () => {
+    cy.get('#username')
+      .type('invalidCreds');
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+    cy.get('button.radius')
+      .click();
+    cy.get('#flash')
+      .should('include.text', 'Your username is invalid!');
+  });
+
+  it('should provide an ability to log out', () => {
+    cy.get('#username')
+      .type('tomsmith');
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+    cy.get('button.radius')
+      .click();
+    cy.get('#flash')
+      .should('contain', 'logged into');
+    cy.get('a.button.secondary.radius')
+      .contains('Logout')
+      .click();
+    cy.get('#flash')
+      .should('contain', 'You logged out');
   });
 });
