afe9bcee25b45a6261bf93129d8c7431f4109f64
Artur Broichenko <orturko77@gmail.com>
2023-12-27 13:12:49 +0000

    e2e_tests

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,24 @@
 /// <reference types="cypress" />
-
+const username = 'tomsmith';
+const password = 'SuperSecretPassword!';
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
-
-  it('', () => {
-
+  it('should to login with valid creds', () => {
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
+    cy.get('.radius').click();
+  });
+  it('should not to login with invalid creds', () => {
+    cy.get('#username').type(`${username}123`);
+    cy.get('#password').type(`${password}123`);
+    cy.get('.radius').click();
+  });
+  it('should to logout from Secure area', () => {
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
+    cy.get('.radius').click();
+    cy.get('.button.secondary.radius').click();
   });
 });
