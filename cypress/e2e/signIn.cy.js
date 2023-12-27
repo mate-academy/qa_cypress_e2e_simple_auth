5e032b23cb813b8a1e7b5342f5cd0c7119d5c748
Anna Mahda <148216090+AnnaMahda@users.noreply.github.com>
2023-12-27 11:44:12 +0200

    New signIn.cy.js

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,26 @@
-/// <reference types="cypress" />
-
-describe('Sign In page', () => {
-  beforeEach(() => {
-
+describe('Login and Logout Flow', () => {
+  it('Login with valid credentials', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('[id="username"]').type('tomsmith');
+    cy.get('[id="password"]').type('SuperSecretPassword!');
+    cy.get('[class="fa fa-2x fa-sign-in"]').click();
+    cy.get('#flash').should('contain', 'secure area')
   });
 
-  it('', () => {
+  it('Login with invalid credentials', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('#username').type('fake');
+    cy.get('#password').type('fake');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain', 'invalid')
+  });
 
+  it('Logout from the app', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('.icon-2x').click();
+    cy.url().should('include', '/login')
   });
 });
