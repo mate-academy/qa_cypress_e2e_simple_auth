eeeb3d6aa5cfa4e47a0ae33829f3c956d0fb9e65
Yura Kisilchuk <yurkis02@gmail.com>
2023-12-27 15:26:16 +0200

    added e2e tests for the login and logout from the app

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,42 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  beforeEach(() => {
+beforeEach(() => {
+  cy.visit('/login');
+});
 
+const username = 'tomsmith';
+const password = 'SuperSecretPassword!';
+
+describe('Sign In page', () => {
+  it('Should allow to login with valid creds', () => {
+    cy.findById('username').type(username);
+    cy.findById('password').type(password);
+    cy.get('.fa-sign-in').click();
+    cy.get('.flash.success').should('contain', 'You logged into a secure area!');
+    //cy.url().should('include', '/secure');
   });
 
-  it('', () => {
+  it('Should not login with invalid username', () => {
+    cy.findById('username').type(username + Math.random().toFixed(3));
+    cy.findById('password').type(password);
+    cy.get('.fa-sign-in').click();
+    // cy.url().should('not.include', '/secure');
+    cy.get('.flash.error').should('contain', 'Your username is invalid!');
+  });
 
+  it('Should not login with invalid password', () => {
+    cy.findById('username').type(username);
+    cy.findById('password').type(password + Math.random().toFixed(3));
+    cy.get('.fa-sign-in').click();
+    cy.url().should('not.include', '/secure');
+    cy.get('.flash.error').should('contain', 'Your password is invalid!');
+  });
+    it('Should allow to login and logout from the app', () => {
+      cy.findById('username').type(username);
+      cy.findById('password').type(password);
+      cy.get('.fa-sign-in').click();
+      cy.get('.icon-signout').click();
+      //cy.url().should('include', '/login');
+      cy.get('.flash.success').should('contain', 'You logged out of the secure area!');
   });
 });
