2e5e19b6f4d86250cf93cde53388588df7bfe46c
Natalia-Slavuta <tritoutike@gmail.com>
2023-12-27 15:22:14 +0200

    testing sign in

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,32 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
-
-  it('', () => {
-
+  it('should assert  successful loggedin', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('button.radius').click();
+    cy.url().should('include', '/secure');
+    cy.get('a.button.secondary.radius').click();
+  });
+  it('should assert validation errors for invalid username', () => {
+    cy.get('#username').type('!@dfvefv222');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('button.radius').click();
+    cy.get('#flash').should('contain', 'Your username is invalid!');
+  });
+  it('should assert validation errors for invalid password', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('!!1SuperSecretPassword!');
+    cy.get('button.radius').click();
+    cy.get('#flash').should('contain', 'Your password is invalid!');
+  });
+  it('should assert  successful loggedout', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('button.radius').click();
+    cy.get('a.button.secondary.radius').click();
+    cy.url().should('include', '/login');
   });
 });
