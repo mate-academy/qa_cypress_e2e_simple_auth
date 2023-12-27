4bb7a6cf01485aa066f6b2ea3e8695d8dd9b286b
IvannaRA <ivankarehalova@gmail.com>
2023-12-27 14:42:38 +0300

    Тести для авторизації

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,42 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('should successfully log in with valid credentials', () => {
+    // Login with valid credentials
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('button[type="submit"]').click();
 
+    // Assert successful login
+    cy.url().should('include', '/secure');
+    cy.get('.flash').should('contain', 'You logged into a secure area!');
+
+    // Logout after successful login
+    cy.contains('Logout').click();
+  });
+
+  it('should show validation errors with invalid credentials', () => {
+    // Login with invalid credentials
+    cy.get('#username').type('invalidUsername');
+    cy.get('#password').type('invalidPassword');
+    cy.get('button[type="submit"]').click();
+
+    // Assert validation errors
+    cy.get('.flash').should('contain', 'Your username is invalid!');
+  });
+
+  it('should successfully log out', () => {
+    // Login with valid credentials first
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('button[type="submit"]').click();
+
+    cy.url().should('include', '/secure');
+    cy.get('.flash').should('contain', 'You logged into a secure area!');
+    // Logout
+    cy.contains('Logout').click();
   });
 });
