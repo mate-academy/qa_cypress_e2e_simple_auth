172f37b51bc7019806f99d62e3c9d4767a16b877
OlgaBelmega <143718672+OlgaBelmega@users.noreply.github.com>
2023-12-27 18:05:49 +0200

    Update signIn.cy.js

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -4,18 +4,21 @@ describe('Sign In page', () => {
   beforeEach(() => {
     cy.visit('/login');
    });
+  
   it('should login with valid creds', () => {
     cy.get('#username').type('tomsmith');
     cy.get('#password'). type('SuperSecretPassword!');
     cy.get('.fa').click();
     cy.url().should('not.include', '/login');
   });
+  
   it('shouldnt login with invalid creds', () => {
     cy.get('#username').type('1');
     cy.get('#password'). type('1');
     cy.get('.fa').click();
     cy.get('.flash').should('contain', 'Your username is invalid');
   })
+  
   it('should logout from the app', () => {
   cy.get('#username').type('tomsmith');
   cy.get('#password'). type('SuperSecretPassword!');
