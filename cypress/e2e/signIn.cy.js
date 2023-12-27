255b81421db664fc9ded66cffdf340db8781d798
Orest Halushka <halslor@gmail.com>
2023-12-27 12:14:15 +0200

    new tests added

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,35 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('log in should be successfully with valid credits', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
 
+    cy.get('.fa.fa-2x.fa-sign-in').click();
+
+    cy.get('#flash').should('contain', 'You logged into');
+  });
+
+  it('validation errors should be displayed with invalid credits', () => {
+    cy.get('#username').type('Qwerty');
+    cy.get('#password').type('123');
+
+    cy.get('.fa.fa-2x.fa-sign-in').click();
+
+    cy.get('#flash').should('contain', 'Your username is invalid!');
+  });
+
+  it('log out should be successfull', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+
+    cy.get('.fa.fa-2x.fa-sign-in').click();
+
+    cy.get('.button.secondary.radius').click();
+
+    cy.url().should('contain', '/login');
   });
 });
