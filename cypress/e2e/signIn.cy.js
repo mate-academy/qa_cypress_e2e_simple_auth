79a7a0c8f28c1a1cf081d2bba02be92adc19328a
Anastasiia <anastassyayassnska13@gmail.com>
2024-02-03 15:03:10 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,68 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  beforeEach(() => {
+  const username = 'tomsmith';
+  const password = 'SuperSecretPassword!';
 
+  beforeEach(() => {
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('login with valid creds', () => {
+    cy.get('h2')
+      .should('contain.text', 'Login Page');
 
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
+    cy.get('.radius').click();
+    cy.contains('#flash', 'You logged into a secure area!')
+      .should('exist');
+  });
+
+  it('invalid username', () => {
+    cy.get('h2')
+      .should('contain.text', 'Login Page');
+
+    cy.get('#username').type(username + '@');
+    cy.get('#password').type(password);
+    cy.get('.radius').click();
+    cy.contains('#flash', 'Your username is invalid!')
+      .should('exist');
+  });
+
+  it('invalid password', () => {
+    cy.get('h2')
+      .should('contain.text', 'Login Page');
+
+    cy.get('#username').type(username);
+    cy.get('#password').type(password + '@');
+    cy.get('.radius').click();
+    cy.contains('#flash', 'Your password is invalid')
+      .should('exist');
+  });
+
+  it('invalid username and password', () => {
+    cy.get('h2')
+      .should('contain.text', 'Login Page');
+
+    cy.get('#username').type(username + '@');
+    cy.get('#password').type(password + '@');
+    cy.get('.radius').click();
+    cy.contains('#flash', 'Your username is invalid!')
+      .should('exist');
+  });
+
+  it('should logout logged in user from the app', () => {
+    cy.get('h2')
+      .should('contain.text', 'Login Page');
+
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
+    cy.get('.radius').click();
+    cy.contains('#flash', 'You logged into a secure area!')
+      .should('exist');
+    cy.get('.button').click();
+    cy.contains('#flash', 'You logged out of the secure area!')
+      .should('exist');
   });
 });
