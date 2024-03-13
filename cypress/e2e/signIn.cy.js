014e1cd9541e34a6a7e07c738815ab332a8481c5
Serhii Sharabura <youserhii@gmail.com>
2024-03-13 20:27:09 +0200

    fixes_are_done

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -3,41 +3,30 @@
 describe('Sign In page', () => {
   const username = 'tomsmith';
   const password = 'SuperSecretPassword!';
+  const invalidUsername = username + Math.floor(Math.random(1000) * 100);
+  const invalidPassword = password + Math.floor(Math.random(1000) * 100);
 
   beforeEach(() => {
     cy.visit('/login');
   });
 
   it('should Login with valid creds', () => {
-    /*  cy.get('#username').type(username)
-        cy.get('#password').type(password)
-        cy.get('.fa.fa-2x.fa-sign-in').click()  */
-    cy.RegValidUser(username, password);
+    cy.loginUser(username, password);
     cy.get('h2').should('contain.text', 'Secure Area');
   });
 
   it('should NOT Login with invalid Username', () => {
-    /*  cy.get('#username').type(username + Math.floor(Math.random(1000)*100))
-        //cy.log('Username', username + Math.floor(Math.random(1000)*100))
-        cy.get('#password').type(password)
-        cy.get('.fa.fa-2x.fa-sign-in').click()  */
-    cy.RegValidUser(username + Math.floor(Math.random(1000) * 100), password);
+    cy.loginUser(invalidUsername, password);
     cy.get('.flash.error').should('contain.text', 'Your username is invalid!');
   });
 
   it('should NOT Login with invalid Password', () => {
-    /*  cy.get('#username').type(username)
-        cy.get('#password').type(password + Math.floor(Math.random(1000)*100))
-        cy.get('.fa.fa-2x.fa-sign-in').click() */
-    cy.RegValidUser(username, password + Math.floor(Math.random(1000) * 100));
+    cy.loginUser(username, invalidPassword);
     cy.get('.flash.error').should('contain.text', 'Your password is invalid!');
   });
 
   it('should Logout from the app', () => {
-    /*  cy.get('#username').type(username)
-        cy.get('#password').type(password)
-        cy.get('.fa.fa-2x.fa-sign-in').click() */
-    cy.RegValidUser(username, password);
+    cy.loginUser(username, password);
     cy.get('.icon-2x.icon-signout').click();
     cy.get('.flash.success').should(
       'contain.text', 'You logged out of the secure area!');
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,7 +24,7 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('RegValidUser', (username, password) => {
+Cypress.Commands.add('loginUser', (username, password) => {
   cy.get('#username').type(username);
   cy.get('#password').type(password);
   cy.get('.fa.fa-2x.fa-sign-in').click();
