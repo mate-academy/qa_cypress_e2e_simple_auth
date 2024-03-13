332a3e03343f1668cd1eb7712ddafe0c1d34a482
Vladyslav Ovsiannikov <vladovsiannikov07@gmail.com>
2024-03-13 16:52:55 +0200

    Tests

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,69 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  beforeEach(() => {
+  const username = 'tomsmith';
+  const password = 'SuperSecretPassword!';
 
+  beforeEach(() => {
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('should provide an ability to log in with valid credentials', () => {
+    cy.get('h2')
+      .should('contain.text', 'Login Page');
 
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
+    cy.get('.radius').click();
+    cy.contains('#flash', 'You logged into a secure area!')
+      .should('exist');
+  });
+
+  it(`should display validation error when entering invalid 'username'`, () => {
+    cy.get('h2')
+      .should('contain.text', 'Login Page');
+
+    cy.get('#username').type(username + '@');
+    cy.get('#password').type(password);
+    cy.get('.radius').click();
+    cy.contains('#flash', 'Your username is invalid!')
+      .should('exist');
+  });
+
+  it(`should display validation error when entering invalid 'password'`, () => {
+    cy.get('h2')
+      .should('contain.text', 'Login Page');
+
+    cy.get('#username').type(username);
+    cy.get('#password').type(password + '@');
+    cy.get('.radius').click();
+    cy.contains('#flash', 'Your password is invalid')
+      .should('exist');
+  });
+
+  it(`should display validation error when entering` +
+    `invalid 'username' and 'password'`, () => {
+    cy.get('h2')
+      .should('contain.text', 'Login Page');
+
+    cy.get('#username').type(username + '@');
+    cy.get('#password').type(password + '@');
+    cy.get('.radius').click();
+    cy.contains('#flash', 'Your username is invalid!')
+      .should('exist');
+  });
+
+  it('should logout logged in user from the app', () => {
+    cy.get('h2')
+      .should('contain.text', 'Login Page');
+
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
+    cy.get('.radius').click();
+    cy.contains('#flash', 'You logged into a secure area!')
+      .should('exist');
+    cy.get('.button').click();
+    cy.contains('#flash', 'You logged out of the secure area!')
+      .should('exist');
   });
 });
