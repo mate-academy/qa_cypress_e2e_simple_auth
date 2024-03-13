afe21da27ad8c6ca3452749785fac4a43c632cc2
Inna Levytska <inna.levytska24.com>
2024-03-13 21:12:35 +0200

    Levytska_tests_fix

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+const username = 'tomsmith';
+const password = 'SuperSecretPassword!';
 
 describe('Sign In page', () => {
   beforeEach(() => {
@@ -7,31 +9,42 @@ describe('Sign In page', () => {
 
   it('should provide an ability to login with existing data', () => {
     cy.get('[name="username"]')
-      .type('tomsmith');
+      .type(username);
     cy.get('[name="password"]')
-      .type('SuperSecretPassword!');
+      .type(password);
     cy.get('[type="submit"]').contains('Login')
       .click();
     cy.get('.subheader')
       .should('contain.text', 'Welcome to the Secure Area.');
   });
 
-  it('should not provide ability to login with invalid credentials', () => {
+  it('should not provide ability to login with invalid username', () => {
     cy.get('[name="username"]')
       .type('tomsmit');
     cy.get('[name="password"]')
-      .type('SuperSecretPasswor');
+      .type(password);
     cy.get('[type="submit"]').contains('Login')
       .click();
     cy.get('#flash')
       .should('contain.text', 'Your username is invalid!');
   });
 
+  it('should not provide ability to login with invalid password', () => {
+    cy.get('[name="username"]')
+      .type(username);
+    cy.get('[name="password"]')
+      .type('uperSecretPasswor');
+    cy.get('[type="submit"]').contains('Login')
+      .click();
+    cy.get('#flash')
+      .should('contain.text', 'Your password is invalid!');
+  });
+
   it('should provide an ability to logout', () => {
     cy.get('[name="username"]')
-      .type('tomsmith');
+      .type(username);
     cy.get('[name="password"]')
-      .type('SuperSecretPassword!');
+      .type(password);
     cy.get('[type="submit"]').contains('Login')
       .click();
     cy.get('.subheader')
