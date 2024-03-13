dc27a10508c9fda9af515df93142d6fb60dec5d7
Oksana Kachmar <profiscribe@gmail.com>
2024-03-13 15:25:39 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,47 @@
-/// <reference types="cypress" />
+/// <reference types="cypress" />;
 
-describe('Sign In page', () => {
-  beforeEach(() => {
+const { loginData } = require('../support/logindata');
 
+describe('Login and Logout Functionality', () => {
+  const { password, username } = loginData();
+
+  it('Login with valid creds (tomsmith/SuperSecretPassword!)', () => {
+    cy.visit('/login');
+    cy.get('input[id="username"]').type(username);
+    cy.get('input[id="password"]').type(password);
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/secure');
+    cy.get('div[id="flash"]').contains('You logged into a secure area!')
+      .should('be.visible');
   });
 
-  it('', () => {
+  it('Login with invalid creds (invalid Username)', () => {
+    cy.visit('/login');
+    cy.get('input[id="username"]').type('tomsmit');
+    cy.get('input[id="password"]').type(password);
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/login');
+    cy.get('div[id="flash"]').contains('Your username is invalid!')
+      .should('be.visible');
+  });
 
+  it('Login with invalid creds (invalid Password)', () => {
+    cy.visit('/login');
+    cy.get('input[id="username"]').type(username);
+    cy.get('input[id="password"]').type('SuperSecret');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/login');
+    cy.contains('Your password is invalid!').should('be.visible');
+  });
+
+  it('Logout from the app', () => {
+    cy.visit('/login');
+    cy.get('input[id="username"]').type(username);
+    cy.get('input[id="password"]').type(password);
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/secure');
+    cy.get('a[href="/logout"]').click();
+    cy.url().should('include', '/login');
+    cy.contains('You logged out of the secure area!').should('be.visible');
   });
 });
