d829120c779d111fb799fbaad31a2d8b50be3684
Inna Levytska <inna.levytska24.com>
2024-03-13 16:33:01 +0200

    Levytska_tests

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,43 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('should provide an ability to login with existing data', () => {
+    cy.get('[name="username"]')
+      .type('tomsmith');
+    cy.get('[name="password"]')
+      .type('SuperSecretPassword!');
+    cy.get('[type="submit"]').contains('Login')
+      .click();
+    cy.get('.subheader')
+      .should('contain.text', 'Welcome to the Secure Area.');
+  });
 
+  it('should not provide ability to login with invalid credentials', () => {
+    cy.get('[name="username"]')
+      .type('tomsmit');
+    cy.get('[name="password"]')
+      .type('SuperSecretPasswor');
+    cy.get('[type="submit"]').contains('Login')
+      .click();
+    cy.get('#flash')
+      .should('contain.text', 'Your username is invalid!');
+  });
+
+  it('should provide an ability to logout', () => {
+    cy.get('[name="username"]')
+      .type('tomsmith');
+    cy.get('[name="password"]')
+      .type('SuperSecretPassword!');
+    cy.get('[type="submit"]').contains('Login')
+      .click();
+    cy.get('.subheader')
+      .should('contain.text', 'Welcome to the Secure Area.');
+    cy.get('[href="/logout"]').contains('Logout')
+      .click();
+    cy.get('[id="flash"]')
+      .should('contain.text', 'You logged out of the secure area!');
   });
 });
