d24648c7ec7f0c1a364977e7c8f08bb7a49f79c2
Veronika <veronikadzul@gmail.com>
2024-03-13 13:44:26 +0200

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,39 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('should provide an ability to successfully sign in', () => {
+    cy.get('[id="username"]').type('tomsmith');
+    cy.get('[id="password"]').type('SuperSecretPassword!');
+    cy.get('.fa-sign-in').contains('Login').click();
+    cy.get('.flash.success').contains('You logged into a secure area!')
+      .should('be.visible');
+  });
 
+  it('should provide validation errors login with invalid username', () => {
+    cy.get('[id="username"]').type('tomsmit');
+    cy.get('[id="password"]').type('SuperSecretPassword!');
+    cy.get('.fa-sign-in').contains('Login').click();
+    cy.get('.flash.error').contains('Your username is invalid!')
+      .should('be.visible');
+  });
+
+  it('should provide validation errors login with invalid password', () => {
+    cy.get('[id="username"]').type('tomsmith');
+    cy.get('[id="password"]').type('SecretPassword!');
+    cy.get('.fa-sign-in').contains('Login').click();
+    cy.get('.flash.error').contains('Your password is invalid!')
+      .should('be.visible');
+  });
+
+  it('should provide an ability to successfully logged out', () => {
+    cy.get('[id="username"]').type('tomsmith');
+    cy.get('[id="password"]').type('SuperSecretPassword!');
+    cy.get('.fa-sign-in').contains('Login').click();
+    cy.get('.icon-signout').contains('Logout').click();
+    cy.get('.flash.success').contains('You logged out of the secure area!')
+      .should('be.visible');
   });
 });
