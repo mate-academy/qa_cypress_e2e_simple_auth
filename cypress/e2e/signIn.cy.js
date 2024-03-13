a1edd7a38154fff14692432775e0bd4ffdf37d89
falatorry <dbgdnv@gmail.com>
2024-03-13 18:13:36 +0200

    Solution by Bohdanov Daniil

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,67 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('/login');
   });
 
-  it('', () => {
+  const username = 'tomsmith';
 
+  it('should provide the ability to login', () => {
+    cy.get('#username')
+      .type(username);
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('[type="submit"]')
+      .click();
+
+    cy.get('[href="/logout"]')
+      .should('contain.text', 'Logout');
+  });
+
+  it('should prevent login with invalid username', () => {
+    cy.get('#username')
+      .type('Jsjkjk!fjkfhfj7233fggfgfsd');
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('[type="submit"]')
+      .click();
+
+    cy.get('#flash')
+      .should('contain.text', 'Your username is invalid!');
+  });
+
+  it('should prevent login with invalid password', () => {
+    cy.get('#username')
+      .type(username);
+
+    cy.get('#password')
+      .type('q232rfsfefds');
+
+    cy.get('[type="submit"]')
+      .click();
+
+    cy.get('#flash')
+      .should('contain.text', 'Your password is invalid!');
+  });
+
+  it('should provide the ability to logout', () => {
+    cy.get('#username')
+      .type(username);
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('[type="submit"]')
+      .click();
+
+    cy.get('[href="/logout"]')
+      .click();
+
+    cy.get('#flash')
+      .should('contain.text', 'You logged out of the secure area!');
   });
 });
