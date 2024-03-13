ee9f7be69a653c6f583aeea815b5245be3e78b89
Maksym Akhtamov <maksym.akhtamov@gamil.com>
2024-03-13 13:55:48 +0300

    My solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,36 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('/login');
   });
 
-  it('', () => {
+  it('Login with valid creds', () => {
+    cy.loginUser('tomsmith', 'SuperSecretPassword!');
 
+    cy.get('[href="/logout"]').contains('Logout');
+  });
+
+  it('Login with invalid password', () => {
+    cy.loginUser('tomsmith', 'invalidPassword');
+
+    cy.get('.flash.error')
+      .should('contain.text', 'Your password is invalid!');
+  });
+
+  it('Login with invalid username', () => {
+    cy.loginUser('invalidUsername', 'SuperSecretPassword!');
+
+    cy.get('.flash.error')
+      .should('contain.text', 'Your username is invalid!');
+  });
+
+  it('Logout from the app', () => {
+    cy.loginUser('tomsmith', 'SuperSecretPassword!');
+
+    cy.get('[href="/logout"]')
+      .click();
+
+    cy.get('.flash.success')
+      .should('contain.text', 'You logged out of the secure area!');
   });
 });
