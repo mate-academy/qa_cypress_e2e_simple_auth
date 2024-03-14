1d85db0a6b88f8c091a080fb9a6d52ed324d0292
AnatoliiTsymbaliuk <anatoliitsimbaliuk13@gmail.com>
2024-03-14 19:09:39 +0200

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-    }
+
+    },
+    baseUrl: 'https://the-internet.herokuapp.com'
   }
 });
