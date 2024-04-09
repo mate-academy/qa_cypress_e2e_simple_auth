9ea109f61ee9d64fe17a595b7ff28dad674da713
bla2001 <blasmartlab@gmail.com>
2024-04-09 23:12:07 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,45 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit(`/login`);
   });
 
-  it('', () => {
+  it('should login with valid creds', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!' + `{Enter}`);
+    cy.url().should('equal', '/secure');
+    cy.get('#flash')
+      .should('contain.text', 'You logged into a secure area!')
+      .should('be.visible');
+  });
 
+  it.only(`should successfully logout from the app`, () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!' + `{Enter}`);
+    cy.contains('a', 'Logout').should('have.attr', 'href', '/logout').click();
+    cy.get('#flash')
+      .should('contain.text', 'You logged out of the secure area!')
+      .should('be.visible');
+    cy.url().should('equal', '/login');
+  });
+
+  it(`should get validation errors when` +
+      ` trying to login with invalid username`, () => {
+    cy.get('#username').type('username');
+    cy.get('#password').type('SuperSecretPassword!' + `{Enter}`);
+    cy.get('#flash')
+      .should('contain.text', ' Your username is invalid!')
+      .should('be.visible');
+    cy.url().should('equal', '/login');
+  });
+
+  it(`should get validation errors when` +
+      ` trying to login with invalid password`, () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('Password!' + `{Enter}`);
+    cy.get('#flash')
+      .should('contain.text', 'Your password is invalid!')
+      .should('be.visible');
+    cy.url().should('equal', '/login');
   });
 });
