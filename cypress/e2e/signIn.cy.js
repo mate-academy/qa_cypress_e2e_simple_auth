297a7204ac38de9b680632dee5e0eeb338415c60
Anastasiya145 <a.ivanova-external@powernext.com>
2024-04-11 10:45:11 +0200

    the task is done

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,40 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('should provide an ability to log in with correct creds', () => {
+    const username = 'tomsmith';
+    const userPassword = 'SuperSecretPassword!';
 
+    cy.get('input[id="username"]').type(username);
+    cy.get('input[type="password"]').type(userPassword);
+    cy.get('.radius').contains('Login').click();
+    cy.contains('Secure Area').should('be.visible');
+  });
+
+  it('should provide an ability to log in with wrong creds', () => {
+    const username = 'test';
+    const userPassword = 'wrong';
+
+    cy.get('input[id="username"]').type(username);
+    cy.get('input[type="password"]').type(userPassword);
+    cy.get('.radius').contains('Login').click();
+    cy.get('#flash').should('contain.text', 'Your username is invalid!');
+  });
+});
+
+describe('Sign In page', () => {
+  it('should provide an ability to log out', () => {
+    const username = 'tomsmith';
+    const userPassword = 'SuperSecretPassword!';
+
+    cy.get('input[id="username"]').type(username);
+    cy.get('input[type="password"]').type(userPassword);
+    cy.get('.radius').contains('Login').click();
+    cy.contains('Secure Area').should('be.visible');
+    cy.contains('Logout').click();
+    cy.contains('Login Page').should('be.visible');
   });
 });
