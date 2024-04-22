e96a7c60116ce39c63d159f1da477e47756d2e81
gajdziormax@gmail.com <gajdziormax@gmail.com>
2024-04-22 14:16:04 +0200

    e2e SignIn tests

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,21 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
   it('', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
+    cy.get('.icon-2x').click();
 
+    cy.get('#flash').should('contain', ' You logged out of the secure area!');
+
+    cy.get('#username').type('invalidUsername');
+    cy.get('#password').type('invalidPassword');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain', 'Your username is invalid!');
   });
 });
