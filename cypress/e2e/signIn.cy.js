dd28ee34c0688cba577789fd7a201b9f054eedcb
Jakub Stein <steiniak20@gmail.com>
2024-04-23 08:13:47 +0200

    Solution the second

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -18,9 +18,15 @@ describe('Sign In page', () => {
   });
   it('failed login should display validation error', () => {
     cy.get('input[name="username"]').type(invalidUser);
-    cy.get('input[name="password"]').type(invalidPassword);
+    cy.get('input[name="password"]').type(validPassword);
     cy.get('button').contains('Login').click();
     cy.get('.error').should('contain.text', 'Your username is invalid!');
+  });
+  it('failed password should display validation error', () => {
+    cy.get('input[name="username"]').type(validUser);
+    cy.get('input[name="password"]').type(invalidPassword);
+    cy.get('button').contains('Login').click();
+    cy.get('.error').should('contain.text', 'Your password is invalid!');
   });
   it('logged in user should be able to log out', () => {
     cy.get('input[name="username"]').type(validUser);
@@ -28,5 +34,6 @@ describe('Sign In page', () => {
     cy.get('button').contains('Login').click();
     cy.get('.button').contains('Logout').click();
     cy.get('.success').should('contain.text', 'You logged out');
+    cy.contains('h2', 'Login Page');
   });
 });
