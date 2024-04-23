5510ae44ee9709886b3b9fbaa730164c8426db4c
Nivon35 <Nivon35@gmail.com>
2024-04-23 08:35:09 +0200

    signIn.cy.js fixed

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -16,15 +16,26 @@ describe('Sign In page', () => {
       .contains('You logged into a secure area!');
   });
 
-  it('Should not login user with invalid credentials', () => {
+  it('Should not login user with invalid username', () => {
     cy.get('#username')
       .type('tom');
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+    cy.get('.fa')
+      .click();
+    cy.get('#flash')
+      .contains('Your username is invalid!');
+  });
+
+  it('Should not login user with invalid password', () => {
+    cy.get('#username')
+      .type('tomsmith');
     cy.get('#password')
       .type('notSecurePassword!');
     cy.get('.fa')
       .click();
     cy.get('#flash')
-      .contains('Your username is invalid!');
+      .contains('Your password is invalid!');
   });
 
   it('Should logout user', () => {
@@ -38,12 +49,16 @@ describe('Sign In page', () => {
       .click();
     cy.get('#flash')
       .contains('You logged out of the secure area!');
+    cy.url('https://the-internet.herokuapp.com/login')
+      .should('include', '/login');
   });
 });
 
 // 1. Login with valid creds ( tomsmith / SuperSecretPassword! ):
 // - assert you successfully logged in.
-// 2. Login with invalid creds (invalid Username, invalid Password):
+// 2. Login with invalid Username):
 // - assert validation errors.
-// 3. Logout from the app:
+// 3. Login with invalid Password):
+// - assert validation errors.
+// 4. Logout from the app:
 // - assert you successfully logged out.
