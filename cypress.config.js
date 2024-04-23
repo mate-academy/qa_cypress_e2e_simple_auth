c427c2635e0287c4f1dc61851476734f810e5af9
ElaOz <ozga.elzbieta94@gmail.com>
2024-04-23 22:21:59 +0200

    adding missing config patches

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    viewportHeight: 500,
+    baseUrl: 'https://the-internet.herokuapp.com',
     setupNodeEvents(on, config) {
     }
   }
