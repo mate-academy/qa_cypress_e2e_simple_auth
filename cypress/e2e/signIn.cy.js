3907b6cc3385fdc863cb53ebf3370d277b0d7f14
Rozdorozhnii <rozdorozhnii@ukr.net>
2024-05-02 23:22:16 +0300

    Solution_v1.0

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,57 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('/login');
   });
 
-  it('', () => {
+  const userData = {
+    username: 'tomsmith',
+    password: 'SuperSecretPassword!'
+  };
 
+  it('should provide an ability to log in with valid creds', () => {
+    cy.get('form[name="login"] input[name="username"]')
+      .type(`${userData.username}`);
+
+    cy.get('form[name="login"] input[name="password"]')
+      .type(`${userData.password}`);
+
+    cy.get('form[name="login"] > button[type="submit"]')
+      .should('contain', ' Login').click();
+
+    cy.get('div[id="flash"]')
+      .should('contains.text', 'You logged into a secure area!');
+  });
+
+  it('shouldn\'t provide an ability to log in with invalid creds', () => {
+    cy.get('form[name="login"] input[name="username"]').type('invalid');
+
+    cy.get('form[name="login"] input[name="password"]')
+      .type('invalid');
+
+    cy.get('form[name="login"] > button[type="submit"]')
+      .should('contain', ' Login').click();
+
+    cy.get('div[id="flash"]')
+      .should('contains.text', 'Your username is invalid!');
+  });
+
+  it('should provide an ability to log out from the app', () => {
+    cy.get('form[name="login"] input[name="username"]')
+      .type(`${userData.username}`);
+
+    cy.get('form[name="login"] input[name="password"]')
+      .type(`${userData.password}`);
+
+    cy.get('form[name="login"] > button[type="submit"]')
+      .should('contain', ' Login').click();
+
+    cy.get('div[id="flash"]')
+      .should('contains.text', 'You logged into a secure area!');
+
+    cy.get('a[href="/logout"]').click();
+
+    cy.get('div[id="flash"]')
+      .should('contains.text', 'You logged out of the secure area!');
   });
 });
