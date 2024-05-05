93b4c97edf8baef5b0fbe189bde60bd15d5cf71e
Maksym Kozhyn <mkozyn@ukr.net>
2024-05-05 21:44:05 +0300

    Simple Auth

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,100 @@
 /// <reference types="cypress" />
 
+const username = 'tomsmith';
+const truePassword = 'SuperSecretPassword!';
+const badUsername = 'tomsmith_2';
+const badPassword = 'SuperSecretPassword!!';
+
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('/login');
   });
 
-  it('', () => {
+  it('should allow user to login using valid data in Username/Password fields',
+    () => {
+      cy.get('#username')
+        .should('exist')
+        .type(username);
 
+      cy.get('#password')
+        .should('exist')
+        .type(truePassword);
+
+      cy.get('.radius')
+        .contains('Login')
+        .should('exist')
+        .click();
+
+      cy.get('#flash')
+        .contains('You logged into a secure area!')
+        .should('exist');
+    });
+
+  it('should allow user to logout', () => {
+    cy.get('#username')
+      .should('exist')
+      .type(username);
+
+    cy.get('#password')
+      .should('exist')
+      .type(truePassword);
+
+    cy.get('.radius')
+      .contains('Login')
+      .should('exist')
+      .click();
+
+    cy.get('#flash')
+      .contains('You logged into a secure area!')
+      .should('exist');
+
+    cy.get('.button')
+      .contains('Logout')
+      .should('exist')
+      .click();
+
+    cy.get('#flash')
+      .contains('You logged out of the secure area!')
+      .should('exist');
   });
+
+  it('shouldn`t allow user to login using invalid data in username field',
+    () => {
+      cy.get('#username')
+        .should('exist')
+        .type(badUsername);
+
+      cy.get('#password')
+        .should('exist')
+        .type(truePassword);
+
+      cy.get('.radius')
+        .contains('Login')
+        .should('exist')
+        .click();
+
+      cy.get('#flash')
+        .contains('Your username is invalid!')
+        .should('exist');
+    });
+
+  it('shouldn`t allow user to login using invalid data in password field',
+    () => {
+      cy.get('#username')
+        .should('exist')
+        .type(username);
+
+      cy.get('#password')
+        .should('exist')
+        .type(badPassword);
+
+      cy.get('.radius')
+        .contains('Login')
+        .should('exist')
+        .click();
+
+      cy.get('#flash')
+        .contains('Your password is invalid!')
+        .should('exist');
+    });
 });
