7c0d6a081e2f4ddeb35f3c5447c4ec2cc7d47790
Maksymm <makshack10@gmail.com>
2024-05-07 12:59:44 +0300

    Cypress: Simple Auth Assertions

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,42 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
+  const validUsername = 'tomsmith';
+  const validPassword = 'SuperSecretPassword!';
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('should display validation errors with invalid username', () => {
+    cy.get('#username').should('exist').type('Username');
+    cy.get('#password').should('exist').type(validPassword);
+    cy.get('.radius').should('exist').click();
+    cy.get('#flash').should('exist')
+      .should('contain.text', ' Your username is invalid!');
+  });
 
+  it('should display validation errors with invalid password', () => {
+    cy.get('#username').should('exist').type(validUsername);
+    cy.get('#password').should('exist').type('Password');
+    cy.get('.radius').should('exist').click();
+    cy.get('#flash').should('exist')
+      .should('contain.text', ' Your password is invalid!');
+  });
+
+  it('should login with valid creds', () => {
+    cy.get('#username').should('exist').type(validUsername);
+    cy.get('#password').should('exist').type(validPassword);
+    cy.get('.radius').should('exist').click();
+    cy.get('#flash').should('exist')
+      .should('contain.text', 'You logged into a secure area!');
+  });
+
+  it('should logout from the app', () => {
+    cy.get('#username').should('exist').type(validUsername);
+    cy.get('#password').should('exist').type(validPassword);
+    cy.get('.radius').should('exist').click();
+    cy.get('.button').should('exist').click();
+    cy.get('#flash').should('exist')
+      .should('contain.text', 'You logged out of the secure area!');
   });
 });
