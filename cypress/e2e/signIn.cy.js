6cfc3b1b029fb85f95245d450c2f38dac2d03e0a
Nazar Zherlitsyn <nazzherlitsyn777@gmail.com>
2024-05-08 13:59:27 +0300

    Sign In

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -11,8 +11,14 @@ describe('Sign In page', () => {
     cy.get('.fa').click();
     cy.get('#flash').should('contain.text', 'You logged into a secure area!');
   });
-  it('shouldn not provide log in', () => {
+  it('should not provide log in with invalid username', () => {
     cy.get('#username').type('avaz');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain.text', 'Your username is invalid!');
+  });
+  it('should not provide log in with invalid password', () => {
+    cy.get('#username').type('tomsmith');
     cy.get('#password').type('lalala');
     cy.get('.fa').click();
     cy.get('#flash').should('contain.text', 'Your username is invalid!');
@@ -22,5 +28,7 @@ describe('Sign In page', () => {
     cy.get('#password').type('SuperSecretPassword!');
     cy.get('.fa').click();
     cy.get('.icon-2x').click();
+    // eslint-disable-next-line max-len
+    cy.get('#flash').should('contain.text', 'You logged out of the secure area!');
   });
 });
