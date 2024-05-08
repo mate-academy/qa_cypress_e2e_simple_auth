0cdf879fa3e694d4b43a7a604b1e2a738effa770
Nazar Zherlitsyn <nazzherlitsyn777@gmail.com>
2024-05-08 12:43:39 +0300

    Sign in

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,25 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
-
+  it('should provide an ability to log in', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain.text', 'You logged into a secure area!');
+  });
+  it('shouldn not provide log in', () => {
+    cy.get('#username').type('avaz');
+    cy.get('#password').type('lalala');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain.text', 'Your username is invalid!');
+  });
+  it('should provide an ability to log out', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('.icon-2x').click();
   });
 });
