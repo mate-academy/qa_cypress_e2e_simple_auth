8a31caedde378d6b4d534bd66bb235c3238f1e30
Pavlo Roiev <157385125+SilverF1nger@users.noreply.github.com>
2024-05-08 12:43:50 +0300

    message

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,31 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  beforeEach(() => {
-
+describe('Sign In page', () => { 
+  beforeEach (() => {
+    cy.visit('https://the-internet.herokuapp.com/login')
   });
 
-  it('', () => {
-
+  it('should provide an ability to log in', () => {
+   cy.get ('#username').type('tomsmith');
+   cy.get ('#password').type('SuperSecretPassword!');
+   cy.get ('.fa'). click();
+   cy.get ('#flash').should('contain.text',' You logged into a secure area!');
   });
+
+  it('should not provide an ability to log in', () => {
+    cy.get ('#username').type('tomossmith');
+    cy.get ('#password').type('SuperittoSecretPassword!');
+    cy.get ('.fa'). click();
+    cy.get ('#flash').should('contain.text','Your username is invalid');
+   });
+
+   it('click logout button', () => {
+    cy.get ('#username').type('tomsmith');
+    cy.get ('#password').type('SuperSecretPassword!');
+    cy.get ('.fa'). click();
+    cy.get ('#flash').should('contain.text','You logged into a secure area!');
+    cy.get ('.icon-2x'). click();
+    cy.get ('#flash').should('contain.text','You logged out of the secure area!');
+   })
 });
+
