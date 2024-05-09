05bd033c9f11e1b899fec7ed6c7580353a7eb8de
Kristina Iechkalova <kristina.echkalova@gmail.com>
2024-05-09 19:48:10 +0200

    Hurra

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,31 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('user can log in with valid data', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
 
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
+  });
+
+  it('user cant log in with invalid data', () => {
+    cy.get('#username').type('kristinanowitski');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+
+    cy.get('#flash').should('contain', 'Your username is invalid!');
+  });
+
+  it('user can log out', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('.icon-2x').click();
+
+    cy.get('#flash').should('contain', 'You logged out of the secure area!');
   });
 });
