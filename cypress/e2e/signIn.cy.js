14618acaeab75db76f2985d94cc7ac2f80712d30
Kristina Iechkalova <kristina.echkalova@gmail.com>
2024-05-09 20:02:48 +0200

    Hurra

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -13,12 +13,20 @@ describe('Sign In page', () => {
     cy.get('#flash').should('contain', 'You logged into a secure area!');
   });
 
-  it('user cant log in with invalid data', () => {
+  it('user cant log in with invalid username', () => {
     cy.get('#username').type('kristinanowitski');
     cy.get('#password').type('SuperSecretPassword!');
     cy.get('.fa').click();
 
     cy.get('#flash').should('contain', 'Your username is invalid!');
+  });
+
+  it('user cant log in with invalid password', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword3!');
+    cy.get('.fa').click();
+
+    cy.get('#flash').should('contain', 'Your password is invalid!');
   });
 
   it('user can log out', () => {
