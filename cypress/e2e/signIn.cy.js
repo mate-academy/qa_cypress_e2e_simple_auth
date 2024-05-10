b1c17aceebbe513f70b448c822b45bda929b889a
Ivan <panait.vanya@gmail.com$>
2024-05-10 11:21:36 +0300

    Cypress2

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,45 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
+describe('Sign in page', () => {
   beforeEach(() => {
-
-  });
-
-  it('', () => {
-
-  });
-});
+    cy.visit('/login')
+  })
+  
+  it('Login with valid creds', () => {
+      cy.get('h2')
+      .should('contain.text','Login Page')
+      cy.get('#username')
+      .type('tomsmith')
+      cy.get('#password')
+      .type('SuperSecretPassword!')
+      cy.get('.fa')
+      .click();
+      cy.get('#flash')
+      .should('contain.text','You logged into a secure area!')
+    });
+  it('Login with invalid creds', () => {
+      cy.get('h2')
+      .should('contain.text','Login Page')
+      cy.get('#username')
+      .type('tomsm')
+      cy.get('#password')
+      .type('SuperSecretPad!')
+      cy.get('.fa')
+      .click();
+      cy.get('#flash')
+      .should('contain.text','Your username is invalid!')
+      });
+    it('Logout from the app', () => {
+      cy.get('h2')
+      .should('contain.text','Login Page')
+      cy.get('#username')
+      .type('tomsmith')
+      cy.get('#password')
+      .type('SuperSecretPassword!')
+      cy.get('.fa')
+      .click();
+      cy.get('.button').click();
+      cy.get('#flash')
+      .should('contain.text','You logged out of the secure area!')
+      });
+}); 
