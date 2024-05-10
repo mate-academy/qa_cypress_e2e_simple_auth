3d53422ae0a7d24539635428d5881e7c51b19bf9
MarkelKo <davidmarkelovich@gmail.com>
2024-05-10 13:47:30 +0300

    Hi mentor)

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,28 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  beforeEach(() => {
+  });
+
+  it('should provide an ability to log in', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('#username').type(`tomsmith`);
+    cy.get('#password').type(`SuperSecretPassword!`);
+    cy.get('.fa').click();
+  });
+
+  it('Logout from the app', () => {
+    cy.visit(`https://the-internet.herokuapp.com/secure`)
+    cy.get('#username').type(`tomsmith`);
+    cy.get('#password').type(`SuperSecretPassword!`);
+    cy.get('.radius').click();
+    cy.get('.button').click();
+  }); 
+
+  it('should not provide an ability to log in with invalid data', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('#username').type('invalid');
+    cy.get('#password').type('invalid');
+    cy.get('.fa').click();
 
   });
 
-  it('', () => {
-
-  });
-});
