46166a10912bf4603affaa1e72472af723ce67b0
Anton <one.month.for23@gmail.com>
2024-05-10 13:45:06 +0300

    message

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,26 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  beforeEach(() => {
-
+describe('Sign In page', () => { });
+it('Should provide an ability to Log In with valid creds  ', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.radius').click();
   });
 
-  it('', () => {
-
+  it('Should provide an ability to Log Out from the app', () => {
+    cy.visit('https://the-internet.herokuapp.com/secure');
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.radius').click();
+    cy.get('.button').click();
   });
+
+ it('Should not provide an ability to Log In with invalid creds', () => {
+  cy.visit('https://the-internet.herokuapp.com/login');
+  cy.get('#username').type('invalid');
+  cy.get('#password').type('invalid');
+  cy.get('.fa').click();
 });
+
+
