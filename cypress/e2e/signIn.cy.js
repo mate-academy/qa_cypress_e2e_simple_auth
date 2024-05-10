d5801b45fdfc88af510eca41861e55360540d36a
Agillka <mrmadness6661313@gmail.com>
2024-05-10 14:03:45 +0300

    Done

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,36 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('Login with valid creds (tomsmith/SuperSecretPassword!)', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
 
+    cy.get('.fa').click();
+  });
+
+  it('Login with invalid Username', () => {
+    cy.get('#username').type('Some_username');
+    cy.get('#password').type('SuperSecretPassword!');
+
+    cy.get('.fa').click();
+  });
+
+  it('Login with invalid Password', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('Some_password');
+
+    cy.get('.fa').click();
+  });
+
+  it('Logout from the app', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+
+    cy.get('.fa').click();
+
+    cy.get('.button').click();
   });
 });
