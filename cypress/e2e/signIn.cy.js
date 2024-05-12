eee6dcd38ac6ee11f68163e428b439c2247f0cba
Anton <one.month.for23@gmail.com>
2024-05-12 21:06:29 +0300

    message

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -6,6 +6,7 @@ it('Should provide an ability to Log In with valid creds  ', () => {
     cy.get('#username').type('tomsmith');
     cy.get('#password').type('SuperSecretPassword!');
     cy.get('.radius').click();
+    cy.get('#flash').should('contain.text', 'You logged into a secure area!');
   });
 
   it('Should provide an ability to Log Out from the app', () => {
@@ -14,13 +15,21 @@ it('Should provide an ability to Log In with valid creds  ', () => {
     cy.get('#password').type('SuperSecretPassword!');
     cy.get('.radius').click();
     cy.get('.button').click();
+    cy.get('h2').should('contain.text', 'Login Page');
   });
 
- it('Should not provide an ability to Log In with invalid creds', () => {
+ it('Should not provide an ability to Log In with invalid username', () => {
   cy.visit('https://the-internet.herokuapp.com/login');
   cy.get('#username').type('invalid');
-  cy.get('#password').type('invalid');
+  cy.get('#password').type('SuperSecretPassword!');
   cy.get('.fa').click();
+  cy.get('#flash').should('contain', ' Your username is invalid!');
 });
 
-
+it('Should not provide an ability to Log In with invalid password', () => {
+  cy.visit('https://the-internet.herokuapp.com/login');
+  cy.get('#username').type('tomsmith');
+  cy.get('#password').type('invalid');
+  cy.get('.fa').click();
+  cy.get('#flash').should('contain', ' Your password is invalid!');
+});
