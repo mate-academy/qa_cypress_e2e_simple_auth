f455ab54bc04cadda03bdfec37b1df2135cc2e17
Agillka <mrmadness6661313@gmail.com>
2024-05-13 10:44:30 +0300

    RE-Done

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -5,33 +5,43 @@ describe('Sign In page', () => {
     cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('Login with valid creds (tomsmith/SuperSecretPassword!)', () => {
+  it('should have an ability to log in with valid credentials', () => {
     cy.get('#username').type('tomsmith');
     cy.get('#password').type('SuperSecretPassword!');
 
     cy.get('.fa').click();
+
+    cy.contains('div', 'You logged into a secure area!').should('exist');
   });
 
-  it('Login with invalid Username', () => {
+  it('Should not have an ability to log in with invalid username', () => {
     cy.get('#username').type('Some_username');
     cy.get('#password').type('SuperSecretPassword!');
 
     cy.get('.fa').click();
+
+    cy.contains('div', 'Your username is invalid!').should('exist');
   });
 
-  it('Login with invalid Password', () => {
+  it('Should not have an ability to log in with invalid password', () => {
     cy.get('#username').type('tomsmith');
     cy.get('#password').type('Some_password');
 
     cy.get('.fa').click();
+
+    cy.contains('div', 'Your password is invalid!').should('exist');
   });
 
-  it('Logout from the app', () => {
+  it('Should have an ability to log out', () => {
     cy.get('#username').type('tomsmith');
     cy.get('#password').type('SuperSecretPassword!');
 
     cy.get('.fa').click();
 
+    cy.contains('i', 'Logout').should('exist');
+
     cy.get('.button').click();
+
+    cy.contains('h2', 'Login Page').should('exist');
   });
 });
