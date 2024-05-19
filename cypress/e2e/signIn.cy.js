25032df349cbb79d9e9094b4c69623da461baf2c
Rozdorozhnii <rozdorozhnii@ukr.net>
2024-05-19 21:26:27 +0300

    Solution_v1.1

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -11,48 +11,42 @@ describe('Sign In page', () => {
   };
 
   it('should provide an ability to log in with valid creds', () => {
-    cy.get('form[name="login"] input[name="username"]')
-      .type(`${userData.username}`);
+    cy.get('[id="username"]').type(`${userData.username}`);
 
-    cy.get('form[name="login"] input[name="password"]')
-      .type(`${userData.password}`);
+    cy.get('[id="password"]').type(`${userData.password}`);
 
     cy.get('form[name="login"] > button[type="submit"]')
       .should('contain', ' Login').click();
 
-    cy.get('div[id="flash"]')
+    cy.get('[id="flash"]')
       .should('contains.text', 'You logged into a secure area!');
   });
 
   it('shouldn\'t provide an ability to log in with invalid creds', () => {
-    cy.get('form[name="login"] input[name="username"]').type('invalid');
+    cy.get('[id="username"]').type('invalid');
 
-    cy.get('form[name="login"] input[name="password"]')
-      .type('invalid');
+    cy.get('[id="password"]').type('invalid');
 
     cy.get('form[name="login"] > button[type="submit"]')
       .should('contain', ' Login').click();
 
-    cy.get('div[id="flash"]')
-      .should('contains.text', 'Your username is invalid!');
+    cy.get('[id="flash"]').should('contains.text', 'Your username is invalid!');
   });
 
   it('should provide an ability to log out from the app', () => {
-    cy.get('form[name="login"] input[name="username"]')
-      .type(`${userData.username}`);
+    cy.get('[id="username"]').type(`${userData.username}`);
 
-    cy.get('form[name="login"] input[name="password"]')
-      .type(`${userData.password}`);
+    cy.get('[id="password"]').type(`${userData.password}`);
 
     cy.get('form[name="login"] > button[type="submit"]')
       .should('contain', ' Login').click();
 
-    cy.get('div[id="flash"]')
+    cy.get('[id="flash"]')
       .should('contains.text', 'You logged into a secure area!');
 
     cy.get('a[href="/logout"]').click();
 
-    cy.get('div[id="flash"]')
+    cy.get('[id="flash"]')
       .should('contains.text', 'You logged out of the secure area!');
   });
 });
