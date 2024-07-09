45f1d6297fdc21005d48bc5b3330bc881333211e
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-07-09 14:42:00 +0200

    solved

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("enterCredentials", (name, password) => {
+  cy.get("#username").type(name);
+  cy.get("#password").type(password + `{Enter}`);
+});
