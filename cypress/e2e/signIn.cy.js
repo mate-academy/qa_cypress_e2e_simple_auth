5910144d44d617eba227db296ad8183268358e97
Roksolana Kalytyn <roksa.kalytyn@gmail.com>
2024-07-15 22:17:55 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,40 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('should login with valid creds', () => {
+    cy.get('#username')
+      .type('tomsmith');
 
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('.fa').click();
+
+    cy.get('.subheader')
+      .should('contain.text',
+        'Welcome to the Secure Area. When you are done click logout below.');
+
+    cy.get('.icon-2x').click();
+  });
+
+  it('should not login with invalid creds', () => {
+    cy.get('#username')
+      .type('notusername');
+
+    cy.get('#password')
+      .type('notpassword');
+
+    cy.get('.fa').click();
+
+    cy.get('#flash')
+      .should('contain.text', 'Your username is invalid!');
+  });
+
+  it('should be logout from the app', () => {
+    cy.get('h2')
+      .should('contain.text', 'Login Page');
   });
 });
