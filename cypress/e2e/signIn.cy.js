0d0ddebe2d7126360a6da4ff95c6e8069f075397
Voronaolga <voronaolga971@gmail.com>
2024-07-25 18:50:54 +0300

    Automate_Sign_In_Tests

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,45 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    // Runs before each test in the `describe` block
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('should login with valid credentials', () => {
+    // Fill in username and password fields with valid credentials
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
 
+    // Click on Login button
+    cy.get('button[type="submit"]').click();
+
+    // Assert successful login
+    // eslint-disable-next-line max-len
+    cy.contains('#flash-messages', 'You logged into a secure area!').should('be.visible');
+  });
+
+  it('should show validation errors with invalid credentials', () => {
+    // Fill in username and password fields with invalid credentials
+    cy.get('#username').type('invalidUsername');
+    cy.get('#password').type('invalidPassword');
+
+    // Click on Login button
+    cy.get('button[type="submit"]').click();
+
+    // Assert validation errors
+    cy.contains('Your username is invalid!').should('be.visible');
+  });
+
+  it('should logout successfully', () => {
+    // Login with valid credentials first
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('button[type="submit"]').click();
+
+    // Click on Logout link or button
+    cy.contains('Logout').click();
+
+    // Assert successful logout
+    cy.contains('You logged out of the secure area!').should('be.visible');
   });
 });
