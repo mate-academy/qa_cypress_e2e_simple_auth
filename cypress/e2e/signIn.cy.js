f0bb33c6b413d8639b5db4fd2ee53e0ebe64c003
Bohdan98Kuch <bohdan13kuch@gmail.com>
2024-07-25 20:39:12 +0300

    Added tests

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,28 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('Log in with valid data test 1', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
+  });
 
+  it('Log in with invalid data test 2',() => {
+    cy.get('#username').type('Qwerty123');
+    cy.get('#password').type('Qwert123@7');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain', 'Your username is invalid!');
+  });
+
+  it('Logout test 3', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('.button').click();
+    cy.get('#flash').should('contain', 'You logged out of the secure area!');
   });
 });
