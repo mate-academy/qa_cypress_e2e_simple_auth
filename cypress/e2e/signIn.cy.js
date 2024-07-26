6bd399e478a8c2b057cfded529fa693abae5263a
Bohdan98Kuch <bohdan13kuch@gmail.com>
2024-07-26 10:31:48 +0300

    Changes are added

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,27 +1,30 @@
 /// <reference types="cypress" />
 
+const username = 'tomsmith'
+const password = 'SuperSecretPassword!'
+
 describe('Sign In page', () => {
   beforeEach(() => {
     cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('Should login with valid creds', () => {
-    cy.get('#username').type('tomsmith');
-    cy.get('#password').type('SuperSecretPassword!');
+  it('Log in with valid data', () => {
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
     cy.get('.fa').click();
     cy.get('#flash').should('contain', 'You logged into a secure area!');
   });
 
-  it('Should fail login with invalid creds',() => {
+  it('Log in with invalid username',() => {
     cy.get('#username').type('Qwerty123');
     cy.get('#password').type('Qwert123@7');
     cy.get('.fa').click();
     cy.get('#flash').should('contain', 'Your username is invalid!');
   });
 
-  it('Should logout from the app', () => {
-    cy.get('#username').type('tomsmith');
-    cy.get('#password').type('SuperSecretPassword!');
+  it('Log out from the app', () => {
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
     cy.get('.fa').click();
     cy.get('.button').click();
     cy.get('#flash').should('contain', 'You logged out of the secure area!');
