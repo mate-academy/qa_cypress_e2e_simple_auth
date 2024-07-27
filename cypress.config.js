51306fd0664a20403204952510ea16f40edc9cb6
Ilya Pulcho <pulcho91@gmail.com>
2024-07-27 22:41:06 +0300

    Fix eslint issues

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-    }
+    baseUrl: 'https://the-internet.herokuapp.com'
   }
 });
