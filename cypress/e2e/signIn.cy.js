83de3f8c8766de0a5de4fec080df2034cb8ee1f2
Kate Hubko <gubkoekaterina9592@gmail.com>
2024-08-21 14:51:41 +0300

    Simple Auth Assertions

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,28 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login')
   });
 
-  it('', () => {
+  it('Log in with a valid account', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain.text', 'You logged into a secure area!')
+  });
 
+  it('Log in with an invalid account', () => {
+    cy.get('#username').type('magnetic');
+    cy.get('#password').type('qweqwe');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain.text', 'Your username is invalid!')
+  });
+  
+  it('Log out of the app', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('.button').click();
+    cy.get('#flash').should('contain.text', 'You logged out of the secure area!')
   });
 });
