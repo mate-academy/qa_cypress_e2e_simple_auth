c84c4cb4cf0ce9903220e64bd207f7d30b53393b
KateAlieksieienko <kate.alieksieienko@gmail.com>
2024-08-27 17:14:57 +0200

    Added for invalid password

--- cypress/e2e/signIn-invalid-credits.cy.js
+++ cypress/e2e/signIn-invalid-credits.cy.js
@@ -3,11 +3,19 @@ describe('Sign In page', () => {
     cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('should provide an error for log in with invalid credits', () => {
+  it('should provide an error for log in with invalid username', () => {
     cy.get('#username').type('tomsmith23');
     cy.get('#password').type('SuperSecretPassword!');
     cy.get('.fa').click();
 
     cy.get('#flash').should('contain', 'Your username is invalid!');
+  });
+
+  it('should provide an error for log in with invalid password', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!12');
+    cy.get('.fa').click();
+
+    cy.get('#flash').should('contain', 'Your password is invalid!');
   });
 });
