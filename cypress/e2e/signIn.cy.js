dd18be3f25501cb3a851ba1aaa8a3fe21dfa9ce4
Piotr <piotrwandas66@gmail.com>
2024-08-27 09:17:47 +0200

    Solution to the task

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,45 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login')
   });
 
-  it('', () => {
+  it('should provide an ability to log in', () => {
+    cy.get('#username').type('tomsmith')
+    cy.get('#password').type('SuperSecretPassword!')
 
+    cy.get('.radius').click()
+
+    cy.get('#flash').should('contain','You logged into a secure area!')
+  });
+
+  it('should display a message about an incorrect user name', () => {
+    cy.get('#username').type('tomsmit')
+    cy.get('#password').type('SuperSecretPassword!')
+
+    cy.get('.radius').click()
+
+    cy.get('#flash').should('contain','Your username is invalid!')
+  });
+
+  it('should display a message about an incorrect password', () => {
+    cy.get('#username').type('tomsmith')
+    cy.get('#password').type('SuperSecretPass!')
+
+    cy.get('.radius').click()
+
+    cy.get('#flash').should('contain','Your password is invalid!')
+  });
+
+  it('should provide an ability to loged out', () => {
+    cy.get('#username').type('tomsmith')
+    cy.get('#password').type('SuperSecretPassword!')
+
+    cy.get('.radius').click()
+
+    cy.get('#flash').should('contain','You logged into a secure area!')
+
+    cy.get('.button').click()
+    cy.get('#flash').should('contain','You logged out of the secure area!')    
   });
 });
