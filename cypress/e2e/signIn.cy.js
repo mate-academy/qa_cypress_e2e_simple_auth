e8f476551d169c352395ae9a150801f4556693c7
Agnieksza <agawin128@gmail.com>
2024-08-27 16:40:03 +0200

    fixed solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,35 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+  });
+  const userName = 'tomsmith';
+  const password = 'SuperSecretPassword!';
 
+  it('should be able to log in with valid credentials', () => {
+    cy.get('#username').type(userName);
+    cy.get('#password').type(password);
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
+  });
+  it('should not be able to log in with invalid username', () => {
+    cy.get('#username').type(userName + '123');
+    cy.get('#password').type(password);
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain', 'Your username is invalid!');
+  });
+  it('should not be able to log in with invalid password', () => {
+    cy.get('#username').type(userName);
+    cy.get('#password').type(password + '123');
+    cy.get('.fa').click();
+    cy.get('#flash').should('contain', 'Your password is invalid!');
   });
 
-  it('', () => {
-
+  it('should succesfuly log out', () => {
+    cy.get('#username').type(userName);
+    cy.get('#password').type(password);
+    cy.get('.fa').click();
+    cy.get('.icon-2x').click();
+    cy.get('#flash').should('contain', 'You logged out of the secure area!');
   });
 });
