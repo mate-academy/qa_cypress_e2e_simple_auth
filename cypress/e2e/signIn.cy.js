27e5bfd8a1bff1c03846c0e1a4f22ee256de4be9
Piotr <piotrwandas66@gmail.com>
2024-08-27 14:56:35 +0200

    commit after code review

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,45 +2,37 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-    cy.visit('https://the-internet.herokuapp.com/login')
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
   it('should provide an ability to log in', () => {
-    cy.get('#username').type('tomsmith')
-    cy.get('#password').type('SuperSecretPassword!')
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
 
-    cy.get('.radius').click()
+    cy.get('.radius').click();
 
-    cy.get('#flash').should('contain','You logged into a secure area!')
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
   });
 
-  it('should display a message about an incorrect user name', () => {
-    cy.get('#username').type('tomsmit')
-    cy.get('#password').type('SuperSecretPassword!')
+  it('should display message about an incorrect username and password', () => {
+    cy.get('#username').type('tomsmit');
+    cy.get('#password').type('SuperSecretPassword!');
 
-    cy.get('.radius').click()
+    cy.get('.radius').click();
 
-    cy.get('#flash').should('contain','Your username is invalid!')
-  });
-
-  it('should display a message about an incorrect password', () => {
-    cy.get('#username').type('tomsmith')
-    cy.get('#password').type('SuperSecretPass!')
-
-    cy.get('.radius').click()
-
-    cy.get('#flash').should('contain','Your password is invalid!')
+    cy.get('#flash').should('contain', 'Your username is invalid!');
+    cy.get('#flash').should('contain', 'Your password is invalid!');
   });
 
   it('should provide an ability to loged out', () => {
-    cy.get('#username').type('tomsmith')
-    cy.get('#password').type('SuperSecretPassword!')
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
 
-    cy.get('.radius').click()
+    cy.get('.radius').click();
 
-    cy.get('#flash').should('contain','You logged into a secure area!')
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
 
-    cy.get('.button').click()
-    cy.get('#flash').should('contain','You logged out of the secure area!')    
+    cy.get('.button').click();
+    cy.get('#flash').should('contain', 'You logged out of the secure area!');
   });
 });
