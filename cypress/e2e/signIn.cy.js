e3dfc9d60ed0620bc4f8f32f2d58b0fbce415ffa
Grzegorz <grzegorzwieli@gmail.com>
2024-08-29 22:15:22 +0200

    Added tests

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,21 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
-
+  it('Should login succesfuly', () => {
+    //login with proprer credentials
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.contains('i', ' Login').click();
+    cy.contains('#flash', 'You logged into a secure area!').should('be.visible');
+    //logout
+    cy.get('a[href="/logout"]').click();
+    //login with oncorrect credentials
+    cy.get('#username').type('tom-smith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.contains('i', ' Login').click();
+    cy.contains('#flash', 'Your username is invalid!').should('be.visible');
   });
 });
