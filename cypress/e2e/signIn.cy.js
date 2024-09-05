115b75ba35a353606b339a282bd612bc1b7cf1fe
maksim-atanasov <atanasovmaksim56@gmail.com>
2024-09-05 22:49:37 +0300

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -5,7 +5,27 @@ describe('Sign In page', () => {
 
   });
 
-  it('', () => {
+  it('should succesfully login and logout', () => {
+    const username = 'tomsmith';
+    const password = 'SuperSecretPassword!';
 
+    cy.visit('/login');
+    cy.get('input[name="username"]').type(username);
+    cy.get('input[name="password"]').type(password + '{enter}');
+
+    cy.contains('div', 'You logged into a secure area!');
+    cy.contains('i', 'Logout').click();
+    cy.contains('div', 'You logged out of the secure area!');
+  });
+
+  it('should return error if wrong login and password', () => {
+    const username = 'ffff';
+    const password = 'fffffffffff';
+
+    cy.visit('/login');
+    cy.get('input[name="username"]').type(username);
+    cy.get('input[name="password"]').type(password + '{enter}');
+
+    cy.contains('div', 'Your username is invalid!');
   });
 });
