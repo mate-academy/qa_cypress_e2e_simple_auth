571346ea2a0001e35671fc17176042b8912823a2
Sofi Mamzer <sofiiamamzer06@gmail.com>
2024-10-10 12:54:35 +0300

    Add login tests for valid/invalid creds and logout

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,31 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('Login with valid creds', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa.fa-2x.fa-sign-in').click();
+    cy.get('#flash')
+      .should('contain', 'You logged into a secure area!');
+  });
 
+  it('Login with invalid creds', () => {
+    cy.get('#username').type('jhope');
+    cy.get('#password').type('qwertyty');
+    cy.get('.fa.fa-2x.fa-sign-in').click();
+    cy.get('#flash')
+      .should('contain', 'Your username is invalid!');
+  });
+
+  it('Logout from the app', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa.fa-2x.fa-sign-in').click();
+    cy.get('.icon-2x.icon-signout').click();
+    cy.get('#flash')
+      .should('contain', 'You logged out of the secure area!');
   });
 });
