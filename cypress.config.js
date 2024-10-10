60d2847c41d290b812fe20f8b2a20347076d8a8b
mhyryak <hyryakm@gmail.com>
2024-10-10 11:48:39 +0200

    Sign in test

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,10 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+    },
+    env: {
+      validUsername: 'tomsmith',
+      validPassword: 'SuperSecretPassword!'
     }
   }
 });
--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,41 @@
-/// <reference types="cypress" />
+describe('Login Tests', () => {
+  const validUsername = Cypress.env('validUsername');
+  const validPassword = Cypress.env('validPassword');
 
-describe('Sign In page', () => {
-  beforeEach(() => {
+  it('Login with valid credentials', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
 
+    cy.get('input[name="username"]').type(validUsername);
+    cy.get('input[name="password"]').type(validPassword);
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/secure');
   });
+});
 
-  it('', () => {
+it('Login with invalid credentials', () => {
+  cy.visit('https://the-internet.herokuapp.com/login');
 
-  });
+  cy.get('input[name="username"]').type('test123');
+  cy.get('input[name="password"]').type('TestTest123+');
+  cy.get('button[type="submit"]').click();
+
+  cy.get('.flash.error').should('contain', 'Your username is invalid!');
+});
+
+it('Logout from the app', () => {
+  cy.visit('https://the-internet.herokuapp.com/login');
+
+  const validUsername = Cypress.env('validUsername');
+  const validPassword = Cypress.env('validPassword');
+
+  cy.get('input[name="username"]').type(validUsername);
+  cy.get('input[name="password"]').type(validPassword);
+  cy.get('button[type="submit"]').click();
+
+  cy.url().should('include', '/secure');
+  cy.get('.flash.success').should('contain', 'You logged into a secure area!');
+
+  cy.get('a[href="/logout"]').click();
+
+  cy.url().should('include', '/login');
 });
