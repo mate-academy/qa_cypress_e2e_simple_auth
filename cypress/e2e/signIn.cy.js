bd405f8b0dff51dac67e9108004616fbc2d9e87c
Mikola Lemeshev <mate.lemeshev@gmail.com>
2024-10-10 22:58:06 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,40 @@
 /// <reference types="cypress" />
 
+const username = 'tomsmith';
+const password = 'SuperSecretPassword!';
+const signInPage = 'Login Page';
+const sigInSucces = 'Welcome';
+const invalidUsername = 'jeimelanister';
+const invalidPassword = 'User123$';
+const notSucces = 'Your username is invalid!';
+
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('Should sign in with valid creds', () => {
+    cy.get('h2').should('contain', signInPage);
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
+    cy.get('button[type="submit"]').click();
+    cy.get('h4.subheader').should('contain', sigInSucces);
+  });
 
+  it('Should message with invalid creds', () => {
+    cy.get('h2').should('contain', signInPage);
+    cy.get('#username').type(invalidUsername);
+    cy.get('#password').type(invalidPassword);
+    cy.get('button[type="submit"]').click();
+    cy.get('#flash').should('contain', notSucces);
+  });
+
+  it('Should logout from the app', () => {
+    cy.get('h2').should('contain', signInPage);
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
+    cy.get('button[type="submit"]').click();
+    cy.get('h4.subheader').should('contain', sigInSucces);
+    cy.get('a.button.secondary.radius').click();
   });
 });
