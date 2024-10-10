856c2986829e068e2749a056accfae3d7c26d50f
luckezQA <labchuk2007@gmail.com>
2024-10-10 22:44:04 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,26 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
-
+  it('should log in with valid creds', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('#flash').contains('logged into');
+  });
+  it('should display error message where entered invalid creds', () => {
+    cy.get('#username').type('tomsmith1');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('#flash').contains('invalid');
+  });
+  it('should log out user after click on [Log out] button', () => {
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('.fa').click();
+    cy.get('.icon-2x').click();
+    cy.get('#flash').contains('logged out');
   });
 });
