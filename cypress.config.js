8c31b80309aa4d063092635a4d5f7bee47bff466
LyudmilaDidkivska <qawsed32@ukr.net>
2024-10-10 14:38:42 +0300

    Automate login, invalid login, and logout tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,11 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-    }
-  }
+     
+    },
+    env: {
+      email: 'tomsmith', 
+      password: 'SuperSecretPassword!',      
+    },
+  },
 });
--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,49 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  beforeEach(() => {
+  beforeEach(() => cy.visit('https://the-internet.herokuapp.com/login'));
 
+  
+  
+});
+describe('Login tests', () => {
+  const validUsername = 'tomsmith';
+  const validPassword = 'SuperSecretPassword!';
+  const invalidUsername = 'invalidUser';
+  const invalidPassword = 'invalidPass';
+  
+  beforeEach(() => {
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('Login with valid credentials', () => {
+    cy.get('#username').type(validUsername);
+    cy.get('#password').type(validPassword);
+    cy.get('.radius').click();
 
+   
+    cy.get('.flash.success').should('contain', 'You logged into a secure area!');
+  });
+
+  it('Login with invalid credentials', () => {
+    cy.get('#username').type(invalidUsername);
+    cy.get('#password').type(invalidPassword);
+    cy.get('.radius').click();
+
+    
+    cy.get('.flash.error').should('contain', 'Your username is invalid!');
+  });
+
+  it('Logout from the app', () => {
+    
+    cy.get('#username').type(validUsername);
+    cy.get('#password').type(validPassword);
+    cy.get('.radius').click();
+
+    
+    cy.get('a[href="/logout"]').click();
+
+   
+    cy.get('.flash.success').should('contain', 'You logged out of the secure area!');
   });
 });
