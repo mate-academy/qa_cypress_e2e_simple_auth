029166d48df3c5d329c8a72b3228ab43a977f682
Vlad <lukashukvlad534@gmail.com>
2024-10-10 16:26:17 +0300

    Secure Area testing

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,33 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  beforeEach(() => {
+  beforeEach(() => cy.visit('https://the-internet.herokuapp.com/login'));
 
-  });
+  it('should provide an ability to log in', () => {
+    cy.url().should('contain', 'login');
 
-  it('', () => {
+    cy.get('#username').type('tomsmith');
 
+    cy.get('#password').type('SuperSecretPassword!');
+
+    cy.contains('button', 'Login').click();
+
+    cy.get('#flash').should('contain', 'You logged into a secure area!');
+
+    cy.url().should('contain', 'secure');
+
+    cy.contains('a', 'Logout').click();
+
+    cy.url().should('contain', 'login');
+
+    cy.get('#flash').should('contain', 'You logged out of the secure area!');
+
+    cy.get('#username').type('invalidtomsmith');
+
+    cy.get('#password').type('invalidSuperSecretPassword!');
+
+    cy.contains('button', 'Login').click();
+
+    cy.get('#flash').should('contain', 'Your username is invalid!');
   });
 });
