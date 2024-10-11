cf391c5d62b5f41531b40b0e6f1634b4d9f342ef
Roman Prykhodko <r.prykhodko@yahoo.com>
2024-10-11 18:58:57 +0300

    Added Simple auth tests

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,37 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  beforeEach(() => {
+  const userName = 'tomsmith';
+  const password = 'SuperSecretPassword!';
+  const errorMessage = 'Your username is invalid!';
+  const loggedOutMessage = 'You logged out of the secure area!';
 
+  beforeEach(() => {
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('should login with valid creds', () => {
+    cy.get('#username').type(userName);
+    cy.get('#password').type(password);
+    cy.get('.radius').click();
+  });
 
+  it('should allow the user to log out', () => {
+    cy.get('#username').type(userName);
+    cy.get('#password').type(password);
+    cy.get('.radius').click();
+    cy.get('.button.secondary.radius').click();
+    cy.get('.flash.success')
+      .should('contain.text', loggedOutMessage)
+      .and('be.visible');
+  });
+
+  it('should report an error when entering incorrect data', () => {
+    cy.get('#username').type(userName + 1);
+    cy.get('#password').type(password + 1);
+    cy.get('.radius').click();
+    cy.get('#flash')
+      .should('contain.text', errorMessage)
+      .and('be.visible');
   });
 });
