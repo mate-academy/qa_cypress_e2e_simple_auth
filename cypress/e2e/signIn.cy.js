61594632b57e7007a2c032a4f7d0790a99c0ed0b
Dmytro Saliuk <saliukdmytro1@gmail.com>
2024-10-16 14:35:30 +0300

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,52 @@
-/// <reference types="cypress" />
+describe('Login Flow', () => {
+  const validUsername = 'tomsmith';
+  const validPassword = 'SuperSecretPassword!';
+  const invalidUsername = 'invalidUsername';
+  const invalidPassword = 'invalidPassword';
 
-describe('Sign In page', () => {
-  beforeEach(() => {
+  it('should log in with valid credentials', () => {
 
+    cy.visit('https://the-internet.herokuapp.com/login');
+
+
+    cy.get('input[name="username"]').type(validUsername);
+    cy.get('input[name="password"]').type(validPassword);
+
+
+    cy.get('button[type="submit"]').click();
+
+
+    cy.url().should('include', '/secure');
+    cy.get('div[class="flash success"]').should('contain', 'You logged into a secure area!');
   });
 
-  it('', () => {
+  it('should show validation errors with invalid credentials', () => {
 
+    cy.visit('https://the-internet.herokuapp.com/login');
+
+
+    cy.get('input[name="username"]').type(invalidUsername);
+    cy.get('input[name="password"]').type(invalidPassword);
+
+
+    cy.get('button[type="submit"]').click();
+
+
+    cy.get('div[class="flash error"]').should('contain', 'Your username is invalid!');
+  });
+
+  it('should log out successfully', () => {
+
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('input[name="username"]').type(validUsername);
+    cy.get('input[name="password"]').type(validPassword);
+    cy.get('button[type="submit"]').click();
+
+
+    cy.get('a[href="/logout"]').click();
+
+
+    cy.url().should('include', '/login');
+    cy.get('div[class="flash success"]').should('contain', 'You logged out of the secure area!');
   });
 });
