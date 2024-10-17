01f1ef5416740c747c0e6861959efbb904d00392
Rodion Pushkarskiy <pushkarskiyrodion@gmail.com>
2024-10-17 23:26:24 +0300

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,38 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  beforeEach(() => {
+  const username = 'tomsmith';
+  const password = 'SuperSecretPassword!';
 
+  beforeEach(() => {
+    cy.visit('/login');
+    cy.assertPageURL('/login');
   });
 
-  it('', () => {
+  it('should log in if credentials are correct', () => {
+    cy.login(username, password);
+    cy.get('.flash.success').contains('You logged into a secure area!');
+  });
 
+  it('should display an error message for invalid username', () => {
+    cy.login(username + 'invalid', password);
+    cy.get('.flash.error').contains('Your username is invalid!');
+  });
+
+  it('should display an error message for invalid password', () => {
+    cy.login(username, password + 'invalid');
+    cy.get('.flash.error').contains('Your password is invalid!');
+  });
+
+  it('should redirect to the correct URL after successful login', () => {
+    cy.login(username, password);
+    cy.assertPageURL('/secure');
+    cy.get('.flash.success').contains('You logged into a secure area!');
+  });
+
+  it('should log out from the app', () => {
+    cy.login(username, password);
+    cy.get('[href="/logout"]').click();
+    cy.get('.flash').contains('You logged out of the secure area!');
   });
 });
