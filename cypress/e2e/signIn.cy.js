68d4c1fbcce1fbb08cd85c95918d882583cc4748
DmytroSaliuk <139687940+DmytroSaliuk@users.noreply.github.com>
2024-10-22 15:31:11 +0300

    Update signIn.cy.js

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,34 +2,46 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-  beforeEach(() => cy.visit('https://the-internet.herokuapp.com/login'));
+    cy.visit('https://the-internet.herokuapp.com/login');
+  });
 
   it('Should login with valid creds', () => {
-    cy.get('#username').type('tomsmith');
-    cy.get('#password').type('SuperSecretPassword!');
+    const username = 'tomsmith';
+    const password = 'SuperSecretPassword!';
+
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
     cy.get('[type="submit"]').click();
     cy.contains('#flash', 'You logged into a secure area!');
   });
 
-  it('', () => {
+  it('Shouldn\'t login with invalid username', () => {
+    const password = 'SuperSecretPassword!';
 
-  it('Should not login with invalid username', () => {
-    cy.get('#username').type('anton');
-    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('#username').type('invalidUsername');
+    cy.get('#password').type(password);
     cy.get('[type="submit"]').click();
     cy.contains('#flash', 'Your username is invalid!');
   });
-  it('Should not login with invalid password', () => {
-    cy.get('#username').type('tomsmith');
-    cy.get('#password').type('SuperSecretPasswordf!');
+
+  it('Shouldn\'t login with invalid password', () => {
+    const username = 'tomsmith';
+
+    cy.get('#username').type(username);
+    cy.get('#password').type('invalidPassword');
     cy.get('[type="submit"]').click();
     cy.contains('#flash', 'Your password is invalid!');
   });
+
   it('Should logout from the app', () => {
-    cy.get('#username').type('tomsmith');
-    cy.get('#password').type('SuperSecretPassword!');
+    const username = 'tomsmith';
+    const password = 'SuperSecretPassword!';
+
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
     cy.get('[type="submit"]').click();
     cy.contains('#flash', 'You logged into a secure area!');
+
     cy.get('[class="button secondary radius"]').click();
     cy.contains('#flash', 'You logged out of the secure area!');
   });
