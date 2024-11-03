a0acaad19ae44705fa97955e54ee2c4cd553d5a8
Demian Zolotukhin <demianzolotukhin@gmail.com>
2024-11-03 21:02:12 +0200

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,43 @@
 /// <reference types="cypress" />
 
+// const succesMessage = 'Welcome to the Secure Area. When you are done click logout below.'
+// const errorMessage = 'Your username is invalid!'
+
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('Should allow log in with valid data', () => {
+    cy.get('#username').type('tomsmith');
 
+    cy.get('#password').type('SuperSecretPassword!');
+
+    cy.get('.radius').click();
+
+    cy.get('h4').should('contain.text', 'Welcome to the Secure Area. ' +
+      ' When you are done click logout below.');
+  });
+
+  it('Should not allow log in with invalid data', () => {
+    cy.get('#username').type('tomsmith1');
+
+    cy.get('#password').type('SuperSecretPassword!1');
+
+    cy.get('.radius').click();
+
+    cy.get('#flash').should('contain.text', 'Your username is invalid!');
+  });
+
+  it('Should allow to log out', () => {
+    cy.get('#username').type('tomsmith');
+
+    cy.get('#password').type('SuperSecretPassword!');
+
+    cy.get('.radius').click();
+
+    cy.get('.button').click();
+
+    cy.get('h2').should('contain.text', 'Login Page');
   });
 });
