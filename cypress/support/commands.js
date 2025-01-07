9729b4150c52453a6eaa02e855af2a8ac1ad8049
Galers <galosaandrew@gmail.com>
2025-01-07 16:57:35 +0200

    test: Added tests for logging in

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,13 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', ({ name, password }) => {
+  cy.visit('/login');
+  cy.get('#username').type(name);
+  cy.get('#password').type(`${password}{Enter}`);
+});
+
+Cypress.Commands.add('getPushMessage', (message) => {
+  cy.get('#flash').should('contain', message);
+});
+
+Cypress.Commands.add('clickOnLogout', (message) => {
+  cy.get('.button').should('be.visible').click();
+});
