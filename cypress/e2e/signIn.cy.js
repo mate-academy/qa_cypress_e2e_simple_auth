da2b30d3df8d7541c238d668f9247d6a8fdb5c67
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2025-01-28 15:09:19 +0200

    tests for Sign in

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,22 @@
 /// <reference types="cypress" />
 
 describe('Sign In page', () => {
-  beforeEach(() => {
+  beforeEach(() => {});
 
+  it('should login with valid creds', () => {
+    cy.login('tomsmith', 'SuperSecretPassword!');
+    cy.get('.flash.success').should('be.visible');
   });
 
-  it('', () => {
+  it('should not login with invalid creds', () => {
+    cy.login('tomsmith', 'badpassword');
+    cy.get('.flash.error').should('be.visible');
+  });
 
+  it('should logout', () => {
+    cy.login('tomsmith', 'SuperSecretPassword!');
+    cy.get('.flash.success').should('be.visible');
+    cy.contains('Logout').click();
+    cy.get('.flash.success').should('be.visible');
   });
 });
