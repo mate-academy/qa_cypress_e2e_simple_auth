7a4de4b7f6deb2afbf140fd420ac0d8eb291c02b
Svitlana <svitlana.gaiduk@gmail.com>
2025-02-05 22:32:47 +0200

    use id selectors instead of placeholder

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -11,25 +11,25 @@ describe('Sign In page', () => {
   });
 
   it('Should log in successfully', () => {
-    cy.get('[placeholder=username]').type(validUsername);
-    cy.get('[placeholder=password]').type(validPassword);
-    cy.get('button[type=submit]').click();
+    cy.get('[id="username"]').type(validUsername);
+    cy.get('[id="password"]').type(validPassword);
+    cy.get('button[type="submit"]').click();
     cy.url().should('include', '/secure');
     cy.contains('You logged into a secure area!').should('be.visible');
   });
 
   it('Should fail to log in', () => {
-    cy.get('[placeholder=username]').type(invalidUsername);
-    cy.get('[placeholder=password]').type(invalidPassword);
-    cy.get('button[type=submit]').click();
+    cy.get('[id="username"]').type(invalidUsername);
+    cy.get('[id="password"]').type(invalidPassword);
+    cy.get('button[type="submit"]').click();
     cy.url().should('include', '/login');
     cy.contains('Your username is invalid!').should('be.visible');
   });
 
   it('Should log out successfully', () => {
-    cy.get('[placeholder=username]').type(validUsername);
-    cy.get('[placeholder=password]').type(validPassword);
-    cy.get('button[type=submit]').click();
+    cy.get('[id="username"]').type(validUsername);
+    cy.get('[id="password"]').type(validPassword);
+    cy.get('button[type="submit"]').click();
     cy.contains('Logout').click();
     cy.url().should('include', '/login');
     cy.contains('You logged out of the secure area!').should('be.visible');
