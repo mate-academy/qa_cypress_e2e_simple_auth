85a91f9f98dedf5da5b850c7c7ecab9eebf13c46
Svitlana <svitlana.gaiduk@gmail.com>
2025-02-05 22:18:11 +0200

    add task solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,37 @@
-/// <reference types="cypress" />
+/// <reference types='cypress' />
 
 describe('Sign In page', () => {
-  beforeEach(() => {
+  const validUsername = 'tomsmith';
+  const validPassword = 'SuperSecretPassword!';
+  const invalidUsername = 'invalid';
+  const invalidPassword = 'invalid';
 
+  beforeEach(() => {
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('Should log in successfully', () => {
+    cy.get('[placeholder=username]').type(validUsername);
+    cy.get('[placeholder=password]').type(validPassword);
+    cy.get('button[type=submit]').click();
+    cy.url().should('include', '/secure');
+    cy.contains('You logged into a secure area!').should('be.visible');
+  });
 
+  it('Should fail to log in', () => {
+    cy.get('[placeholder=username]').type(invalidUsername);
+    cy.get('[placeholder=password]').type(invalidPassword);
+    cy.get('button[type=submit]').click();
+    cy.url().should('include', '/login');
+    cy.contains('Your username is invalid!').should('be.visible');
+  });
+
+  it('Should log out successfully', () => {
+    cy.get('[placeholder=username]').type(validUsername);
+    cy.get('[placeholder=password]').type(validPassword);
+    cy.get('button[type=submit]').click();
+    cy.contains('Logout').click();
+    cy.url().should('include', '/login');
+    cy.contains('You logged out of the secure area!').should('be.visible');
   });
 });
